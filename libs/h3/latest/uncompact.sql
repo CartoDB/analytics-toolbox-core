01890a2e4d6f5ee2c6ea1c155a74bd67acb35931
Javier de la Torre <jatorre@Air-de-Javier.fios-router.home>
2019-08-25 22:50:14 -0400

    Fixed minor bug

--- libs/h3/latest/uncompact.sql
+++ libs/h3/latest/uncompact.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION jslibs.h3.uncompact(compactedSet ARRAY, resolution NUMERIC)
+CREATE OR REPLACE FUNCTION jslibs.h3.uncompact(compactedSet ARRAY<STRING>, resolution NUMERIC)
  RETURNS ARRAY<STRING>
  LANGUAGE js AS
 """
