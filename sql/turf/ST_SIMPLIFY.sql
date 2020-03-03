4b1283e13a5ae52206b4d2f782746b9da945abca
François BAPTISTE <elvissieux@gmx.com>
2020-03-04 00:15:20 +0100

    fix typo

--- sql/turf/ST_SIMPLIFY.sql
+++ sql/turf/ST_SIMPLIFY.sql
@@ -1,3 +1,3 @@
 CREATE FUNCTION jslibs.turf.ST_SIMPLIFY(geojson GEOGRAPHY, tolerance NUMERIC) AS (
-  ST_GEOGFROMGEOJSON(jslibs.turf.simplifyt(ST_ASGEOJSON(geojson), STRUCT(tolerance as tolerance, true as highQuality)))
+  ST_GEOGFROMGEOJSON(jslibs.turf.simplify(ST_ASGEOJSON(geojson), STRUCT(tolerance as tolerance, true as highQuality)))
 );
