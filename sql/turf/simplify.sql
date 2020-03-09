b2ec3ee816a253e838d96ad99451658b106daf0a
François BAPTISTE <elvissieux@gmx.com>
2020-03-09 21:59:07 +0100

    use CREATE OR REPLACE instead of CREATE

--- sql/turf/simplify.sql
+++ sql/turf/simplify.sql
@@ -1,4 +1,4 @@
-CREATE FUNCTION jslibs.turf.simplify(geojson STRING, options STRUCT<tolerance NUMERIC, highQuality BOOL>)
+CREATE OR REPLACE FUNCTION jslibs.turf.simplify(geojson STRING, options STRUCT<tolerance NUMERIC, highQuality BOOL>)
  RETURNS STRING
  LANGUAGE js AS
 """
