98a0fd11982d003f6226afbeb6a974f8e8d1bc4a
François BAPTISTE <elvissieux@gmx.com>
2020-03-09 21:59:07 +0100

    use CREATE OR REPLACE instead of CREATE

--- sql/turf/ST_BUFFER.sql
+++ sql/turf/ST_BUFFER.sql
@@ -1,3 +1,3 @@
-CREATE FUNCTION jslibs.turf.ST_BUFFER(geojson GEOGRAPHY, radius NUMERIC, units STRING, steps NUMERIC) AS (
+CREATE OR REPLACE FUNCTION jslibs.turf.ST_BUFFER(geojson GEOGRAPHY, radius NUMERIC, units STRING, steps NUMERIC) AS (
   ST_GEOGFROMGEOJSON(jslibs.turf.buffer(ST_ASGEOJSON(geojson),radius,STRUCT(units,steps)))
 );
