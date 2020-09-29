c099a3429626c7e54715d390b365b71114ec725e
Raúl Marín <git@rmr.ninja>
2020-09-29 12:07:58 +0200

    Add placekey.js functionality
    Taken from placekey.js dist/es6/lib/ and modified:
    - Removed imports
    - Add h3 prefix to h3-js functions

---
+++ sql/placekey/geoToPlacekey.sql
@@ -0,0 +1,11 @@
+CREATE OR REPLACE FUNCTION jslibs.placekey.geoToPlacekey(latitude FLOAT64, longitude FLOAT64)
+    RETURNS STRING
+    LANGUAGE js AS
+"""
+    return geoToPlacekey(latitude, longitude);
+"""
+OPTIONS (
+  library=["gs://bigquery-jslibs/h3-js.umd.js",
+           "gs://bigquery-jslibs/h3-integer.js",
+           "gs://bigquery-jslibs/placekey.js"]
+);
---
+++ sql/placekey/placekeyToH3.sql
@@ -0,0 +1,11 @@
+CREATE OR REPLACE FUNCTION jslibs.placekey.placekeyToH3(placekey STRING)
+    RETURNS STRING
+    LANGUAGE js AS
+"""
+    return placekeyToH3(placekey);
+"""
+OPTIONS (
+  library=["gs://bigquery-jslibs/h3-js.umd.js",
+           "gs://bigquery-jslibs/h3-integer.js",
+           "gs://bigquery-jslibs/placekey.js"]
+);
