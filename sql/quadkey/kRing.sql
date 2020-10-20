155fc25aef68439d577fecc32af09d423ce7eb0d
Javier de la Torre <jatorre@carto.com>
2020-10-20 15:44:35 +0200

    fix small bug
    Small bug on the function

--- sql/quadkey/kRing.sql
+++ sql/quadkey/kRing.sql
@@ -5,7 +5,7 @@ CREATE OR REPLACE FUNCTION jslibs.quadkey.kRing(quadkey STRING, ringSize NUMERIC
 var left      = sibling(quadkey,'left');
 var topleft   = sibling(left,'up');
 var downleft  = sibling(left,'down');
-var right     = sibling(quadkey,'left');
+var right     = sibling(quadkey,'right');
 var topright  = sibling(right,'up');
 var downright = sibling(right,'down');
 var up        = sibling(quadkey,'up');
