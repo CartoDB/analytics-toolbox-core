892470a36c9ae6791b17b3af562482d55b20fb93
Subhargh <SMazu@users.noreply.github.com>
2020-12-15 00:46:46 +0800

    Update ST_S2.sql

--- sql/s2/ST_S2.sql
+++ sql/s2/ST_S2.sql
@@ -1,4 +1,3 @@
-CREATE OR REPLACE FUNCTION jslibs.s2.ST_S2(latitude FLOAT64, longitude FLOAT64, level NUMERIC) AS (
-  (SELECT STRING_AGG(FORMAT('%02x', CAST(jslibs.s2.latLngToId(latitude, longitude, level) AS INT64) >> (byte * 8) & 0xff), '' ORDER BY byte DESC)
-   FROM UNNEST(GENERATE_ARRAY(0, 7)) AS byte)
+CREATE OR REPLACE FUNCTION jslibs.s2.ST_S2(point GEOGRAPHY, resolution NUMERIC) AS (
+  jslibs.s2.geoToS2(ST_Y(point),ST_X(point),resolution)
 );
