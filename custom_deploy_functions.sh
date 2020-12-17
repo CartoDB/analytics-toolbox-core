93bb28af952b5ca79a8ee35c82605b1226f55e9b
Matt Jackson <matthew.jackson@uk.qbe.com>
2020-12-17 10:55:58 +0000

    Working custom deploy script
    Had to move geoToS2.sql around for dependency reasons & fix some quoting

--- custom_deploy_functions.sh
+++ custom_deploy_functions.sh
@@ -1,37 +1,31 @@
 #!/bin/bash
 set -eux
 
-alias gsutil=/usr/lib/google-cloud-sdk/platform/gsutil/gsutil
+GCP_PROJECT_ID="my-project-name"
+GCP_JSLIBS_ID="${GCP_PROJECT_ID}-jslibs"
+GCP_JSLIBS_BUCKET="gs://${GCP_JSLIBS_ID}/"
 
-GCP_PROJECT_ID="matthew-jackson"
-GCP_BUCKET="gs://${GCP_PROJECT_ID}-jslibs/"
+#region to deploy functions
+region="eu"
 
-#regions where to deploy. default_us is there to denote the default wich is US and not qualified
-regions=( eu )
+#make the deployment bucket if not exists
+gsutil -q stat $GCP_JSLIBS_BUCKET -p $GCP_PROJECT_ID && \
+  gsutil mb -c standard -l eu $GCP_JSLIBS_BUCKET -p $GCP_PROJECT_ID  
 
-# make the deployment bucket if not exists
-gsutil -q stat $GCP_BUCKET -p $GCP_PROJECT_ID && \
-  gsutil mb -c standard -l eu $GCP_BUCKET -p $GCP_PROJECT_ID  
+#deploy the target JS libraries
+gsutil cp libs/* $GCP_JSLIBS_BUCKET -p $GCP_PROJECT_ID
 
-#Deploy JS libraries
-gsutil cp libs/* $GCP_BUCKET -p $GCP_PROJECT_ID
-
-#create datsets if it does not exist Datasets in all regions
+#create datasets to host each of the wrapper libraries in sql/
 ls sql | sort -z|while read libname; do
-  #we iterate over the regions
-  for reg in "${regions[@]}"
-  do
-    region="eu"
-    datasetname="$libname"
+  datasetname="$libname"
 
-    #create the dataset if not exists
-    bq show "$datasetname" ||  bq --project_id="$GCP_PROJECT_ID" --location="$reg" mk -d \
-        --description "Dataset in ${reg} for functions of library: ${libname}" \
-        "$datasetname"
-  done
+  #create the dataset if not exists
+  bq show "$datasetname" ||  bq --project_id="$GCP_PROJECT_ID" --location="$reg" mk -d \
+      --description "Dataset in ${reg} for functions of library: ${libname}" \
+      "$datasetname"
 done
 
-# Deploy the SQL wrapper functions to BQ
+#deploy the wrapper functions
 find "$(pwd)" -name "*.sql" | sort  -z |while read fname; do
   echo "deploying: ${fname}"
   DIR=$(dirname "${fname}")
@@ -39,21 +33,12 @@ find "$(pwd)" -name "*.sql" | sort  -z |while read fname; do
   file_name=$(basename "${fname}")
   function_name="${file_name%.*}"
 
-  # Iterate over the regions to update or create all functions in the different regions
-  for reg in "${regions[@]}"
-  do
-  
-    # Update all the project references in the js files example jslibs.s2. with jslibs.eu_s2.
-    sed -e "s/bigquery-jslibs/${GCP_PROJECT_ID}-jslibs/g" -e "s/jslibs\.${libname}\./\`${GCP_PROJECT_ID}\`.${libname}./g" \
-      $fname > tmp1.file
+  #update all the references in the js files, eg. jslibs.h3.func_name -> `my-project-name`.h3.func_name
+  sed -e "s/bigquery-jslibs/${GCP_JSLIBS_ID}/g" -e "s/jslibs\.${libname}\./\`${GCP_PROJECT_ID}\`.${libname}./g" \
+    $fname > tmp1.file
 
-    #cat tmp.file
-    #echo "FINDING ERROR: $(grep 'gs://bigquery-`matthew-jackson.eu_h3`-js.umd.js' tmp1.file )"
-
-    # deploy the function
-    bq  --project_id="${GCP_PROJECT_ID}" --location="$reg" query --use_legacy_sql=false --flagfile=tmp1.file
-    rm tmp1.file 
-
-  done
+  # deploy the function
+  bq  --project_id="${GCP_PROJECT_ID}" --location="$region" query --use_legacy_sql=false --flagfile=tmp1.file
+  rm tmp1.file 
 done
 
