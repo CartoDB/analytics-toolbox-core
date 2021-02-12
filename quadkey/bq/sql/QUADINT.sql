70771ccf8e441b827b4de939fdcae42958bab955
vdelacruzb <vdelacruzb@gmail.com>
2021-02-12 09:51:45 +0100

    Update argument naming

--- quadkey/bq/sql/QUADINT.sql
+++ quadkey/bq/sql/QUADINT.sql
@@ -8,10 +8,10 @@ CREATE OR REPLACE FUNCTION `@@BQ_PROJECTID@@.@@BQ_DATASET_QUADKEY@@.QUADINT_FROM
     RETURNS INT64
 AS (((z & 0x1F) | (x << 5) | (y << (z + 5))));
 
-CREATE OR REPLACE FUNCTION `@@BQ_PROJECTID@@.@@BQ_DATASET_QUADKEY@@.ZXY_FROM_QUADINT`(quadkey INT64)
+CREATE OR REPLACE FUNCTION `@@BQ_PROJECTID@@.@@BQ_DATASET_QUADKEY@@.ZXY_FROM_QUADINT`(quadint INT64)
     RETURNS STRUCT<z INT64, x INT64, y INT64>
 AS (STRUCT(
-  quadkey & 0x1F as z,
-  (quadkey >> 5) & ((1 << (quadkey & 0x1F)) - 1) as x,
-  (quadkey >> (5 + (quadkey & 0x1F))) as y
+  quadint & 0x1F as z,
+  (quadint >> 5) & ((1 << (quadint & 0x1F)) - 1) as x,
+  (quadint >> (5 + (quadint & 0x1F))) as y
 ));
