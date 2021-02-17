79a34fe12e20c6df7215ea30f7a6c6823896da60
vdelacruzb <vdelacruzb@gmail.com>
2021-02-17 14:07:22 +0100

    add integration tests

--- sphericalmercator/bq/test/sphericalmercator_integration.js
+++ sphericalmercator/bq/test/sphericalmercator_integration.js
@@ -19,11 +19,6 @@ describe('SPHERICALMERCATOR integration tests', () => {
 
     it ('Returns the proper version', async () => {
         const query = `SELECT \`${BQ_PROJECTID}\`.\`${BQ_DATASET_SPHERICALMERCATOR}\`.VERSION() as versioncol;`;
-
-        const options = {
-            query: query
-        };
-
         let rows;
         await assert.doesNotReject( async () => {
             const [job] = await client.createQueryJob({ query: query });
@@ -31,5 +26,45 @@ describe('SPHERICALMERCATOR integration tests', () => {
         });
         assert.equal(rows.length, 1);
         assert.equal(rows[0].versioncol, 1);
+    });
+
+    it ('BBOX returns the proper value', async () => {
+        let query = `SELECT \`${BQ_PROJECTID}\`.\`${BQ_DATASET_SPHERICALMERCATOR}\`.BBOX(0, 0, 0, 256) as bbox;`;
+        let rows;
+        await assert.doesNotReject( async () => {
+            const [job] = await client.createQueryJob({ query: query });
+            [rows] = await job.getQueryResults();
+        });
+        assert.equal(rows.length, 1);
+        assert.deepEqual(rows[0].bbox, [-180, -85.05112877980659, 180, 85.0511287798066])
+
+        query = `SELECT \`${BQ_PROJECTID}\`.\`${BQ_DATASET_SPHERICALMERCATOR}\`.BBOX(0, 0, 1, 256) as bbox;`;
+        await assert.doesNotReject( async () => {
+            const [job] = await client.createQueryJob({ query: query });
+            [rows] = await job.getQueryResults();
+        });
+        assert.equal(rows.length, 1);
+        assert.deepEqual(rows[0].bbox, [-180, 0, 0, 85.0511287798066]);
+    });
+
+    it ('XYZ returns the proper value', async () => {
+        let bbox = [-180, -85.05112877980659, 180, 85.0511287798066];
+        let query = `SELECT \`${BQ_PROJECTID}\`.\`${BQ_DATASET_SPHERICALMERCATOR}\`.XYZ([${bbox}], 0, 256) as xyz;`;
+        let rows;
+        await assert.doesNotReject( async () => {
+            const [job] = await client.createQueryJob({ query: query});
+            [rows] = await job.getQueryResults();
+        });
+        assert.equal(rows.length, 1);
+        assert.deepEqual(rows[0].xyz, { minX: 0, minY: 0, maxX: 0, maxY: 0 })
+
+        bbox = [-180, 0, 0, 85.0511287798066];
+        query = `SELECT \`${BQ_PROJECTID}\`.\`${BQ_DATASET_SPHERICALMERCATOR}\`.XYZ([${bbox}], 1, 256) as xyz;`;
+        await assert.doesNotReject( async () => {
+            const [job] = await client.createQueryJob({ query: query});
+            [rows] = await job.getQueryResults();
+        });
+        assert.equal(rows.length, 1);
+        assert.deepEqual(rows[0].xyz, { minX: 0, minY: 0, maxX: 0, maxY: 0 });
     });
 }); /* SPHERICALMERCATOR integration tests */
