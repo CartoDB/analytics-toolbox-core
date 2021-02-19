99539d54a9bd676f6217c45e9ed7669bb2ceaa92
vdelacruzb <vdelacruzb@gmail.com>
2021-02-19 15:52:22 +0100

    group functions

--- turf/bq/sql/ST_BUFFER.sql
+++ turf/bq/sql/ST_BUFFER.sql
@@ -4,6 +4,17 @@
 --
 -----------------------------------------------------------------------
 
+CREATE OR REPLACE FUNCTION `@@BQ_PROJECTID@@.@@BQ_DATASET_TURF@@.BUFFER`
+    (geojson STRING,radius NUMERIC, options STRUCT<unit STRING,steps NUMERIC>)
+    RETURNS STRING
+    DETERMINISTIC
+    LANGUAGE js
+    OPTIONS (library=["@@TURF_BQ_LIBRARY@@"])
+AS """
+    var buffer = turf.buffer(JSON.parse(geojson),radius,{'unit':options.unit,'steps':options.steps});
+    return JSON.stringify(buffer.geometry);
+""";
+
 CREATE OR REPLACE FUNCTION `@@BQ_PROJECTID@@.@@BQ_DATASET_TURF@@.ST_BUFFER`
     (geojson GEOGRAPHY, radius NUMERIC, units STRING, steps NUMERIC)
 AS (
