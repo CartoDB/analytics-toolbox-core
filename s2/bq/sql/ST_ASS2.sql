373c6c936e69369ab82ea896d56a61ce105399ba
vdelacruzb <vdelacruzb@gmail.com>
2021-02-19 15:55:35 +0100

    group functions

--- s2/bq/sql/ST_ASS2.sql
+++ s2/bq/sql/ST_ASS2.sql
@@ -4,6 +4,13 @@
 --
 -----------------------------------------------------------------------
 
+CREATE OR REPLACE FUNCTION `@@BQ_PROJECTID@@.@@BQ_DATASET_S2@@.LONGLAT_ASS2`
+    (longitude FLOAT64, latitude FLOAT64, resolution NUMERIC)
+AS (
+    (SELECT STRING_AGG(FORMAT('%02x', CAST(`@@BQ_PROJECTID@@`.@@BQ_DATASET_S2@@.ID_FROMLONGLAT(longitude, latitude, resolution) AS INT64) >> (byte * 8) & 0xff), '' ORDER BY byte DESC)
+    FROM UNNEST(GENERATE_ARRAY(0, 7)) AS byte)
+);
+
 CREATE OR REPLACE FUNCTION `@@BQ_PROJECTID@@.@@BQ_DATASET_S2@@.ST_ASS2`
     (point GEOGRAPHY, resolution NUMERIC)
 AS (
