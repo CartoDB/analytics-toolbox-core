a00c49eb315326ef5c68ad3248d0234b9c93ee21
vdelacruzb <vdelacruzb@gmail.com>
2021-03-02 09:51:40 +0100

    update test ranges

--- quadkey/library/test/quadkey_test.js
+++ quadkey/library/test/quadkey_test.js
@@ -10,8 +10,12 @@ describe('QUADKEY unit tests', () => {
 
     it('Should be able to encode/decode tiles at any level of zoom', async() => {
         let tilesPerLevel, x, y, xDecoded, yDecoded, zDecoded;
-        for (let z = 1; z < 30; ++z) {
-            tilesPerLevel = 2 << (z - 1);
+        for (let z = 0; z < 30; ++z) {
+            if (z === 0) {
+                tilesPerLevel = 1;
+            } else {
+                tilesPerLevel = 2 << (z - 1);
+            }
 
             x = 0;
             y = 0;
@@ -21,28 +25,34 @@ describe('QUADKEY unit tests', () => {
             yDecoded = zxyDecoded.y;
             assert.ok(z === zDecoded && x === xDecoded && y === yDecoded);
 
-            x = tilesPerLevel / 2;
-            y = tilesPerLevel / 2;
-            zxyDecoded = ZXYFromQuadint(quadintFromZXY(z, x, y));
-            zDecoded = zxyDecoded.z;
-            xDecoded = zxyDecoded.x;
-            yDecoded = zxyDecoded.y;
-            assert.ok(z === zDecoded && x === xDecoded && y === yDecoded);
+            if (z > 0) {
+                x = tilesPerLevel / 2;
+                y = tilesPerLevel / 2;
+                zxyDecoded = ZXYFromQuadint(quadintFromZXY(z, x, y));
+                zDecoded = zxyDecoded.z;
+                xDecoded = zxyDecoded.x;
+                yDecoded = zxyDecoded.y;
+                assert.ok(z === zDecoded && x === xDecoded && y === yDecoded);
 
-            x = tilesPerLevel - 1;
-            y = tilesPerLevel - 1;
-            zxyDecoded = ZXYFromQuadint(quadintFromZXY(z, x, y));
-            zDecoded = zxyDecoded.z;
-            xDecoded = zxyDecoded.x;
-            yDecoded = zxyDecoded.y;
-            assert.ok(z === zDecoded && x === xDecoded && y === yDecoded);
+                x = tilesPerLevel - 1;
+                y = tilesPerLevel - 1;
+                zxyDecoded = ZXYFromQuadint(quadintFromZXY(z, x, y));
+                zDecoded = zxyDecoded.z;
+                xDecoded = zxyDecoded.x;
+                yDecoded = zxyDecoded.y;
+                assert.ok(z === zDecoded && x === xDecoded && y === yDecoded);
+            }
         }
     });
 
     it('Should be able to encode/decode between quadint and quadkey at any level of zoom', async() => {
         let tilesPerLevel, x, y, xDecoded, yDecoded, zDecoded;
-        for (let z = 1; z < 30; ++z) {
-            tilesPerLevel = 2 << (z - 1);
+        for (let z = 0; z < 30; ++z) {
+            if (z === 0) {
+                tilesPerLevel = 1;
+            } else {
+                tilesPerLevel = 2 << (z - 1);
+            }
 
             x = 0;
             y = 0;
@@ -52,27 +62,29 @@ describe('QUADKEY unit tests', () => {
             yDecoded = zxyDecoded.y;
             assert.ok(z === zDecoded && x === xDecoded && y === yDecoded);
 
-            x = tilesPerLevel / 2;
-            y = tilesPerLevel / 2;
-            zxyDecoded = ZXYFromQuadint(quadintFromQuadkey(quadkeyFromQuadint(quadintFromZXY(z, x, y))));
-            zDecoded = zxyDecoded.z;
-            xDecoded = zxyDecoded.x;
-            yDecoded = zxyDecoded.y;
-            assert.ok(z === zDecoded && x === xDecoded && y === yDecoded);
+            if (z > 0) {
+                x = tilesPerLevel / 2;
+                y = tilesPerLevel / 2;
+                zxyDecoded = ZXYFromQuadint(quadintFromQuadkey(quadkeyFromQuadint(quadintFromZXY(z, x, y))));
+                zDecoded = zxyDecoded.z;
+                xDecoded = zxyDecoded.x;
+                yDecoded = zxyDecoded.y;
+                assert.ok(z === zDecoded && x === xDecoded && y === yDecoded);
 
-            x = tilesPerLevel - 1;
-            y = tilesPerLevel - 1;
-            zxyDecoded = ZXYFromQuadint(quadintFromQuadkey(quadkeyFromQuadint(quadintFromZXY(z, x, y))));
-            zDecoded = zxyDecoded.z;
-            xDecoded = zxyDecoded.x;
-            yDecoded = zxyDecoded.y;
-            assert.ok(z === zDecoded && x === xDecoded && y === yDecoded);
+                x = tilesPerLevel - 1;
+                y = tilesPerLevel - 1;
+                zxyDecoded = ZXYFromQuadint(quadintFromQuadkey(quadkeyFromQuadint(quadintFromZXY(z, x, y))));
+                zDecoded = zxyDecoded.z;
+                xDecoded = zxyDecoded.x;
+                yDecoded = zxyDecoded.y;
+                assert.ok(z === zDecoded && x === xDecoded && y === yDecoded);
+            }
         }
     });
 
     it('Inside should work at any level of zoom', async() => {
         let z, lat, lng;
-        for (z = 1; z < 30; ++z) {
+        for (z = 0; z < 30; ++z) {
             for (lat = -90; lat <= 90; lat = lat + 15) {
                 for (lng = -180; lng <= 180; lng = lng + 15) {
                     const location = { lng: lng, lat: lat };
@@ -98,7 +110,7 @@ describe('QUADKEY unit tests', () => {
 
     it('Parent should work at any level of zoom', async() => {
         let z, lat, lng;
-        for (z = 2; z < 30; ++z) {
+        for (z = 1; z < 30; ++z) {
             for (lat = -90; lat <= 90; lat = lat + 15) {
                 for (lng = -180; lng <= 180; lng = lng + 15) {
                     const location = { lng: lng, lat: lat };
@@ -112,7 +124,7 @@ describe('QUADKEY unit tests', () => {
 
     it('Children should work at any level of zoom', async() => {
         let z, lat, lng, cont;
-        for (z = 1; z < 29; ++z) {
+        for (z = 0; z < 29; ++z) {
             for (lat = -90; lat <= 90; lat = lat + 15) {
                 for (lng = -180; lng <= 180; lng = lng + 15) {
                     const location = { lng: lng, lat: lat };
