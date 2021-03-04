c6d41e6eb3cbbb081ea36b0e8ffed73d9ffd92d0
vdelacruzb <vdelacruzb@gmail.com>
2021-03-04 18:35:24 +0100

    group functions

--- s2/bq/sql/HILBERTQUADKEY.sql
+++ s2/bq/sql/HILBERTQUADKEY.sql
@@ -4,6 +4,21 @@
 --
 -----------------------------------------------------------------------
 
+CREATE OR REPLACE FUNCTION `@@BQ_PROJECTID@@.@@BQ_DATASET_S2@@.ID_FROMHILBERTQUADKEY`
+    (quadkey STRING)
+    RETURNS BIGNUMERIC
+    DETERMINISTIC
+    LANGUAGE js
+    OPTIONS (library=["@@S2_BQ_LIBRARY@@"])
+AS """
+    if(!quadkey)
+    {
+        throw new Error('NULL argument passed to UDF');
+    }
+
+    return S2.keyToId(quadkey).toString();
+""";
+
 CREATE OR REPLACE FUNCTION `@@BQ_PROJECTID@@.@@BQ_DATASET_S2@@.HILBERTQUADKEY_FROMID`
     (id BIGNUMERIC)
     RETURNS STRING
--- s2/bq/sql/LONGLAT_FROMID.sql
+++ s2/bq/sql/LONGLAT_FROMID.sql
@@ -18,3 +18,18 @@ AS """
 
     return S2.idToLatLng(id)["lng"];
 """;
+
+CREATE OR REPLACE FUNCTION `@@BQ_PROJECTID@@.@@BQ_DATASET_S2@@.LAT_FROMID`
+    (id BIGNUMERIC)
+    RETURNS FLOAT64
+    DETERMINISTIC
+    LANGUAGE js
+    OPTIONS (library=["@@S2_BQ_LIBRARY@@"])
+AS """
+    if(id == null)
+    {
+        throw new Error('NULL argument passed to UDF');
+    }
+
+    return S2.idToLatLng(id)["lat"];
+""";
