d9a39924f1dd97983a2dfb24ed8d761902f34356
vdelacruzb <vdelacruzb@gmail.com>
2021-03-05 09:25:22 +0100

    update test

--- s2/library/test/s2_test.js
+++ s2/library/test/s2_test.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
 const assert = require('assert').strict;
-const chaiAssert = require('chai').assert;
 /* Emulate how BigQuery would load the file */
 global.eval(fs.readFileSync('../../s2_library.js') + '');
 
@@ -10,30 +9,18 @@ describe('S2 unit tests', () => {
     });
 
     it('Quadkey/S2 id Conversions', async() => {
-        let level = 10;
-        let latitude = 10;
-        let longitude = -20;
-        const quadkey = S2.latLngToKey(latitude, longitude, level);
-        assert.equal(quadkey, S2.idToKey(S2.keyToId(quadkey)));
-
-        level = 11;
-        latitude = 15;
-        longitude = -25;
-        const s2Id = S2.latLngToId(latitude, longitude, level);
-        assert.equal(s2Id, S2.keyToId(S2.idToKey(s2Id)));
-    });
-
-    it('Long Lat id Conversions', async() => {
-        const level = 18;
-        const latitude = -14;
-        const longitude = 125;
-        const quadkey = S2.latLngToKey(latitude, longitude, level);
-        const s2Id = S2.latLngToId(latitude, longitude, level);
-        assert.equal(quadkey, S2.idToKey(s2Id));
-        chaiAssert.closeTo(-14, S2.idToLatLng(s2Id).lat, 0.001);
-        chaiAssert.closeTo(-14, S2.keyToLatLng(quadkey).lat, 0.001);
-        chaiAssert.closeTo(125, S2.idToLatLng(s2Id).lng, 0.001);
-        chaiAssert.closeTo(125, S2.keyToLatLng(quadkey).lng, 0.001);
+        let z, lat, lng;
+        for (z = 1; z < 30; ++z) {
+            for (lat = -89; lat <= 89; lat = lat + 15) {
+                for (lng = -179; lng <= 179; lng = lng + 15) {
+                    const quadkey = S2.latLngToKey(lat, lng, z);
+                    assert.equal(quadkey, S2.idToKey(S2.keyToId(quadkey)));
+                    
+                    const s2Id = S2.latLngToId(lat, lng, z);
+                    assert.equal(s2Id, S2.keyToId(S2.idToKey(s2Id)));
+                }
+            }    
+        }
     });
 
     it('Boundary check', async() => {
