788332474b38d3276ffc064febd349721b36ebc3
vdelacruzb <vdelacruzb@gmail.com>
2021-03-11 10:02:13 +0100

    update function and tests

--- quadkey/bq/sql/KRING.sql
+++ quadkey/bq/sql/KRING.sql
@@ -11,20 +11,14 @@ CREATE OR REPLACE FUNCTION `@@BQ_PROJECTID@@.@@BQ_DATASET_QUADKEY@@.KRING`
     LANGUAGE js
     OPTIONS (library=["@@QUADKEY_BQ_LIBRARY@@"])
 AS """
-    if(quadint == null)
+if(quadint == null)
     {
         throw new Error('NULL argument passed to UDF');
     }
     if(distance == null)
     {
-        distance = 0;
+        distance = 1;
     }
-    let neighbors = [sibling(quadint,'up').toString()];
-    let moves = ['left','down','right','up'];
-    var i;
-    for (i = 1; i < (1+2*distance)*(1+2*distance)-1; i++) {
-        neighbors.push(sibling(neighbors[neighbors.length-1],moves[Math.floor(i/2)%4]).toString());
-    }
-    neighbors.push(quadint.toString());
-    return neighbors;
+    let neighbors = kring(quadint, distance);
+    return neighbors.map(String);
 """;
