bd7fb4e7cfe7fa01ed331c15304f75ae86a7c478
vdelacruzb <vdelacruzb@gmail.com>
2021-03-11 11:04:19 +0100

    update kring

--- quadkey/sf/sql/KRING.sql
+++ quadkey/sf/sql/KRING.sql
@@ -5,7 +5,7 @@
 -----------------------------------------------------------------------
 
 CREATE OR REPLACE FUNCTION @@SF_DATABASEID@@.@@SF_SCHEMA_QUADKEY@@.KRING
-    (quadint DOUBLE)
+    (quadint DOUBLE, distance DOUBLE)
     RETURNS ARRAY
     LANGUAGE JAVASCRIPT
 AS $$
@@ -15,21 +15,18 @@ AS $$
     {
         throw new Error('NULL argument passed to UDF');
     }
-    var left      = sibling(QUADINT,'left');
-    var topleft   = sibling(left,'up');
-    var downleft  = sibling(left,'down');
-    var right     = sibling(QUADINT,'right');
-    var topright  = sibling(right,'up');
-    var downright = sibling(right,'down');
-    var up        = sibling(QUADINT,'up');
-    var down      = sibling(QUADINT,'down');
 
-    return [left, topleft, downleft, right, topright, downright, up, down, QUADINT];
+    if(DISTANCE == null)
+    {
+        DISTANCE = 1;
+    }
+
+    return kring(QUADINT, DISTANCE);
 $$;
 
 CREATE OR REPLACE SECURE FUNCTION @@SF_DATABASEID@@.@@SF_SCHEMA_QUADKEY@@.KRING
-    (quadint INT)
+    (quadint INT, distance INT)
     RETURNS ARRAY
 AS $$
-    @@SF_DATABASEID@@.@@SF_SCHEMA_QUADKEY@@.KRING(CAST(QUADINT AS DOUBLE))
+    @@SF_DATABASEID@@.@@SF_SCHEMA_QUADKEY@@.KRING(CAST(QUADINT AS DOUBLE), CAST(DISTANCE AS DOUBLE))
 $$;
