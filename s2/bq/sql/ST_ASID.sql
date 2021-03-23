e45a35f8c92d5250cb03676b5f91d22287739464
vdelacruzb <vdelacruzb@gmail.com>
2021-03-23 17:55:18 +0100

    fix s2 function

--- s2/bq/sql/ST_ASID.sql
+++ s2/bq/sql/ST_ASID.sql
@@ -11,7 +11,7 @@ CREATE OR REPLACE FUNCTION `@@BQ_PROJECTID@@.@@BQ_DATASET_S2@@.LONGLAT_ASID`
     LANGUAGE js
     OPTIONS (library=["@@S2_BQ_LIBRARY@@"])
 AS """
-    if(longitude == null || longitude == null || longitude == null)
+    if(latitude == null || longitude == null || level == null)
     {
         throw new Error('NULL argument passed to UDF');
     }
