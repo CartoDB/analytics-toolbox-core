fc55f979d5591a37ebb83e94d58f1cea5e76abda
vdelacruzb <vdelacruzb@gmail.com>
2021-04-05 14:50:39 +0200

    fix as_h3 and integration test

--- h3/sf/sql/AS_H3.sql
+++ h3/sf/sql/AS_H3.sql
@@ -10,7 +10,7 @@ CREATE OR REPLACE FUNCTION @@SF_DATABASEID@@.@@SF_SCHEMA_H3@@._LONGLAT_ASH3(long
 AS $$
     @@LIBRARY_FILE_CONTENT@@
 
-    if (LONGITUDE === null || LATITUDE === null || RESOLUTION === null) {
+    if (LONGITUDE == null || LATITUDE == null || RESOLUTION == null) {
         return null;
     }
     const index = h3.geoToH3(Number(LATITUDE), Number(LONGITUDE), Number(RESOLUTION));
@@ -23,13 +23,15 @@ $$;
 CREATE OR REPLACE FUNCTION @@SF_DATABASEID@@.@@SF_SCHEMA_H3@@.LONGLAT_ASH3(longitude DOUBLE, latitude DOUBLE, resolution INT)
 RETURNS BIGINT
 AS $$
-    CAST(@@SF_DATABASEID@@.@@SF_SCHEMA_H3@@._LONGLAT_ASH3(LONGITUDE, LATITUDE, CAST(RESOLUTION AS DOUBLE)) AS BIGINT)
+    @@SF_DATABASEID@@.@@SF_SCHEMA_H3@@.H3_FROMHEX(@@SF_DATABASEID@@.@@SF_SCHEMA_H3@@._LONGLAT_ASH3(LONGITUDE, LATITUDE, CAST(RESOLUTION AS DOUBLE)))
 $$;
 
 CREATE OR REPLACE FUNCTION @@SF_DATABASEID@@.@@SF_SCHEMA_H3@@.ST_ASH3(geog GEOGRAPHY, resolution INT)
     RETURNS BIGINT
 AS $$
-    @@SF_DATABASEID@@.@@SF_SCHEMA_H3@@.LONGLAT_ASH3(SAFE.ST_X(GEOG), SAFE.ST_Y(GEOG), CAST(RESOLUTION AS DOUBLE)
+    IFF(ST_NPOINTS(geog) = 1, 
+        @@SF_DATABASEID@@.@@SF_SCHEMA_H3@@.LONGLAT_ASH3(ST_X(GEOG), ST_Y(GEOG), CAST(RESOLUTION AS DOUBLE)),
+        null)
 $$;
 
 CREATE OR REPLACE FUNCTION @@SF_DATABASEID@@.@@SF_SCHEMA_H3@@._ST_ASH3_POLYFILL(geojson STRING, _resolution DOUBLE)
@@ -39,17 +41,17 @@ AS $$
     @@LIBRARY_FILE_CONTENT@@
 
     if (!GEOJSON || _RESOLUTION == null) {
-        return null;
+        return [];
     }
 
     const resolution = Number(_RESOLUTION);
     if (resolution < 0 || resolution > 15) {
-        return null;
+        return [];
     }
 
     const featureGeometry = JSON.parse(GEOJSON)
     if (!['Polygon', 'MultiPolygon'].includes(featureGeometry.type)) {
-        return null;
+        return [];
     }
 
     const polygonCoordinates =  featureGeometry.type === 'MultiPolygon' ? featureGeometry.coordinates : [featureGeometry.coordinates];
--- h3/sf/test/ash3_integration.js
+++ h3/sf/test/ash3_integration.js
@@ -55,15 +55,15 @@ describe('*_ASH3 integration tests', () => {
         const query = `
 WITH inputs AS
 (
-    SELECT 1 AS id, ST_GEOGPOINT(-122.0553238, 37.3615593) as geom, 5 as resolution UNION ALL
-    SELECT 2 AS id, ST_GEOGPOINT(-164.991559, 30.943387) as geom, 5 as resolution UNION ALL
-    SELECT 3 AS id, ST_GEOGPOINT(71.52790329909925, 46.04189431883772) as geom, 15 as resolution UNION ALL
+    SELECT 1 AS id, ST_POINT(-122.0553238, 37.3615593) as geom, 5 as resolution UNION ALL
+    SELECT 2 AS id, ST_POINT(-164.991559, 30.943387) as geom, 5 as resolution UNION ALL
+    SELECT 3 AS id, ST_POINT(71.52790329909925, 46.04189431883772) as geom, 15 as resolution UNION ALL
 
     -- null inputs
-    SELECT 4 AS id, NULL AS geom, 5 as resolution UNION ALL
-    SELECT 5 AS id, ST_GEOGPOINT(-122.0553238, 37.3615593) as geom, -1 as resolution UNION ALL
-    SELECT 6 AS id, ST_GEOGPOINT(-122.0553238, 37.3615593) as geom, 20 as resolution UNION ALL
-    SELECT 7 AS id, ST_GEOGPOINT(-122.0553238, 37.3615593) as geom, NULL as resolution
+    SELECT 4 AS id, TRY_TO_GEOGRAPHY(NULL) AS geom, 5 as resolution UNION ALL
+    SELECT 5 AS id, ST_POINT(-122.0553238, 37.3615593) as geom, -1 as resolution UNION ALL
+    SELECT 6 AS id, ST_POINT(-122.0553238, 37.3615593) as geom, 20 as resolution UNION ALL
+    SELECT 7 AS id, ST_POINT(-122.0553238, 37.3615593) as geom, NULL as resolution
 )
 SELECT
     CAST(${SF_DATABASEID}.${SF_SCHEMA_H3}.ST_ASH3(geom, resolution) AS STRING) as h3_id
@@ -75,22 +75,22 @@ ORDER BY id ASC`;
             [statement, rows] = await execAsync(connection, query);
         });
         assert.equal(rows.length, 7);
-        assert.equal(rows[0].h3_id, "599686042433355775");
-        assert.equal(rows[1].h3_id, "600235711274156031");
-        assert.equal(rows[2].h3_id, "644577696667402240");
-        assert.equal(rows[3].h3_id, null);
-        assert.equal(rows[4].h3_id, null);
-        assert.equal(rows[5].h3_id, null);
-        assert.equal(rows[6].h3_id, null);
+        assert.equal(rows[0].H3_ID, "599686042433355775");
+        assert.equal(rows[1].H3_ID, "600235711274156031");
+        assert.equal(rows[2].H3_ID, "644577696667402240");
+        assert.equal(rows[3].H3_ID, null);
+        assert.equal(rows[4].H3_ID, null);
+        assert.equal(rows[5].H3_ID, null);
+        assert.equal(rows[6].H3_ID, null);
     });
 
     it ('ST_ASH3 returns NULL with non POINT geographies', async () => {
         const query = `
 WITH inputs AS
 (
-    SELECT 1 AS id, ST_GEOGFROMTEXT('LINESTRING(0 0, 10 10)') as geom, 5 as resolution UNION ALL
-    SELECT 2 AS id, ST_GEOGFROMTEXT('POLYGON((0 0, 0 10, 10 10, 10 0, 0 0))') as geom, 5 as resolution UNION ALL
-    SELECT 3 AS id, ST_GEOGFROMTEXT('MULTIPOINT(0 0, 0 10, 10 10, 10 0, 0 0)') as geom, 5 as resolution
+    SELECT 1 AS id, TO_GEOGRAPHY('LINESTRING(0 0, 10 10)') as geom, 5 as resolution UNION ALL
+    SELECT 2 AS id, TO_GEOGRAPHY('POLYGON((0 0, 0 10, 10 10, 10 0, 0 0))') as geom, 5 as resolution UNION ALL
+    SELECT 3 AS id, TO_GEOGRAPHY('MULTIPOINT(0 0, 0 10, 10 10, 10 0, 0 0)') as geom, 5 as resolution
 )
 SELECT
     CAST(${SF_DATABASEID}.${SF_SCHEMA_H3}.ST_ASH3(geom, resolution) AS STRING) as h3_id
@@ -100,9 +100,9 @@ ORDER BY id ASC`;
         await assert.doesNotReject(async () => {
             [statement, rows] = await execAsync(connection, query);
         });
-        assert.equal(rows[0].h3_id, null);
-        assert.equal(rows[1].h3_id, null);
-        assert.equal(rows[2].h3_id, null);
+        assert.equal(rows[0].H3_ID, null);
+        assert.equal(rows[1].H3_ID, null);
+        assert.equal(rows[2].H3_ID, null);
     });
 
     it ('LONGLAT_ASH3 returns the proper INT64', async () => {
@@ -133,45 +133,45 @@ ORDER BY id ASC`;
         });
 
         assert.equal(rows.length, 8);
-        assert.equal(rows[0].h3_id, "599686042433355775");
-        assert.equal(rows[1].h3_id, "600235711274156031");
-        assert.equal(rows[2].h3_id, "644577696667402240");
-        assert.equal(rows[3].h3_id, null);
-        assert.equal(rows[4].h3_id, null);
-        assert.equal(rows[5].h3_id, null);
-        assert.equal(rows[6].h3_id, '599686042433355775');
-        assert.equal(rows[7].h3_id, '599686042433355775');
+        assert.equal(rows[0].H3_ID, "599686042433355775");
+        assert.equal(rows[1].H3_ID, "600235711274156031");
+        assert.equal(rows[2].H3_ID, "644577696667402240");
+        assert.equal(rows[3].H3_ID, null);
+        assert.equal(rows[4].H3_ID, null);
+        assert.equal(rows[5].H3_ID, null);
+        assert.equal(rows[6].H3_ID, '599686042433355775');
+        assert.equal(rows[7].H3_ID, '599686042433355775');
     });
 
     it ('ST_ASH3_POLYFILL returns the proper INT64s', async () => {
         const query = `
 WITH inputs AS
 (
-    SELECT 1 AS id, ST_GEOGFROMTEXT('POLYGON((-122.4089866999972145 37.813318999983238, -122.3805436999997056 37.7866302000007224, -122.3544736999993603 37.7198061999978478, -122.5123436999983966 37.7076131999975672, -122.5247187000021967 37.7835871999971715, -122.4798767000009008 37.8151571999998453, -122.4089866999972145 37.813318999983238))') as geom, 9 as resolution UNION ALL
-    SELECT 2 AS id, ST_GEOGFROMTEXT('POLYGON((0 0, 0 10, 10 10, 10 0, 0 0))') as geom, 2 as resolution UNION ALL
-    SELECT 3 AS id, ST_GEOGFROMTEXT('POLYGON((20 20, 20 30, 30 30, 30 20, 20 20))') as geom, 2 as resolution UNION ALL
+    SELECT 1 AS id, TO_GEOGRAPHY('POLYGON((-122.4089866999972145 37.813318999983238, -122.3805436999997056 37.7866302000007224, -122.3544736999993603 37.7198061999978478, -122.5123436999983966 37.7076131999975672, -122.5247187000021967 37.7835871999971715, -122.4798767000009008 37.8151571999998453, -122.4089866999972145 37.813318999983238))') as geom, 9 as resolution UNION ALL
+    SELECT 2 AS id, TO_GEOGRAPHY('POLYGON((0 0, 0 10, 10 10, 10 0, 0 0))') as geom, 2 as resolution UNION ALL
+    SELECT 3 AS id, TO_GEOGRAPHY('POLYGON((20 20, 20 30, 30 30, 30 20, 20 20))') as geom, 2 as resolution UNION ALL
     -- 4 is a multipolygon containing 2 + 3
-    SELECT 4 AS id, ST_GEOGFROMTEXT('MULTIPOLYGON(((0 0, 0 10, 10 10, 10 0, 0 0)), ((20 20, 20 30, 30 30, 30 20, 20 20)))') as geom, 2 as resolution UNION ALL
+    SELECT 4 AS id, TO_GEOGRAPHY('MULTIPOLYGON(((0 0, 0 10, 10 10, 10 0, 0 0)), ((20 20, 20 30, 30 30, 30 20, 20 20)))') as geom, 2 as resolution UNION ALL
 
     -- NULL and empty
-    SELECT 5 AS id, NULL as geom, 2 as resolution UNION ALL
-    SELECT 6 AS id, ST_GEOGFROMTEXT('POLYGON EMPTY') as geom, 2 as resolution UNION ALL
+    SELECT 5 AS id, TRY_TO_GEOGRAPHY(NULL) as geom, 2 as resolution UNION ALL
+    SELECT 6 AS id, TO_GEOGRAPHY('POLYGON EMPTY') as geom, 2 as resolution UNION ALL
 
     -- Invalid resolution
-    SELECT 7 AS id, ST_GEOGFROMTEXT('POLYGON((0 0, 0 10, 10 10, 10 0, 0 0))') as geom, -1 as resolution UNION ALL
-    SELECT 8 AS id, ST_GEOGFROMTEXT('POLYGON((0 0, 0 10, 10 10, 10 0, 0 0))') as geom, 20 as resolution UNION ALL
-    SELECT 9 AS id, ST_GEOGFROMTEXT('POLYGON((0 0, 0 10, 10 10, 10 0, 0 0))') as geom, NULL as resolution UNION ALL
+    SELECT 7 AS id, TO_GEOGRAPHY('POLYGON((0 0, 0 10, 10 10, 10 0, 0 0))') as geom, -1 as resolution UNION ALL
+    SELECT 8 AS id, TO_GEOGRAPHY('POLYGON((0 0, 0 10, 10 10, 10 0, 0 0))') as geom, 20 as resolution UNION ALL
+    SELECT 9 AS id, TO_GEOGRAPHY('POLYGON((0 0, 0 10, 10 10, 10 0, 0 0))') as geom, NULL as resolution UNION ALL
 
     -- Other types are not supported
-    SELECT 10 AS id, ST_GEOGFROMTEXT('POINT(0 0)') as geom, 15 as resolution UNION ALL
-    SELECT 11 AS id, ST_GEOGFROMTEXT('MULTIPOINT(0 0, 1 1)') as geom, 15 as resolution UNION ALL
-    SELECT 12 AS id, ST_GEOGFROMTEXT('LINESTRING(0 0, 1 1)') as geom, 15 as resolution UNION ALL
-    SELECT 13 AS id, ST_GEOGFROMTEXT('MULTILINESTRING((0 0, 1 1), (2 2, 3 3))') as geom, 15 as resolution UNION ALL
-    SELECT 14 AS id, ST_GEOGFROMTEXT('GEOMETRYCOLLECTION(POINT(0 0), LINESTRING(1 2, 2 1))') as geom, 15 as resolution
+    SELECT 10 AS id, TO_GEOGRAPHY('POINT(0 0)') as geom, 15 as resolution UNION ALL
+    SELECT 11 AS id, TO_GEOGRAPHY('MULTIPOINT(0 0, 1 1)') as geom, 15 as resolution UNION ALL
+    SELECT 12 AS id, TO_GEOGRAPHY('LINESTRING(0 0, 1 1)') as geom, 15 as resolution UNION ALL
+    SELECT 13 AS id, TO_GEOGRAPHY('MULTILINESTRING((0 0, 1 1), (2 2, 3 3))') as geom, 15 as resolution UNION ALL
+    SELECT 14 AS id, TO_GEOGRAPHY('GEOMETRYCOLLECTION(POINT(0 0), LINESTRING(1 2, 2 1))') as geom, 15 as resolution
 
 )
 SELECT
-    ARRAY_LENGTH(${SF_DATABASEID}.${SF_SCHEMA_H3}.ST_ASH3_POLYFILL(geom, resolution)) AS id_count
+    ARRAY_SIZE(${SF_DATABASEID}.${SF_SCHEMA_H3}.ST_ASH3_POLYFILL(geom, resolution)) AS id_count
 FROM inputs
 ORDER BY id ASC`;
 
@@ -180,52 +180,57 @@ ORDER BY id ASC`;
             [statement, rows] = await execAsync(connection, query);
         });
         assert.equal(rows.length, 14);
-        assert.equal(rows[0].id_count, 1253);
-        assert.equal(rows[1].id_count, 18);
-        assert.equal(rows[2].id_count, 12);
-        assert.equal(rows[3].id_count, 30);
-        assert.equal(rows[4].id_count, null);
-        assert.equal(rows[5].id_count, null);
-        assert.equal(rows[6].id_count, null);
-        assert.equal(rows[7].id_count, null);
-        assert.equal(rows[8].id_count, null);
-        assert.equal(rows[9].id_count, null);
-        assert.equal(rows[10].id_count, null);
-        assert.equal(rows[11].id_count, null);
-        assert.equal(rows[12].id_count, null);
-        assert.equal(rows[13].id_count, null);
+        assert.equal(rows[0].ID_COUNT, 1253);
+        assert.equal(rows[1].ID_COUNT, 18);
+        assert.equal(rows[2].ID_COUNT, 12);
+        assert.equal(rows[3].ID_COUNT, 30);
+        assert.equal(rows[4].ID_COUNT, 0);
+        assert.equal(rows[5].ID_COUNT, 0);
+        assert.equal(rows[6].ID_COUNT, 0);
+        assert.equal(rows[7].ID_COUNT, 0);
+        assert.equal(rows[8].ID_COUNT, 0);
+        assert.equal(rows[9].ID_COUNT, 0);
+        assert.equal(rows[10].ID_COUNT, 0);
+        assert.equal(rows[11].ID_COUNT, 0);
+        assert.equal(rows[12].ID_COUNT, 0);
+        assert.equal(rows[13].ID_COUNT, 0);
     });
 
     it ('ST_ASH3_POLYFILL returns the expected values', async () => {
         /* Any cell should cover only 1 h3 cell at its resolution (itself) */
-        const query = `
-WITH points AS
-(
-    SELECT ST_GEOGPOINT(0, 0) AS geog UNION ALL
-    SELECT ST_GEOGPOINT(-122.4089866999972145, 37.813318999983238) AS geog UNION ALL
-    SELECT ST_GEOGPOINT(-122.0553238, 37.3615593) AS geog
-),
-cells AS
-(
-    SELECT
-        resolution,
-        ${SF_DATABASEID}.${SF_SCHEMA_H3}.ST_ASH3(geog, resolution) AS h3_id,
-        ${SF_DATABASEID}.${SF_SCHEMA_H3}.ST_BOUNDARY(${SF_DATABASEID}.${SF_SCHEMA_H3}.ST_ASH3(geog, resolution)) AS boundary
-    FROM points, UNNEST(GENERATE_ARRAY(0, 15, 1)) resolution
-),
-polyfill AS
-(
-    SELECT
-        *,
-        ${SF_DATABASEID}.${SF_SCHEMA_H3}.ST_ASH3_POLYFILL(boundary, resolution) p
-    FROM cells
-)
-SELECT
-    *
-FROM  polyfill
-WHERE
-    ARRAY_LENGTH(p) != 1 OR
-    p[OFFSET(0)] != h3_id
+        /* This query has been splitted in Snowflake to avoid JS memory limits reached*/
+        let query = `
+        WITH points AS
+        (
+            SELECT ST_POINT(0, 0) AS geog
+        ),
+        resolution AS
+        (
+            SELECT seq4() AS resolution
+            FROM TABLE(generator(rowcount => 16))
+        ),
+        cells AS
+        (
+            SELECT
+                resolution,
+                ${SF_DATABASEID}.${SF_SCHEMA_H3}.H3_ASHEX(
+                    ${SF_DATABASEID}.${SF_SCHEMA_H3}.ST_ASH3(geog, resolution)) AS hex_id,
+                    ${SF_DATABASEID}.${SF_SCHEMA_H3}.ST_BOUNDARY(${SF_DATABASEID}.${SF_SCHEMA_H3}.ST_ASH3(geog, resolution)) AS boundary
+            FROM points, resolution
+        ),
+        polyfill AS
+        (
+            SELECT
+                *,
+                ${SF_DATABASEID}.${SF_SCHEMA_H3}.ST_ASH3_POLYFILL(boundary, resolution) p
+            FROM cells
+        )
+        SELECT
+            *
+        FROM  polyfill
+        WHERE
+            ARRAY_SIZE(p) != 1 OR
+            GET(p,0) != hex_id;
 `;
 
         let rows;
@@ -234,6 +239,84 @@ WHERE
         });
         assert.equal(rows.length, 0);
 
+        query = `
+        WITH points AS
+        (
+            SELECT ST_POINT(-122.4089866999972145, 37.813318999983238) AS geog
+        
+        ),
+        resolution AS
+        (
+            SELECT seq4() AS resolution
+            FROM TABLE(generator(rowcount => 16))
+        ),
+        cells AS
+        (
+            SELECT
+                resolution,
+                ${SF_DATABASEID}.${SF_SCHEMA_H3}.H3_ASHEX(
+                    ${SF_DATABASEID}.${SF_SCHEMA_H3}.ST_ASH3(geog, resolution)) AS hex_id,
+                    ${SF_DATABASEID}.${SF_SCHEMA_H3}.ST_BOUNDARY(${SF_DATABASEID}.${SF_SCHEMA_H3}.ST_ASH3(geog, resolution)) AS boundary
+            FROM points, resolution
+        ),
+        polyfill AS
+        (
+            SELECT
+                *,
+                ${SF_DATABASEID}.${SF_SCHEMA_H3}.ST_ASH3_POLYFILL(boundary, resolution) p
+            FROM cells
+        )
+        SELECT
+            *
+        FROM  polyfill
+        WHERE
+            ARRAY_SIZE(p) != 1 OR
+            GET(p,0) != hex_id;
+`;
+
+        await assert.doesNotReject(async () => {
+            [statement, rows] = await execAsync(connection, query);
+        });
+        assert.equal(rows.length, 0);
+
+        query = `
+        WITH points AS
+        (
+            SELECT ST_POINT(-122.0553238, 37.3615593) AS geog
+        ),
+        resolution AS
+        (
+            SELECT seq4() AS resolution
+            FROM TABLE(generator(rowcount => 16))
+        ),
+        cells AS
+        (
+            SELECT
+                resolution,
+                ${SF_DATABASEID}.${SF_SCHEMA_H3}.H3_ASHEX(
+                    ${SF_DATABASEID}.${SF_SCHEMA_H3}.ST_ASH3(geog, resolution)) AS hex_id,
+                    ${SF_DATABASEID}.${SF_SCHEMA_H3}.ST_BOUNDARY(${SF_DATABASEID}.${SF_SCHEMA_H3}.ST_ASH3(geog, resolution)) AS boundary
+            FROM points, resolution
+        ),
+        polyfill AS
+        (
+            SELECT
+                *,
+                ${SF_DATABASEID}.${SF_SCHEMA_H3}.ST_ASH3_POLYFILL(boundary, resolution) p
+            FROM cells
+        )
+        SELECT
+            *
+        FROM  polyfill
+        WHERE
+            ARRAY_SIZE(p) != 1 OR
+            GET(p,0) != hex_id;
+`;
+
+        await assert.doesNotReject(async () => {
+            [statement, rows] = await execAsync(connection, query);
+        });
+        assert.equal(rows.length, 0);
     });
 
 }); /* *_ASH3 integration tests */
