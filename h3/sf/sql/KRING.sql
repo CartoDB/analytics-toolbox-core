cda217d37f2ebbe7e8ed23dc3fd7dcce5f86f2f8
vdelacruzb <vdelacruzb@gmail.com>
2021-04-05 09:57:12 +0200

    fix and overload kring

--- h3/sf/sql/KRING.sql
+++ h3/sf/sql/KRING.sql
@@ -11,10 +11,10 @@ AS $$
     @@LIBRARY_FILE_CONTENT@@
 
     if (INDEX_LOWER == null || INDEX_UPPER == null || DISTANCE == null || DISTANCE < 0)
-        return null;
+        return [];
     const h3IndexInput = [Number(INDEX_LOWER), Number(INDEX_UPPER)];
     if (!h3.h3IsValid(h3IndexInput))
-        return null;
+        return [];
 
     return h3.kRing(h3IndexInput, parseInt(DISTANCE)).map(h => '0x' + h);
 $$;
@@ -26,4 +26,10 @@ AS $$
         CAST(BITAND(INDEX, 4294967295) AS DOUBLE), 
         CAST(BITSHIFTRIGHT(INDEX, 32) AS DOUBLE), 
         CAST(DISTANCE AS DOUBLE))
+$$;
+
+CREATE OR REPLACE FUNCTION @@SF_DATABASEID@@.@@SF_SCHEMA_H3@@.KRING(index STRING, distance INT)
+    RETURNS ARRAY
+AS $$
+    @@SF_DATABASEID@@.@@SF_SCHEMA_H3@@.KRING(@@SF_DATABASEID@@.@@SF_SCHEMA_H3@@.H3_FROMHEX(INDEX), DISTANCE)
 $$;
