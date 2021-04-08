4c53c48db18502e40384d13414d3f1dc8e81b3fb
vdelacruzb <vdelacruzb@gmail.com>
2021-04-08 09:26:06 +0200

    update bq s2 arguments

---
+++ s2/sf/sql/_SHARE_CREATE.sql
@@ -0,0 +1,5 @@
+USE @@SF_DATABASEID@@;
+CREATE SHARE IF NOT EXISTS @@SF_SHARE_PUBLIC@@;
+grant usage on database @@SF_DATABASEID@@ to share @@SF_SHARE_PUBLIC@@;
+grant usage on schema @@SF_DATABASEID@@.@@SF_SCHEMA_S2@@ to share @@SF_SHARE_PUBLIC@@;
+
