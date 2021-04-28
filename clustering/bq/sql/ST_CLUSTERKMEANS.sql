ccffeeb3f7bcb294afc21a81c672700c570eb60d
vdelacruzb <vdelacruzb@gmail.com>
2021-04-28 15:58:09 +0200

    update clusterkmeans

--- clustering/bq/sql/ST_CLUSTERKMEANS.sql
+++ clustering/bq/sql/ST_CLUSTERKMEANS.sql
@@ -5,7 +5,7 @@
 -----------------------------------------------------------------------
 
 CREATE OR REPLACE FUNCTION `@@BQ_PROJECTID@@.@@BQ_DATASET_CLUSTERING@@.__CLUSTERKMEANS`
-    (geojson STRING)
+    (geojson ARRAY<STRING>, numberOfClusters INT64)
     RETURNS STRING
     DETERMINISTIC
     LANGUAGE js
@@ -14,13 +14,19 @@ AS """
     if (!geojson) {
         return null;
     }
-
-    let clustered = turf.clustersKmeans(JSON.parse(geojson), {});
-    return JSON.stringify(clustered.geometry);
+    let options = {};
+    if(numberOfClusters != null)
+    {
+        options.numberOfClusters = Number(numberOfClusters);
+    }
+    const features = turf.featureCollection(geojson.map(x => turf.feature(JSON.parse(x))));
+    let clustered = turf.clustersKmeans(features, options);
+    return JSON.stringify(clustered);
+    //return JSON.stringify(clustered.geometry);
 """;
 
 CREATE OR REPLACE FUNCTION `@@BQ_PROJECTID@@.@@BQ_DATASET_CLUSTERING@@.ST_CLUSTERKMEANS`
-    (geog GEOGRAPHY)
-AS (
-    ST_GEOGFROMGEOJSON(`@@BQ_PROJECTID@@`.@@BQ_DATASET_CLUSTERING@@.__CLUSTERKMEANS(ST_ASGEOJSON(geog)))
-);
+    (geog ARRAY<GEOGRAPHY>, numberOfClusters INT64)
+AS ((
+    SELECT ST_GEOGFROMGEOJSON(`@@BQ_PROJECTID@@`.@@BQ_DATASET_CLUSTERING@@.__CLUSTERKMEANS(ARRAY_AGG(ST_ASGEOJSON(x)), numberOfClusters)) FROM unnest(geog) x
+));
