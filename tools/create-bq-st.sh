b2aa8585d40ac410d9ed52105f6bffe99c43b864
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2021-04-28 18:16:42 +0200

    Add Delete staging script. Update Deploy staging script

--- tools/create-bq-st.sh
+++ tools/create-bq-st.sh
@@ -1,31 +1,23 @@
 #!/usr/bin/env bash
 
-# Script to create a project and needed resources to deploy a temporary spatial-extension
+# Script to create a project and needed resources to deploy a
+# temporary spatial-extension staging environment in BigQuery
 
 set -e
-
-
-if [ $# -eq 0 ]
-  then
-    echo "No PR number provided"
-    exit 1
-fi
 
 GOOGLE_CI_FOLDER=571319100622
 GOOGLE_BILLING_ACCOUNT=014EAC-6BDFC0-233D1F
 
-PROJECT_NAME="bqcartost-core-$1"
-
-if gcloud projects describe $PROJECT_NAME > /dev/null 2>&1; then
-    echo "Project $PROJECT_NAME already exists"
+if gcloud projects describe $BQ_PROJECTID > /dev/null 2>&1; then
+    echo "Project $BQ_PROJECTID already exists"
 else
-    gcloud projects create $PROJECT_NAME --folder=$GOOGLE_CI_FOLDER  --quiet 
+    gcloud projects create $BQ_PROJECTID --folder=$GOOGLE_CI_FOLDER --quiet
+
     gcloud components install beta --quiet
-    gcloud beta billing projects link $PROJECT_NAME --billing-account=$GOOGLE_BILLING_ACCOUNT --quiet
 
-    gsutil mb -l us-east1 -p $PROJECT_NAME gs://carto-ext-ci-public-$PROJECT_NAME
-    gsutil mb -l us-east1 -p $PROJECT_NAME gs://carto-ext-ci-private-$PROJECT_NAME
+    gcloud beta billing projects link $BQ_PROJECTID --billing-account=$GOOGLE_BILLING_ACCOUNT --quiet
 
-    gcloud config set project $PROJECT_NAME --quiet 
+    gsutil mb -l us-east1 -p $BQ_PROJECTID $BQ_BUCKET_PUBLIC
+
+    gcloud config set project $BQ_PROJECTID --quiet
 fi
-
