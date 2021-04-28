ecc2c4751cb35043be47d5efeace014b0feb65e2
vdelacruzb <vdelacruzb@gmail.com>
2021-04-28 17:25:44 +0200

    fix linter

--- constructors/library/js/constructors_version.js
+++ constructors/library/js/constructors_version.js
@@ -7,5 +7,5 @@
 /* exported constructorsVersion */
 constructorsVersion = function() {
     return '1.0.1';
-}
+};
 module.exports.constructorsVersion = constructorsVersion;
--- measurements/library/js/measurements_version.js
+++ measurements/library/js/measurements_version.js
@@ -7,5 +7,5 @@
 /* exported measurementsVersion */
 measurementsVersion = function() {
     return '1.0.0';
-}
+};
 module.exports.measurementsVersion = measurementsVersion;
