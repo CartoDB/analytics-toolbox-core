7e87bf2ad2e7cf21e970da15728863b4b0670e39
vdelacruzb <vdelacruzb@gmail.com>
2021-05-11 15:10:25 +0200

    update unit tests

--- modules/transformations/bigquery/test/unit/index.test.js
+++ modules/transformations/bigquery/test/unit/index.test.js
@@ -5,6 +5,12 @@ test('library defined', () => {
     expect(lib.version).toBe(version);
     expect(lib.featureCollection).toBeDefined();
     expect(lib.feature).toBeDefined();
-    expect(lib.voronoi).toBeDefined();
-    expect(lib.polygonToLine).toBeDefined();
+    expect(lib.buffer).toBeDefined();
+    expect(lib.centerMean).toBeDefined();
+    expect(lib.centerMedian).toBeDefined();
+    expect(lib.centerOfMass).toBeDefined();
+    expect(lib.concave).toBeDefined();
+    expect(lib.destination).toBeDefined();
+    expect(lib.greatCircle).toBeDefined();
+    expect(lib.along).toBeDefined();
 });
