da06abdefe771716ddafad7afa5b0fb9e3579302
vdelacruzb <vdelacruzb@gmail.com>
2021-05-13 12:41:03 +0200

    add test-utils in snowflake

---
+++ common/snowflake/test-utils.js
@@ -0,0 +1,48 @@
+const snowflake = require('snowflake-sdk');
+
+const SF_DATABASEID = process.env.SF_DATABASEID;
+const SF_SCHEMA_PREFIX = process.env.SF_SCHEMA_PREFIX;
+const SF_PREFIX = `${SF_DATABASEID}.${SF_SCHEMA_PREFIX}`;
+
+let connection = snowflake.createConnection( {
+    account: process.env.SNOWSQL_ACCOUNT,
+    username: process.env.SNOWSQL_USER,
+    password: process.env.SNOWSQL_PWD
+});
+
+connection.connect( 
+    function(err) {
+        if (err) {
+            console.error('Unable to connect: ' + err.message);
+        } 
+        else 
+        {
+            // Optional: store the connection ID.
+            //const connection_ID = conn.getId();
+        }
+    }
+);
+
+function execAsync(query) {
+    return new Promise((resolve, reject) => {
+        connection.execute({
+            sqlText: query,
+            complete: (err, stmt, rows) => {
+                if (err) {
+                    return reject(err);
+                } 
+                return resolve(rows);
+            }
+        });
+    });
+}
+
+const runQuery = async (query) => {
+    query = query.replace(/@@SF_PREFIX@@/g, SF_PREFIX);
+    const rows = await execAsync(query);
+    return rows;
+}
+
+module.exports = {
+    runQuery
+}
