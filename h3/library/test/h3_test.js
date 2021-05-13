5d352a32276035c9fe8213ee61429d1c2cf7c544
vdelacruzb <vdelacruzb@gmail.com>
2021-05-13 09:58:15 +0200

    remove old h3

--- h3/library/test/h3_test.js
+++
@@ -1,9 +0,0 @@
-const test = require('tape');
-const fs = require('fs');
-/* Emulate how BigQuery would load the file */
-global.eval(fs.readFileSync('./h3_version.js') + '');
-
-test('Version', assert => {
-    assert.equal(h3Version(), '3.7.0.1');
-    assert.end();
-});
