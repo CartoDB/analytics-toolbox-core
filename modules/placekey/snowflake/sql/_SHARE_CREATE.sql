5701392c418c2183fd94010103aa67fe5a6abd52
vdelacruzb <vdelacruzb@gmail.com>
2021-05-13 12:58:53 +0200

    update functions and share

--- modules/placekey/snowflake/sql/_SHARE_CREATE.sql
+++ modules/placekey/snowflake/sql/_SHARE_CREATE.sql
@@ -3,4 +3,7 @@ CREATE SHARE IF NOT EXISTS @@SF_SHARE_PUBLIC@@;
 grant usage on database @@SF_DATABASEID@@ to share @@SF_SHARE_PUBLIC@@;
 grant usage on schema @@SF_DATABASEID@@.@@SF_SCHEMA@@ to share @@SF_SHARE_PUBLIC@@;
 
+grant usage on function @@SF_PREFIX@@placekey.H3_ASPLACEKEY(STRING) to share @@SF_SHARE_PUBLIC@@;
+grant usage on function @@SF_PREFIX@@placekey.PLACEKEY_ASH3(STRING) to share @@SF_SHARE_PUBLIC@@;
+grant usage on function @@SF_PREFIX@@placekey.ISVALID(STRING) to share @@SF_SHARE_PUBLIC@@;
 grant usage on function @@SF_PREFIX@@placekey.VERSION() to share @@SF_SHARE_PUBLIC@@;
