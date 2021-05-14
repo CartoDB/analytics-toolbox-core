60ed6ec1f48215b87f017de0cf4b7ae0e04ad5eb
vdelacruzb <vdelacruzb@gmail.com>
2021-05-14 15:38:54 +0200

    update s2 module for snowflake

--- modules/s2/snowflake/test/integration/ID_FROMHILBERTQUADKEY.test.js
+++ modules/s2/snowflake/test/integration/ID_FROMHILBERTQUADKEY.test.js
@@ -2,7 +2,7 @@ const { runQuery } = require('../../../../../common/snowflake/test-utils');
 
 test('ID_FROMHILBERTQUADKEY should work', async () => {
     const query = `
-        SELECT @@SF_PREFIX@@s2.ID_FROMHILBERTQUADKEY(VALUE) as id
+        SELECT CAST(@@SF_PREFIX@@s2.ID_FROMHILBERTQUADKEY(VALUE) AS STRING) as id
         FROM LATERAL FLATTEN(input => ARRAY_CONSTRUCT(
             '4/12', '2/02300033', '3/03131200023201', '5/0001221313222222120',
             '2/0221200002312111222332101', '5/1331022022103232320303230131'
@@ -10,8 +10,8 @@ test('ID_FROMHILBERTQUADKEY should work', async () => {
     `;
     const rows = await runQuery(query);
     expect(rows.map(r => r.ID.toString())).toEqual([
-        '10160120759347838976', '5008548143403368448', '7416309021449125888',
-        '11544114894487945216', '4985491052606295040', '12656544996034831280'
+        '-8286623314361712640', '5008548143403368448', '7416309021449125888',
+        '-6902629179221606400', '4985491052606295040', '-5790199077674720336'
     ]);
 });
 
--- modules/s2/snowflake/test/integration/LONGLAT_ASID.test.js
+++ modules/s2/snowflake/test/integration/LONGLAT_ASID.test.js
@@ -1,10 +1,10 @@
 const { runQuery } = require('../../../../../common/snowflake/test-utils');
 
 test('LONGLAT_ASID should work', async () => {
-    const query = 'SELECT @@SF_PREFIX@@s2.LONGLAT_ASID(-74.006, 40.7128, 12) as id';
+    const query = 'SELECT CAST(@@SF_PREFIX@@s2.LONGLAT_ASID(-74.006, 40.7128, 12) AS STRING) as id';
     const rows = await runQuery(query);
     expect(rows.length).toEqual(1);
-    expect(rows[0].ID.toString()).toEqual('9926595690882924544');
+    expect(rows[0].ID.toString()).toEqual('-8520148382826627072');
 });
 
 test('LONGLAT_ASID should fail with NULL argument', async () => {
