d5af52704830dbdea8c72c8db50c3f02f3b13aed
vdelacruzb <vdelacruzb@gmail.com>
2021-05-18 16:14:48 +0200

    simplify st_makeenvelope

--- modules/constructors/snowflake/sql/ST_MAKEENVELOPE.sql
+++ modules/constructors/snowflake/sql/ST_MAKEENVELOPE.sql
@@ -6,5 +6,5 @@ CREATE OR REPLACE SECURE FUNCTION @@SF_PREFIX@@constructors.ST_MAKEENVELOPE
 (xmin DOUBLE, ymin DOUBLE, xmax DOUBLE, ymax DOUBLE)
 RETURNS GEOGRAPHY
 AS $$
-    ST_MAKEPOLYGON(TO_GEOGRAPHY(CONCAT('LINESTRING(', xmin, ' ', ymin, ',', xmin, ' ', ymax, ',', xmax, ' ', ymax, ',', xmax, ' ', ymin, ',', xmin, ' ', ymin, ')')))
+    TO_GEOGRAPHY(CONCAT('POLYGON((', xmin, ' ', ymin, ',', xmin, ' ', ymax, ',', xmax, ' ', ymax, ',', xmax, ' ', ymin, ',', xmin, ' ', ymin, '))'))
 $$;
