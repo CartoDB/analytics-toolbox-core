2522c66c3a54b8688814dd1753b11e2c2e8492a9
Valentin de la Cruz Barquero <6054336+vdelacruzb@users.noreply.github.com>
2021-05-20 12:36:10 +0200

    Add measurements module to Snowflake (#109)

---
+++ modules/measurements/snowflake/sql/ST_AZIMUTH.sql
@@ -0,0 +1,23 @@
+----------------------------
+-- Copyright (C) 2021 CARTO
+----------------------------
+
+CREATE OR REPLACE SECURE FUNCTION @@SF_PREFIX@@measurements._AZIMUTH
+(geojsonStart STRING, geojsonEnd STRING)
+RETURNS DOUBLE
+LANGUAGE JAVASCRIPT
+AS $$
+    @@SF_LIBRARY_CONTENT@@
+
+    if (!GEOJSONSTART || !GEOJSONEND) {
+        return null;
+    }
+    return measurementsLib.bearing(JSON.parse(GEOJSONSTART), JSON.parse(GEOJSONEND));
+$$;
+
+CREATE OR REPLACE SECURE FUNCTION @@SF_PREFIX@@measurements.ST_AZIMUTH
+(startPoint GEOGRAPHY, endPoint GEOGRAPHY)
+RETURNS DOUBLE
+AS $$
+    @@SF_PREFIX@@measurements._AZIMUTH(CAST(ST_ASGEOJSON(STARTPOINT) AS STRING), CAST(ST_ASGEOJSON(ENDPOINT) AS STRING))
+$$;
