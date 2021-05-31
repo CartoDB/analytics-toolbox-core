869e0f32f44acef6fcdbe014edd72b3ea6f0452c
vdelacruzb <vdelacruzb@gmail.com>
2021-05-31 17:22:06 +0200

    update measurements share

--- modules/measurements/snowflake/sql/_SHARE_CREATE.sql
+++ modules/measurements/snowflake/sql/_SHARE_CREATE.sql
@@ -11,6 +11,6 @@ grant usage on schema @@SF_DATABASE@@.@@SF_SCHEMA@@ to share @@SF_SHARE@@;
 
 grant usage on function @@SF_PREFIX@@measurements.ST_ANGLE(GEOGRAPHY, GEOGRAPHY, GEOGRAPHY) to share @@SF_SHARE@@;
 grant usage on function @@SF_PREFIX@@measurements.ST_AZIMUTH(GEOGRAPHY, GEOGRAPHY) to share @@SF_SHARE@@;
-grant usage on function @@SF_PREFIX@@measurements.ST_MINKOWSKIDISTANCE(GEOGRAPHY) to share @@SF_SHARE@@;
-grant usage on function @@SF_PREFIX@@measurements.ST_MINKOWSKIDISTANCE(GEOGRAPHY, DOUBLE) to share @@SF_SHARE@@;
+grant usage on function @@SF_PREFIX@@measurements.ST_MINKOWSKIDISTANCE(ARRAY) to share @@SF_SHARE@@;
+grant usage on function @@SF_PREFIX@@measurements.ST_MINKOWSKIDISTANCE(ARRAY, DOUBLE) to share @@SF_SHARE@@;
 grant usage on function @@SF_PREFIX@@measurements.VERSION() to share @@SF_SHARE@@;
