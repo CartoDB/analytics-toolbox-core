0041876cd910ece90c88c1e13d6469b104775622
François BAPTISTE <7817985+francois-baptiste@users.noreply.github.com>
2021-07-23 15:31:15 +0200

    fix

--- modules/quadkey/bigquery/sql/ST_GEOGPOINTFROMQUADINT.sql
+++ modules/quadkey/bigquery/sql/ST_GEOGPOINTFROMQUADINT.sql
@@ -12,7 +12,7 @@ CREATE OR REPLACE FUNCTION `@@BQ_PREFIX@@quadkey.ST_GEOGPOINTFROMQUADINT`(quadin
 RETURNS GEOGRAPHY AS (
 ST_GEOGPOINT((`@@BQ_PREFIX@@quadkey.ZXY_FROMQUADINT`(quadint).x+0.5)/POW(2,`@@BQ_PREFIX@@quadkey.ZXY_FROMQUADINT`(quadint).z)*360-180,
       180/ACOS(-1)*ATAN(0.5*(
-      EXP(`@@BQ_PREFIX@@quadkey.__N`)
-      -EXP(-`@@BQ_PREFIX@@quadkey.__N`)
+      EXP(`@@BQ_PREFIX@@quadkey.__N`(quadint))
+      -EXP(-`@@BQ_PREFIX@@quadkey.__N`(quadint))
       )))
 );
