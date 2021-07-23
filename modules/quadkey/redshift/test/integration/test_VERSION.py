abe8ad2c80c2d991969066df4de6703b8850252a
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2021-07-23 17:01:13 +0200

    Update lint,test commands in redshift. Add quadkey VERSION function

--- modules/quadkey/redshift/test/integration/test_VERSION.py
+++ modules/quadkey/redshift/test/integration/test_VERSION.py
@@ -1,20 +1,22 @@
-import redshift_connector
 import os
-import pytest
+import redshift_connector
+
+from lib import quadkeyLib
+
 
 # Connects to Redshift cluster using AWS credentials
 conn = redshift_connector.connect(
     host=os.environ["RS_HOST"],
     database=os.environ["RS_DATABASE"],
     user=os.environ["RS_USER"],
-    password=os.environ["RS_PASSWORD"]
+    password=os.environ["RS_PASSWORD"],
 )
 schema_prefix = os.environ["RS_SCHEMA_PREFIX"]
-cursor= conn.cursor()
+cursor = conn.cursor()
+
 
 def test_version():
-    version = 1
-    cursor.execute(f"SELECT {schema_prefix}quadkey.f_distance(0,0,1,0)")
+    cursor.execute(f"SELECT {schema_prefix}quadkey.VERSION()")
     result = cursor.fetchall()
-    
-    assert version == result[0][0]
+
+    assert result[0][0] == quadkeyLib.__version__
