0ab28a5a090f7fb220fe41c885a99f5e367dd311
François BAPTISTE <7817985+francois-baptiste@users.noreply.github.com>
2021-07-23 14:43:45 +0200

    add quadkey centroid function

---
+++ modules/quadkey/bigquery/sql/ST_GEOGPOINTFROMQUADINT.sql
@@ -0,0 +1,18 @@
+----------------------------
+-- Copyright (C) 2021 CARTO
+----------------------------
+
+CREATE OR REPLACE FUNCTION `@@BQ_PREFIX@@quadkey.__N`(quadint INT64)
+RETURNS FLOAT64 AS (
+ACOS(-1)-2*ACOS(-1)*(`@@BQ_PREFIX@@quadkey.ZXY_FROMQUADINT`(quadint).y+0.5)/POW(2,`@@BQ_PREFIX@@quadkey.ZXY_FROMQUADINT`(quadint).z)
+);
+
+
+CREATE OR REPLACE FUNCTION `@@BQ_PREFIX@@quadkey.ST_GEOGPOINTFROMQUADINT`(quadint INT64)
+RETURNS GEOGRAPHY AS (
+ST_GEOGPOINT((`@@BQ_PREFIX@@quadkey.ZXY_FROMQUADINT`(quadint).x+0.5)/POW(2,`@@BQ_PREFIX@@quadkey.ZXY_FROMQUADINT`(quadint).z)*360-180,
+      180/ACOS(-1)*ATAN(0.5*(
+      EXP(`@@BQ_PREFIX@@quadkey.__N`)
+      -EXP(-`@@BQ_PREFIX@@quadkey.__N`)
+      )))
+);
