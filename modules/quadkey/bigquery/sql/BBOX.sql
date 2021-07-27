d5a143739efd4fec88d8de849470e15288136093
François BAPTISTE <7817985+francois-baptiste@users.noreply.github.com>
2021-07-27 18:20:51 +0200

    rewrite ST_BOUNDARY

--- modules/quadkey/bigquery/sql/BBOX.sql
+++ modules/quadkey/bigquery/sql/BBOX.sql
@@ -2,15 +2,30 @@
 -- Copyright (C) 2021 CARTO
 ----------------------------
 
-CREATE OR REPLACE FUNCTION `@@BQ_PREFIX@@quadkey.BBOX`
-(quadint INT64)
-RETURNS ARRAY<FLOAT64>
-DETERMINISTIC
-LANGUAGE js
-OPTIONS (library=["@@BQ_LIBRARY_BUCKET@@"])
-AS """
-    if (quadint == null) {
-        throw new Error('NULL argument passed to UDF');
-    }
-    return quadkeyLib.bbox(quadint);
-""";
+CREATE OR REPLACE FUNCTION `@@BQ_PREFIX@@quadkey.BBOX`(quadint INT64)
+RETURNS GEOGRAPHY AS (
+ST_MAKEPOLYGON(
+ST_MAKELINE([
+ST_GEOGPOINT(
+`@@BQ_PREFIX@@quadkey._tile2lon`(`@@BQ_PREFIX@@quadkey.ZXY_FROMQUADINT`(quadint).x  ,`@@BQ_PREFIX@@quadkey.ZXY_FROMQUADINT`(quadint).z),
+`@@BQ_PREFIX@@quadkey._tile2lat`(`@@BQ_PREFIX@@quadkey.ZXY_FROMQUADINT`(quadint).y  ,`@@BQ_PREFIX@@quadkey.ZXY_FROMQUADINT`(quadint).z)
+),
+ST_GEOGPOINT(
+`@@BQ_PREFIX@@quadkey._tile2lon`(`@@BQ_PREFIX@@quadkey.ZXY_FROMQUADINT`(quadint).x  ,`@@BQ_PREFIX@@quadkey.ZXY_FROMQUADINT`(quadint).z),
+`@@BQ_PREFIX@@quadkey._tile2lat`(`@@BQ_PREFIX@@quadkey.ZXY_FROMQUADINT`(quadint).y+1,`@@BQ_PREFIX@@quadkey.ZXY_FROMQUADINT`(quadint).z)
+),
+ST_GEOGPOINT(
+`@@BQ_PREFIX@@quadkey._tile2lon`(`@@BQ_PREFIX@@quadkey.ZXY_FROMQUADINT`(quadint).x+1,`@@BQ_PREFIX@@quadkey.ZXY_FROMQUADINT`(quadint).z),
+`@@BQ_PREFIX@@quadkey._tile2lat`(`@@BQ_PREFIX@@quadkey.ZXY_FROMQUADINT`(quadint).y+1,`@@BQ_PREFIX@@quadkey.ZXY_FROMQUADINT`(quadint).z)
+),
+ST_GEOGPOINT(
+`@@BQ_PREFIX@@quadkey._tile2lon`(`@@BQ_PREFIX@@quadkey.ZXY_FROMQUADINT`(quadint).x+1,`@@BQ_PREFIX@@quadkey.ZXY_FROMQUADINT`(quadint).z),
+`@@BQ_PREFIX@@quadkey._tile2lat`(`@@BQ_PREFIX@@quadkey.ZXY_FROMQUADINT`(quadint).y  ,`@@BQ_PREFIX@@quadkey.ZXY_FROMQUADINT`(quadint).z)
+),
+ST_GEOGPOINT(
+`@@BQ_PREFIX@@quadkey._tile2lon`(`@@BQ_PREFIX@@quadkey.ZXY_FROMQUADINT`(quadint).x  ,`@@BQ_PREFIX@@quadkey.ZXY_FROMQUADINT`(quadint).z),
+`@@BQ_PREFIX@@quadkey._tile2lat`(`@@BQ_PREFIX@@quadkey.ZXY_FROMQUADINT`(quadint).y  ,`@@BQ_PREFIX@@quadkey.ZXY_FROMQUADINT`(quadint).z)
+)
+])
+)
+);
