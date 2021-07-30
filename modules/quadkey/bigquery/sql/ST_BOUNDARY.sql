19abf29d54c6e50e495f7cbb07838b6acbf5a700
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2021-07-30 19:54:04 +0200

    Adding internal _MAKEENVELOPE for ST_BOUNDARY

--- modules/quadkey/bigquery/sql/ST_BOUNDARY.sql
+++ modules/quadkey/bigquery/sql/ST_BOUNDARY.sql
@@ -2,11 +2,18 @@
 -- Copyright (C) 2021 CARTO
 ----------------------------
 
+CREATE OR REPLACE FUNCTION `@@BQ_PREFIX@@quadkey._MAKEENVELOPE`
+(xmin FLOAT64, ymin FLOAT64, xmax FLOAT64, ymax FLOAT64)
+RETURNS GEOGRAPHY
+AS (
+    ST_GeogFromText(CONCAT('POLYGON((', xmin, ' ', ymin, ',', xmin, ' ', ymax, ',', xmax, ' ', ymax, ',', xmax, ' ', ymin, ',', xmin, ' ', ymin, '))'))
+);
+
 CREATE OR REPLACE FUNCTION `@@BQ_PREFIX@@quadkey.ST_BOUNDARY`(quadint INT64)
 RETURNS GEOGRAPHY
 AS (
     COALESCE(
-        `@@BQ_PREFIX@@constructors.ST_MAKEENVELOPE`(
+        `@@BQ_PREFIX@@quadkey._MAKEENVELOPE`(
             `@@BQ_PREFIX@@quadkey.__BBOX_E`(quadint),
             `@@BQ_PREFIX@@quadkey.__BBOX_N`(quadint),
             `@@BQ_PREFIX@@quadkey.__BBOX_W`(quadint),
