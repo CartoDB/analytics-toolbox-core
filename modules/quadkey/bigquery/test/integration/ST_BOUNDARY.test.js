cf187d66eb7ea92090108b5572300426cfea0ebf
vdelacruzb <vdelacruzb@gmail.com>
2021-07-30 19:04:00 +0200

    Fix Quadint ST_BOUNDARY

--- modules/quadkey/bigquery/test/integration/ST_BOUNDARY.test.js
+++ modules/quadkey/bigquery/test/integration/ST_BOUNDARY.test.js
@@ -8,10 +8,10 @@ test('ST_BOUNDARY should work', async () => {
             \`@@BQ_PREFIX@@quadkey.ST_BOUNDARY\`(12960460429066265) as geog3`;
     
     const rows = await runQuery(query);
-    expect(rows.length).toEqual(1);  
-    expect(JSON.stringify(rows[0].geog1.value)).toEqual('"POLYGON((-45 45.089035564831, -45 44.840290651398, -44.6484375 44.840290651398, -45 45.089035564831))"');
-    expect(JSON.stringify(rows[0].geog2.value)).toEqual('"POLYGON((-45 45.0007380782907, -45 44.9997670191813, -44.9986267089844 44.9997670191813, -45 45.0007380782907))"');
-    expect(JSON.stringify(rows[0].geog3.value)).toEqual('"POLYGON((-45 45.0000021990696, -45 44.9999946126367, -44.9999892711639 44.9999946126367, -45 45.0000021990696))"');
+    expect(rows.length).toEqual(1);
+    expect(JSON.stringify(rows[0].geog1.value)).toEqual('"POLYGON((-44.6484375 44.840290651398, -44.6484375 45.089035564831, -45 45.089035564831, -45 44.840290651398, -44.6484375 44.840290651398))"');
+    expect(JSON.stringify(rows[0].geog2.value)).toEqual('"POLYGON((-44.9986267089844 44.9997670191813, -44.9986267089844 45.0007380782907, -45 45.0007380782907, -45 44.9997670191813, -44.9986267089844 44.9997670191813))"');
+    expect(JSON.stringify(rows[0].geog3.value)).toEqual('"POLYGON((-44.9999892711639 44.9999946126367, -44.9999892711639 45.0000021990696, -45 45.0000021990696, -45 44.9999946126367, -44.9999892711639 44.9999946126367))"'); 
 });
 
 test('ST_BOUNDARY should fail with NULL argument', async () => {
