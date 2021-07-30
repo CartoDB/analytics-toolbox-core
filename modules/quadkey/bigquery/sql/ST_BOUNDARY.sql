2803f5030e5b460c0076ba6556d4478f1aac5658
Valentin de la Cruz Barquero <6054336+vdelacruzb@users.noreply.github.com>
2021-07-30 19:57:35 +0200

    Fix Quadint ST_BOUNDARY (#141)
    Co-authored-by: Jesús Arroyo Torrens <jarroyo@cartodb.com>

--- modules/quadkey/bigquery/sql/ST_BOUNDARY.sql
+++ modules/quadkey/bigquery/sql/ST_BOUNDARY.sql
@@ -2,33 +2,23 @@
 -- Copyright (C) 2021 CARTO
 ----------------------------
 
+CREATE OR REPLACE FUNCTION `@@BQ_PREFIX@@quadkey._MAKEENVELOPE`
+(xmin FLOAT64, ymin FLOAT64, xmax FLOAT64, ymax FLOAT64)
+RETURNS GEOGRAPHY
+AS (
+    ST_GeogFromText(CONCAT('POLYGON((', xmin, ' ', ymin, ',', xmin, ' ', ymax, ',', xmax, ' ', ymax, ',', xmax, ' ', ymin, ',', xmin, ' ', ymin, '))'))
+);
+
 CREATE OR REPLACE FUNCTION `@@BQ_PREFIX@@quadkey.ST_BOUNDARY`(quadint INT64)
-RETURNS GEOGRAPHY AS (
-COALESCE(
-ST_MAKEPOLYGON(
-ST_MAKELINE([
-ST_GEOGPOINT(
-`@@BQ_PREFIX@@quadkey.__BBOX_E`(quadint),
-`@@BQ_PREFIX@@quadkey.__BBOX__N`(quadint)
-),
-ST_GEOGPOINT(
-`@@BQ_PREFIX@@quadkey.__BBOX_E`(quadint),
-`@@BQ_PREFIX@@quadkey.__BBOX_W`(quadint)
-),
-ST_GEOGPOINT(
-`@@BQ_PREFIX@@quadkey.__BBOX_S`(quadint),
-`@@BQ_PREFIX@@quadkey.__BBOX_W`(quadint)
-),
-ST_GEOGPOINT(
-`@@BQ_PREFIX@@quadkey.__BBOX_S`(quadint),
-`@@BQ_PREFIX@@quadkey.__BBOX_W`(quadint)
-),
-ST_GEOGPOINT(
-`@@BQ_PREFIX@@quadkey.__BBOX_E`(quadint),
-`@@BQ_PREFIX@@quadkey.__BBOX__N`(quadint)
-)
-])
-),
-ERROR('NULL argument passed to UDF')
-)
+RETURNS GEOGRAPHY
+AS (
+    COALESCE(
+        `@@BQ_PREFIX@@quadkey._MAKEENVELOPE`(
+            `@@BQ_PREFIX@@quadkey.__BBOX_E`(quadint),
+            `@@BQ_PREFIX@@quadkey.__BBOX_N`(quadint),
+            `@@BQ_PREFIX@@quadkey.__BBOX_W`(quadint),
+            `@@BQ_PREFIX@@quadkey.__BBOX_S`(quadint)
+        ),
+        ERROR('NULL argument passed to UDF')
+    )
 );
