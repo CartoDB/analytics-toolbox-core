5a37c215654c9420611e777e37bc1e095f445b3b
vdelacruzb <vdelacruzb@gmail.com>
2021-08-05 16:14:53 +0200

    add integration test

---
+++ modules/quadkey/redshift/test/integration/test_ADD.py
@@ -0,0 +1,22 @@
+import os
+import redshift_connector
+
+from lib import quadkeyLib
+
+
+# Connects to Redshift cluster using AWS credentials
+conn = redshift_connector.connect(
+    host=os.environ["RS_HOST"],
+    database=os.environ["RS_DATABASE"],
+    user=os.environ["RS_USER"],
+    password=os.environ["RS_PASSWORD"],
+)
+schema_prefix = os.environ["RS_SCHEMA_PREFIX"]
+cursor = conn.cursor()
+
+
+def test_add():
+    cursor.execute(f"SELECT {schema_prefix}quadkey.ADD(123, 245)")
+    result = cursor.fetchall()
+
+    assert result[0][0] == 368
