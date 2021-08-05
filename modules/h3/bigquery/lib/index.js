52f40ed5d959dda854d95aa7e9ac928d8059d114
François BAPTISTE <7817985+francois-baptiste@users.noreply.github.com>
2021-08-05 17:20:20 +0200

    fix lints

--- modules/h3/bigquery/lib/index.js
+++ modules/h3/bigquery/lib/index.js
@@ -1,5 +1,5 @@
 import { version }  from '../package.json';
-import { geoToH3, compact, h3Distance, h3IsValid, hexRing, h3IsPentagon, kRing, polyfill, h3ToGeoBoundary, h3ToChildren, h3ToParent, uncompact, experimentalH3ToLocalIj, experimentalLocalIjToH3} from 'h3-js';
+import { geoToH3, compact, h3Distance, h3IsValid, hexRing, h3IsPentagon, kRing, polyfill, h3ToGeoBoundary, h3ToChildren, h3ToParent, uncompact, experimentalH3ToLocalIj} from 'h3-js';
 
 export default {
     geoToH3,
