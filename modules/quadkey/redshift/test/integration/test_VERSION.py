7df535a3efed3a891c3197f887782a8e140a2881
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2021-08-10 10:53:20 +0200

    Update rs linter to use single-quotes

--- modules/quadkey/redshift/test/integration/test_VERSION.py
+++ modules/quadkey/redshift/test/integration/test_VERSION.py
@@ -3,5 +3,5 @@ from test_utils import run_query
 
 
 def test_version():
-    result = run_query("SELECT @@RS_PREFIX@@quadkey.VERSION()")
+    result = run_query('SELECT @@RS_PREFIX@@quadkey.VERSION()')
     assert result[0][0] == __version__
--- modules/quadkey/redshift/test/unit/__init__.py
+++ modules/quadkey/redshift/test/unit/__init__.py
@@ -3,5 +3,5 @@ import sys
 
 # Include this to allow importing lib from source code
 sys.path.insert(
-    1, os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "..")
+    1, os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..')
 )
