4d0b89cd10f96f9c1e00bab0e84ce011b637bab2
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2021-08-10 13:55:09 +0200

    Improve RS deploy output

--- common/redshift/test_utils/__init__.py
+++ common/redshift/test_utils/__init__.py
@@ -2,14 +2,14 @@ import os
 import redshift_connector
 
 conn = redshift_connector.connect(
-    host=os.environ["RS_HOST"],
-    database=os.environ["RS_DATABASE"],
-    user=os.environ["RS_USER"],
-    password=os.environ["RS_PASSWORD"],
+    host=os.environ['RS_HOST'],
+    database=os.environ['RS_DATABASE'],
+    user=os.environ['RS_USER'],
+    password=os.environ['RS_PASSWORD'],
 )
 
 
 def run_query(query):
     cursor = conn.cursor()
-    cursor.execute(query.replace("@@RS_PREFIX@@", os.environ["RS_SCHEMA_PREFIX"]))
+    cursor.execute(query.replace('@@RS_PREFIX@@', os.environ['RS_SCHEMA_PREFIX']))
     return cursor.fetchall()
