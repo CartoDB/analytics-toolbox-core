30dacded55f089d2849891d4fd55ca78f588822e
vdelacruzb <vdelacruzb@gmail.com>
2021-09-03 15:54:09 +0200

    add quadint_fromquadkey

--- modules/quadkey/redshift/lib/__init__.py
+++ modules/quadkey/redshift/lib/__init__.py
@@ -1,1 +1,33 @@
-__version__ = '1.0.0'
+import numpy as np
+import mercantile
+
+_version__ = '1.0.0'
+
+
+def quadintFromZXY(z, x, y):
+    if z < 0 or z > 29:
+        return None
+
+    quadint = np.int64(y)
+    quadint <<= z
+    quadint |= x
+    quadint <<= 5
+    quadint |= z
+    return quadint
+
+
+def ZXYFromQuadint(quadint):
+    quadint = np.int64(quadint)
+    z = quadint & 31
+    x = (quadint >> 5) & ((1 << z) - 1)
+    y = quadint >> (z + 5)
+    return {'z': z, 'x': x, 'y': y}
+
+def quadintFromQuadkey (quadkey):
+    tile = mercantile.quadkey_to_tile(quadkey);
+    return quadintFromZXY(tile.z, tile.x, tile.y);
+
+
+def quadkeyFromQuadint (quadint):
+    tile = ZXYFromQuadint(quadint);
+    return mercantile.quadkey(tile.x, tile.y, tile.z);
---
+++ modules/quadkey/redshift/sql/QUADINT_FROMQUADKEY.sql
@@ -0,0 +1,21 @@
+----------------------------
+-- Copyright (C) 2021 CARTO
+----------------------------
+
+CREATE OR REPLACE FUNCTION @@RS_PREFIX@@quadkey._QUADINT_FROMQUADKEY
+(quadkey VARCHAR)
+RETURNS VARCHAR
+IMMUTABLE
+AS $$
+    from @@RS_PREFIX@@quadkeyLib import quadintFromQuadkey
+    return str(quadintFromQuadkey(quadkey))
+$$ LANGUAGE plpythonu;
+
+CREATE OR REPLACE FUNCTION @@RS_PREFIX@@quadkey.QUADINT_FROMQUADKEY
+(VARCHAR)
+-- (quadkey)
+RETURNS BIGINT
+IMMUTABLE
+AS $$
+    SELECT @@RS_PREFIX@@quadkey._QUADINT_FROMQUADKEY($1)::BIGINT
+$$ language sql;
