64c6222e0573d091e8143e560ab4ea7152822da2
Alex Graciano <agraciano@cartodb.com>
2021-09-07 12:18:00 +0200

    Include module installaction package in Makefile

--- common/redshift/libraryinstaller.sh
+++ common/redshift/libraryinstaller.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
-# Install Pip Module as Redshift Library
+# Install Pip Module as Redshift Library. 
+# It is based on the repository https://github.com/aws-samples/amazon-redshift-udfs
 
 function usage {
 	echo "./libraryInstaller.sh -m <module> -f <requirement_file>"
@@ -32,70 +33,6 @@ function notNull {
 	fi
 }
 
-# make sure we have pip and the aws cli installed
-checkDep "aws"
-checkDep "pip3"
-
-# make sure we have wheel installed into pip
-pip3 show wheel &> /dev/null
-if [ $? != 0 ]; then
-  echo "pip3 wheel not found. Please install with 'sudo pip install wheel'"
-  exit -1
-fi
-
-# look up runtime arguments of the module name and the destination S3 Prefix
-while getopts "m:f:h" opt; do
-    case $opt in
-        m) module="$OPTARG";;
-        f) module_file="$OPTARG";;
-		h) usage;;
-		\?) echo "Invalid option: -"$OPTARG"" >&2
-			exit 1;;
-		:) usage;;
-	esac
-done
-
-# validate arguments
-notNull "$module" "Please provide the pip module name using -m"
-notNull "$AWS_S3_BUCKET" "Please provide an S3 bucket to store the library in using export AWS_S3_BUCKET=bucket"
-notNull "$RS_DATABASE" "Please provide a Redshift database using export RS_DATABASE=database"
-notNull "$RS_REGION" "Please provide a region using export RS_REGION=region"
-notNull "$RS_USER" "Please provide a Redshift user using export RS_USER=user"
-notNull "$RS_CLUSTER_ID" "Please provide a Redshift cluster using export RS_CLUSTER_ID=cluster"
-notNull "$AWS_ACCESS_KEY_ID" "Please provide an AWS access key ID using export AWS_ACCESS_KEY_ID=key"
-notNull "$AWS_SECRET_ACCESS_KEY" "Please provide an AWS secret access key ID using export AWS_SECRET_ACCESS_KEY=secret_key"
-
-# check that the s3 prefix is in the right format
-# starts with 's3://'
-
-if ! [[ $AWS_S3_BUCKET == s3:\/\/* ]]; then
-	echo "S3 Prefix must start with 's3://'"
-	echo
-	usage
-fi
-
-# extract modules from requirement file
-
-# found the module - install to a local hidden directory
-echo "Installing $module with pip and uploading to $AWS_S3_BUCKET"
-
-TMPDIR=.tmp
-if [ ! -d "$TMPDIR" ]; then
-  mkdir $TMPDIR
-fi
-
-rm -Rf "$TMPDIR/.$module" &> /dev/null
-
-mkdir "$TMPDIR/.$module"
-
-pip3 wheel $module --wheel-dir "$TMPDIR/.$module"
-if [ $? != 0 ]; then
-	echo "Unable to find module $module in pip."
-	rm -Rf "$TMPDIR/.$module"
-	exit $?
-fi
-
-
 execQuery()
 {
   output=`aws redshift-data execute-statement --cluster-identifier $1 --database $2 --db-user $3 --sql "$4" --region $5`
@@ -115,20 +52,91 @@ execQuery()
   fi
 }
 
-files=`ls ${TMPDIR}/.${module}/*.whl`
-for depname in `basename -s .whl $files`
-do
-	echo $depname
-	aws s3 cp "$TMPDIR/.$module/$depname.whl" "$AWS_S3_BUCKET/$depname.zip"
-	sql="CREATE OR REPLACE LIBRARY ${depname%%-*} LANGUAGE plpythonu FROM '$AWS_S3_BUCKET/$depname.zip' WITH CREDENTIALS 'aws_access_key_id=$AWS_ACCESS_KEY_ID;aws_secret_access_key=$AWS_SECRET_ACCESS_KEY'; "
-    execQuery $RS_CLUSTER_ID $RS_DATABASE $RS_USER "$sql" $RS_REGION
-	if [ $? != 0 ]; then
-		rm -Rf "$TMPDIR/.$module"
-		exit $?
-	fi
+# make sure we have pip and the aws cli installed
+checkDep "aws"
+checkDep "pip3"
+
+# make sure we have wheel installed into pip
+pip3 show wheel &> /dev/null
+if [ $? != 0 ]; then
+  echo "pip3 wheel not found. Please install with 'sudo pip install wheel'"
+  exit -1
+fi
+
+# look up runtime arguments of the module name and the destination S3 Prefix
+while getopts "m:f:h" opt; do
+    case $opt in
+        m) module="$OPTARG";;
+        f) modules_file="$OPTARG";;
+		h) usage;;
+		\?) echo "Invalid option: -"$OPTARG"" >&2
+			exit 1;;
+		:) usage;;
+	esac
 done
 
-rm -Rf "$TMPDIR/.$module"
-cd - &> /dev/null
+# validate arguments
+notNull "$AWS_S3_BUCKET" "Please provide an S3 bucket to store the library in using export AWS_S3_BUCKET=bucket"
+notNull "$RS_DATABASE" "Please provide a Redshift database using export RS_DATABASE=database"
+notNull "$RS_REGION" "Please provide a region using export RS_REGION=region"
+notNull "$RS_USER" "Please provide a Redshift user using export RS_USER=user"
+notNull "$RS_CLUSTER_ID" "Please provide a Redshift cluster using export RS_CLUSTER_ID=cluster"
+notNull "$AWS_ACCESS_KEY_ID" "Please provide an AWS access key ID using export AWS_ACCESS_KEY_ID=key"
+notNull "$AWS_SECRET_ACCESS_KEY" "Please provide an AWS secret access key ID using export AWS_SECRET_ACCESS_KEY=secret_key"
+
+# check that the s3 prefix is in the right format
+# starts with 's3://'
+
+if ! [[ $AWS_S3_BUCKET == s3:\/\/* ]]; then
+	echo "S3 Prefix must start with 's3://'"
+	echo
+	usage
+fi
+
+# extract modules from requirement file
+modules_to_install=($module)
+while IFS="==" read -r m v || [ -n "$m" ]
+do
+  modules_to_install+=($m)
+done < $modules_file
+
+# found the modules - install to a local hidden directory
+for m in "${modules_to_install[@]}"
+do
+   : 
+	echo "Installing $m with pip and uploading to $AWS_S3_BUCKET"
+	
+	TMPDIR=.tmp
+	if [ ! -d "$TMPDIR" ]; then
+	  mkdir $TMPDIR
+	fi
+	
+	rm -Rf "$TMPDIR/.$m" &> /dev/null
+	
+	mkdir "$TMPDIR/.$m"
+	
+	pip3 wheel $m --wheel-dir "$TMPDIR/.$m"
+	if [ $? != 0 ]; then
+		echo "Unable to find module $m in pip."
+		rm -Rf "$TMPDIR/.$m"
+		exit $?
+	fi
+	
+	
+	files=`ls ${TMPDIR}/.${m}/*.whl`
+	for depname in `basename -s .whl $files`
+	do
+		echo $depname
+		aws s3 cp "$TMPDIR/.$m/$depname.whl" "$AWS_S3_BUCKET/$depname.zip"
+		sql="CREATE OR REPLACE LIBRARY ${depname%%-*} LANGUAGE plpythonu FROM '$AWS_S3_BUCKET/$depname.zip' WITH CREDENTIALS 'aws_access_key_id=$AWS_ACCESS_KEY_ID;aws_secret_access_key=$AWS_SECRET_ACCESS_KEY'; "
+	    execQuery $RS_CLUSTER_ID $RS_DATABASE $RS_USER "$sql" $RS_REGION
+		if [ $? != 0 ]; then
+			rm -Rf "$TMPDIR/.$m"
+			exit $?
+		fi
+	done
+
+	rm -Rf "$TMPDIR/"
+done
 
 exit 0
