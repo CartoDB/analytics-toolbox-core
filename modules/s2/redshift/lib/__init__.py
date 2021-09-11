4bd8301c2aa030f32968f529698c9a7672199cc8
arredond <alvaroarredondoparra@gmail.com>
2021-09-11 10:26:24 +0200

    lint and remove notebook stuff

--- modules/s2/redshift/lib/__init__.py
+++ modules/s2/redshift/lib/__init__.py
@@ -1,10 +1,10 @@
-import json
+# import json
 import math
 
 import s2sphere
 
 # from shapely import wkt
-from shapely.geometry import mapping, Polygon
+# from shapely.geometry import mapping, Polygon
 
 VERSION = '1.0.0'
 
@@ -20,6 +20,16 @@ def lnglat_as_id(longitude, latitude, resolution):
     return cell.id()
 
 
+def id_to_token(cell_id):
+    """Returns a unique string token for this cell id.
+
+    This is a hex encoded version of the cell id with the right zeros stripped of.
+    """
+    cell = s2sphere.CellId(cell_id)
+
+    return str(cell.to_token())
+
+
 def get_resolution(cell_id):
     """Returns the resolution (level) of a certain cell"""
     return s2sphere.CellId(cell_id).level()
@@ -27,7 +37,7 @@ def get_resolution(cell_id):
 
 def to_parent(cell_id, resolution=None):
     """Returns the parent cell ID of a given cell ID for a specific resolution.
-    
+
     A parent cell is the smaller resolution containing cell.
 
     By default, this function returns the direct parent (where parent resolution
@@ -35,15 +45,15 @@ def to_parent(cell_id, resolution=None):
     with the desired parent resolution.
     """
     cell = s2sphere.CellId(cell_id)
-    parent_cell = (cell.parent(resolution) if resolution else cell.parent())
+    parent_cell = cell.parent(resolution) if resolution else cell.parent()
 
     return parent_cell.id()
 
 
 def to_children(cell_id, resolution=None):
     """Returns a string of comma-separated cell IDs of the S2 cell's children
-    for a specific resolution. A child cell is a cell of higher level of detail
-    that is contained by the current cell. Each cell has four direct children by definition.
+    for a specific resolution. A child cell is a cell of higher level of detail that is
+    contained by the current cell. Each cell has four direct children by definition.
 
     By default, this function returns the direct children (where parent resolution
     is children resolution - 1). However, an optional resolution argument can be passed
@@ -51,7 +61,7 @@ def to_children(cell_id, resolution=None):
     the power of four per zoom level.
     """
     cell = s2sphere.CellId(cell_id)
-    children_cells = (cell.children(resolution) if resolution else cell.children())
+    children_cells = cell.children(resolution) if resolution else cell.children()
     children_cell_ids = ','.join([str(child.id()) for child in children_cells])
 
     return children_cell_ids
@@ -59,21 +69,21 @@ def to_children(cell_id, resolution=None):
 
 def get_cell_bounds(cell_id):
     """Return the vertices of an s2 cell.
-    
+
     TO DO: Shapely can't be imported so we have to figure out a way
     of returning the data: text array, GeoJSON, WKT, etc"""
     cell = s2sphere.Cell(s2sphere.CellId(cell_id))
-    
+
     verts = []
     for i in range(4):
         v = cell.get_vertex(i)
         v_ll = s2sphere.LatLng.from_point(v)
         v_lat = v_ll.lat().degrees
         v_lng = v_ll.lng().degrees
-        
+
         # verts.append((v_lat, v_lng))
         verts.append(str(v_lng) + ',' + str(v_lat))
-    
+
     # bounds_poly = Polygon(verts)
 
     # bounds_geojson = json.dumps(mapping(bounds_poly))
