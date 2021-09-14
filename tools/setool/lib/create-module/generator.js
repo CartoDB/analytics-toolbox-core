142422e88b3c3231fb6dc3a35da06861235558ea
vdelacruzb <vdelacruzb@gmail.com>
2021-09-14 10:52:21 +0200

    add create module

--- tools/setool/lib/create-module/generator.js
+++ tools/setool/lib/create-module/generator.js
@@ -45,7 +45,9 @@ function createModule () {
     createTestUnitIndex();
     createChangelog();
     createMakefile();
-    createPackage();
+    if (cloud === 'bigquery' || cloud === 'snowflake') {
+        createPackage();
+    }
     createReadme();
 }
 
@@ -61,7 +63,11 @@ TODO.`;
 
 function createDocVersion () {
     const project = { bigquery: 'bqcarto', snowflake: 'sfcarto' }[cloud];
-    const content = `### VERSION
+    let content;
+    switch (cloud){
+    case 'bigquery':
+    case 'snowflake':
+        content = `### VERSION
 
 {{% bannerNote type="code" %}}
 ${name}.VERSION()
@@ -83,23 +89,62 @@ Returns the current version of the ${name} module.
 SELECT ${project}.${name}.VERSION();
 -- 1.0.0
 \`\`\``;
+        break;
+    case 'redshift':
+        content = `### VERSION
+
+{{% bannerNote type="code" %}}
+${name}.VERSION()
+{{%/ bannerNote %}}
+
+**Description**
+
+Returns the current version of the ${name} module.
+
+**Return type**
+
+\`VARCHAR\`
+
+{{% customSelector %}}
+**Example**
+{{%/ customSelector %}}
+
+\`\`\`sql
+SELECT ${name}.VERSION();
+-- 1.0.0
+\`\`\``;
+        break;
+    }
 
     createFile([root, 'modules', name, cloud, 'doc', 'VERSION.md'], content);
 }
 
 function createLibIndex () {
-    const content = `import { version }  from '../package.json';
+    const filename = { bigquery: 'index.js', snowflake: 'index.js', redshift: '__init__.py' }[cloud];
+    let content = '';
+    switch (cloud){
+    case 'bigquery':
+    case 'snowflake':
+        content = `import { version }  from '../package.json';
 
 export default {
     version
 };`;
 
-    createFile([root, 'modules', name, cloud, 'lib', 'index.js'], content);
+        createFile([root, 'modules', name, cloud, 'lib', 'index.js'], content);
+        break;
+    case 'redshift':
+        content = '__version__ = \'1.0.0\'';  
+        break;
+    }
+
+    createFile([root, 'modules', name, cloud, 'lib', filename], content);
 }
 
 function createSQLVersion () {
     let content = '';
-    if (cloud === 'bigquery') {
+    switch (cloud){
+    case 'bigquery':
         content = `${header}
 
 CREATE OR REPLACE FUNCTION \`@@BQ_PREFIX@@${name}.VERSION\`
@@ -111,8 +156,8 @@ OPTIONS (library=["@@BQ_LIBRARY_BUCKET@@"])
 AS """
     return ${name}Lib.version;
 """;`;
-    }
-    if (cloud === 'snowflake') {
+        break;
+    case 'snowflake':
         content = `${header}
 
 CREATE OR REPLACE SECURE FUNCTION @@SF_PREFIX@@${name}.VERSION
@@ -124,6 +169,19 @@ AS $$
     
     return ${name}Lib.version;
 $$;`;
+        break;
+    case 'redshift':
+        content = `${header}
+
+CREATE OR REPLACE FUNCTION @@RS_PREFIX@@${name}.VERSION
+() 
+RETURNS VARCHAR
+IMMUTABLE
+AS $$
+    from @@RS_PREFIX@@${name}Lib import __version__
+    return __version__
+$$ LANGUAGE plpythonu;`;
+        break;
     }
 
     createFile([root, 'modules', name, cloud, 'sql', 'VERSION.sql'], content);
@@ -152,10 +210,15 @@ DROP SHARE @@SF_SHARE@@;`;
 }
 
 function createTestIntegrationVersion () {
+    const filename = { bigquery: 'VERSION.test.js', snowflake: 'VERSION.test.js', redshift: 'test_VERSION.py' }[cloud];
     const cover = { bigquery: '`', snowflake: '' }[cloud];
     const variable = { bigquery: 'v', snowflake: 'V' }[cloud];
-    const prefix = { bigquery: 'BQ_PREFIX', snowflake: 'SF_PREFIX' }[cloud];
-    const content = `const { runQuery } = require('../../../../../common/${cloud}/test-utils');
+    const prefix = { bigquery: 'BQ_PREFIX', snowflake: 'SF_PREFIX', redshift: 'RS_PREFIX' }[cloud];
+    let content = '';
+    switch (cloud){
+    case 'bigquery':
+    case 'snowflake':
+        content = `const { runQuery } = require('../../../../../common/${cloud}/test-utils');
 const version = require('../../package.json').version;
 
 test('VERSION returns the proper version', async () => {
@@ -164,19 +227,78 @@ test('VERSION returns the proper version', async () => {
     expect(rows.length).toEqual(1);
     expect(rows[0].${variable}).toEqual(version);
 });`;
+        break;
+    case 'redshift':
+        content = `import os
+import sys
 
-    createFile([root, 'modules', name, cloud, 'test', 'integration', 'VERSION.test.js'], content);
+# Include this to allow importing lib from source code
+sys.path.insert(
+    1, os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..')
+)
+sys.path.insert(
+    1,
+    os.path.join(
+        os.path.dirname(os.path.realpath(__file__)),
+        '..',
+        '..',
+        '..',
+        '..',
+        '..',
+        'common',
+        'redshift',
+    ),
+)`;
+
+        createFile([root, 'modules', name, cloud, 'test', 'integration', '__init__.py'], content);
+
+        content = `from lib import __version__
+from test_utils import run_query
+
+
+def test_version():
+    result = run_query('SELECT @@${prefix}@@${name}.VERSION()')
+    assert result[0][0] == __version__`;
+        break;
+    }
+
+    createFile([root, 'modules', name, cloud, 'test', 'integration', filename], content);
 }
 
 function createTestUnitIndex () {
-    const content = `const ${name}Lib = require('../../dist/index');
+    const filename = { bigquery: 'VERSION.test.js', snowflake: 'VERSION.test.js', redshift: 'test_VERSION.py' }[cloud];
+    let content = '';
+    switch (cloud){
+    case 'bigquery':
+    case 'snowflake':
+        content = `const ${name}Lib = require('../../dist/index');
 const version = require('../../package.json').version;
 
 test('${name} library defined', () => {
     expect(${name}Lib.version).toBe(version);
 });`;
+        break;     
+    case 'redshift':
+        content = `import os
+import sys
 
-    createFile([root, 'modules', name, cloud, 'test', 'unit', 'index.test.js'], content);
+# Include this to allow importing lib from source code
+sys.path.insert(
+    1, os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..')
+)`;
+
+        createFile([root, 'modules', name, cloud, 'test', 'unit', '__init__.py'], content);
+
+        content = `from lib import ${name}Lib, __version__
+
+
+def test_init():
+    assert ${name}Lib.__version__ == __version__`;
+        break;
+        
+    }
+
+    createFile([root, 'modules', name, cloud, 'test', 'unit', filename], content);
 }
 
 function createChangelog () {
@@ -222,7 +344,7 @@ function createPackage () {
 
 function createReadme () {
     const cname = capitalize(name);
-    const ccloud = { bigquery: 'BigQuery', snowflake: 'Snowflake' }[cloud];
+    const ccloud = { bigquery: 'BigQuery', snowflake: 'Snowflake', redshift: 'Redshift' }[cloud];
     const content = `# ${cname} module for ${ccloud}
 
 TODO: add module description.`;
