a0399da0ad06b8e4417d87614281978da8f04b0f
alex-graciano <79551116+alex-graciano@users.noreply.github.com>
2021-09-16 09:36:10 +0200

    Improve Python libraries deployment in Redshift (#167)
    Co-authored-by: Jesús Arroyo Torrens <jarroyo@cartodb.com>

--- common/redshift/libraryinstaller.sh
+++ common/redshift/libraryinstaller.sh
@@ -4,7 +4,7 @@ set -e
 # It is based on the repository https://github.com/aws-samples/amazon-redshift-udfs
 
 function usage {
-	echo "./libraryInstaller.sh -m <module> -f <requirement_file>"
+	echo "./libraryinstaller.sh -m <module> -f <requirement_file>"
 	echo
 	echo "where <module> is the name of the Pip module to be installed. The next environment variables should be set:"
 	echo "      AWS_S3_BUCKET is the location on S3 to upload the artifact to. Must be in format s3://bucket/prefix/"
@@ -35,31 +35,67 @@ function notNull {
 
 execQuery()
 {
-  output=`aws redshift-data execute-statement --cluster-identifier $1 --database $2 --db-user $3 --sql "$4" --region $5`
-  id=`echo $output | jq -r .Id`
+	output=`aws redshift-data execute-statement --cluster-identifier $1 --database $2 --db-user $3 --sql "$4" --region $5`
+	id=`echo $output | jq -r .Id`
 
-  status="SUBMITTED"
-  while [ "$status" != "FINISHED" ] && [ "$status" != "FAILED" ]
-  do
-    sleep 1
-    status=`aws redshift-data describe-statement --id $id --region $5 | jq -r .Status`
-  done
+	status="SUBMITTED"
+	while [ "$status" != "FINISHED" ] && [ "$status" != "FAILED" ]
+	do
+		sleep 1
+		output=`aws redshift-data describe-statement --id $id --region $5`
+		status=`echo $output | jq -r .Status`
+		has_results=`echo $output | jq -r .HasResultSet`
+	done
+
 	if [ "$status" == "FAILED" ]; then
-    aws redshift-data describe-statement --id $id --region $5
-    return 1
-  else
-    echo $id:$status
-  fi
+    	aws redshift-data describe-statement --id $id --region $5
+    	exit 1
+  	else
+    	# echo $id:$status
+		if [ "$has_results" == "true" ]; then
+			exec_output=`aws redshift-data get-statement-result --id $id --region $5`
+		fi
+  	fi
+}
+
+libraryInstalled()
+{
+	sql="SELECT * FROM pg_library WHERE name='$1';"
+	execQuery $RS_CLUSTER_ID $RS_DATABASE $RS_USER "$sql" $RS_REGION
+	echo $exec_output | jq -r '.Records | length'
+}
+
+libraryVersion()
+{
+	timestamp=$(date +%s)
+	module=$1
+	function="v$timestamp"
+	# Map Python modules
+	[ "$module" = "python_dateutil" ] && module="dateutil"
+	sql="CREATE OR REPLACE FUNCTION public.$function() RETURNS VARCHAR IMMUTABLE AS \$\$
+    	from $module import __version__
+    	return __version__
+	\$\$ LANGUAGE plpythonu;"
+	execQuery $RS_CLUSTER_ID $RS_DATABASE $RS_USER "$sql" $RS_REGION
+
+	sql="SELECT public.$function();"
+	execQuery $RS_CLUSTER_ID $RS_DATABASE $RS_USER "$sql" $RS_REGION
+	library_version=`echo $exec_output | jq -r '.Records[0][0].stringValue'`
+
+	sql="DROP FUNCTION public.$function();"
+	execQuery $RS_CLUSTER_ID $RS_DATABASE $RS_USER "$sql" $RS_REGION
+
+	echo $library_version
 }
 
 # make sure we have pip and the aws cli installed
 checkDep "aws"
-checkDep "pip3"
+checkDep "pip"
 
 # make sure we have wheel installed into pip
-pip3 show wheel &> /dev/null
+pip show wheel &> /dev/null
 if [ $? != 0 ]; then
-  echo "pip3 wheel not found. Please install with 'sudo pip install wheel'"
+  echo "pip wheel not found. Please install with 'sudo pip install wheel'"
   exit -1
 fi
 
@@ -86,7 +122,6 @@ notNull "$AWS_SECRET_ACCESS_KEY" "Please provide an AWS secret access key ID usi
 
 # check that the s3 prefix is in the right format
 # starts with 's3://'
-
 if ! [[ $AWS_S3_BUCKET == s3:\/\/* ]]; then
 	echo "S3 Prefix must start with 's3://'"
 	echo
@@ -95,16 +130,21 @@ fi
 
 # extract modules from requirement file
 modules_to_install=($module)
-while IFS="==" read -r m v || [ -n "$m" ]
-do
+while IFS= read -r m v || [ -n "$m" ]; do
   modules_to_install+=($m)
 done < $modules_file
 
 # found the modules - install to a local hidden directory
-for m in "${modules_to_install[@]}"
-do
-   : 
-	echo "Installing $m with pip and uploading to $AWS_S3_BUCKET"
+for m in "${modules_to_install[@]}"; do
+    redshift_default_libraries='numpy pandas python-dateutil pytz scipy six wsgiref';
+
+	if [[ $redshift_default_libraries == *${m%%==*}* ]]; then
+		echo "Skipping ${m%%==*}"
+		echo "- Library already installed in RedShift"
+		continue
+	fi
+
+	echo "Installing $m in $RS_CLUSTER_ID"
 	
 	TMPDIR=.tmp
 	if [ ! -d "$TMPDIR" ]; then
@@ -115,21 +155,48 @@ do
 	
 	mkdir "$TMPDIR/.$m"
 	
-	pip3 wheel $m --wheel-dir "$TMPDIR/.$m"
+	pip wheel $m --wheel-dir "$TMPDIR/.$m" &> /dev/null
 	if [ $? != 0 ]; then
 		echo "Unable to find module $m in pip."
 		rm -Rf "$TMPDIR/.$m"
 		exit $?
 	fi
 	
-	
 	files=`ls ${TMPDIR}/.${m}/*.whl`
-	for depname in `basename -s .whl $files`
-	do
-		echo $depname
-		aws s3 cp "$TMPDIR/.$m/$depname.whl" "$AWS_S3_BUCKET/$depname.zip"
-		sql="CREATE OR REPLACE LIBRARY ${depname%%-*} LANGUAGE plpythonu FROM '$AWS_S3_BUCKET/$depname.zip' WITH CREDENTIALS 'aws_access_key_id=$AWS_ACCESS_KEY_ID;aws_secret_access_key=$AWS_SECRET_ACCESS_KEY'; "
-	    execQuery $RS_CLUSTER_ID $RS_DATABASE $RS_USER "$sql" $RS_REGION
+	for depname in `basename -s .whl $files`; do
+		depname=$(echo "$depname" | tr '[:upper:]' '[:lower:]')
+		if [[ $redshift_default_libraries == *${depname%%-*}* ]]; then
+			echo "Skipping ${depname%%-*}"
+			echo "- Library already installed in RedShift"
+			continue
+		fi
+		echo "> Library to be installed: $depname"
+		# check library installed
+		library_installed=`libraryInstalled ${depname%%-*}`
+		if [ $library_installed == 0 ]; then
+			echo "- Library not found in the cluster"
+			echo "- Installing $depname"
+			aws s3 cp "$TMPDIR/.$m/$depname.whl" "$AWS_S3_BUCKET$depname.zip"
+			sql="CREATE OR REPLACE LIBRARY ${depname%%-*} LANGUAGE plpythonu FROM '$AWS_S3_BUCKET$depname.zip' WITH CREDENTIALS 'aws_access_key_id=$AWS_ACCESS_KEY_ID;aws_secret_access_key=$AWS_SECRET_ACCESS_KEY';"
+	    	execQuery $RS_CLUSTER_ID $RS_DATABASE $RS_USER "$sql" $RS_REGION
+			echo "- Done"
+		else
+			# check library version
+			library_version=`libraryVersion ${depname%%-*}`
+			if [[ $depname == ${depname%%-*}-$library_version-* ]]; then
+				echo "- Library already installed"
+			else
+				echo "- Library installed with different version: $library_version"
+				echo "- Installing $depname"
+				sql="DROP LIBRARY ${depname%%-*};"
+	    		execQuery $RS_CLUSTER_ID $RS_DATABASE $RS_USER "$sql" $RS_REGION
+				aws s3 cp "$TMPDIR/.$m/$depname.whl" "$AWS_S3_BUCKET$depname.zip"
+				sql="CREATE OR REPLACE LIBRARY ${depname%%-*} LANGUAGE plpythonu FROM '$AWS_S3_BUCKET$depname.zip' WITH CREDENTIALS 'aws_access_key_id=$AWS_ACCESS_KEY_ID;aws_secret_access_key=$AWS_SECRET_ACCESS_KEY';"
+				execQuery $RS_CLUSTER_ID $RS_DATABASE $RS_USER "$sql" $RS_REGION
+				echo "- Done"
+			fi
+		fi
+		
 		if [ $? != 0 ]; then
 			rm -Rf "$TMPDIR/.$m"
 			exit $?
