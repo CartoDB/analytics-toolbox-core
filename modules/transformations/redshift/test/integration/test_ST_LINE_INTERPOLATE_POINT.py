8b9c6b4359b693e77a32caf9e2b001031bb3406a
Alex Graciano <agraciano@cartodb.com>
2021-09-29 16:30:35 +0200

    Remove ST_LINE_INTERPOLATE_POINT since it's already implemented

--- modules/transformations/redshift/test/integration/test_ST_LINE_INTERPOLATE_POINT.py
+++
@@ -1,6 +0,0 @@
-from test_utils import run_query, redshift_connector
-
-
-def test_st_line_interpolate_point():
-    result = run_query('SELECT @@RS_PREFIX@@transformations.ST_LINE_INTERPOLATE_POINT(geog, distance, units)')
-    assert result[0][0] == 
