1d49e898a83b06d81fb8d3c494bca32e062b5830
François BAPTISTE <7817985+francois-baptiste@users.noreply.github.com>
2021-09-30 16:31:24 +0200

    Boundary issue with quadint 0

--- modules/quadkey/bigquery/sql/ST_BOUNDARY.sql
+++ modules/quadkey/bigquery/sql/ST_BOUNDARY.sql
@@ -5,26 +5,35 @@
 CREATE OR REPLACE FUNCTION `@@BQ_PREFIX@@quadkey.ST_BOUNDARY`(quadint INT64)
 RETURNS GEOGRAPHY
 AS (
-    COALESCE(
-        ST_MAKEPOLYGON(
-            ST_MAKELINE([
-                ST_GEOGPOINT(
-                    `@@BQ_PREFIX@@quadkey.__BBOX_E`(quadint),
-                    `@@BQ_PREFIX@@quadkey.__BBOX_N`(quadint)),
-                ST_GEOGPOINT(
-                    `@@BQ_PREFIX@@quadkey.__BBOX_E`(quadint),
-                    `@@BQ_PREFIX@@quadkey.__BBOX_S`(quadint)),
-                ST_GEOGPOINT(
-                    `@@BQ_PREFIX@@quadkey.__BBOX_W`(quadint),
-                    `@@BQ_PREFIX@@quadkey.__BBOX_S`(quadint)),
-                ST_GEOGPOINT(
-                    `@@BQ_PREFIX@@quadkey.__BBOX_W`(quadint),
-                    `@@BQ_PREFIX@@quadkey.__BBOX_N`(quadint)),
-                ST_GEOGPOINT(
-                    `@@BQ_PREFIX@@quadkey.__BBOX_E`(quadint),
-                    `@@BQ_PREFIX@@quadkey.__BBOX_N`(quadint))
-                ])
-            ),
-        ERROR('NULL argument passed to UDF')
-    )
+    CASE
+    -- Deal with level 0 boundary issue.
+      WHEN quadint=0 THEN ST_GeogFromText('FULLGLOBE')
+    -- Deal with level 1. Prevent error from antipodal vertices.
+      WHEN quadint=1 THEN ST_GeogFromText('POLYGON((0 0,0 90,-180 90,-180 0,-90 0,0 0))')
+      WHEN quadint=33 THEN ST_GeogFromText('POLYGON((180 0,180 90,0 90,0 0,90 0,180 0))')
+      WHEN quadint=65 THEN ST_GeogFromText('POLYGON((0 0,-90 0,-180 0,-180 -90,0 -90,0 0))')
+      WHEN quadint=97 THEN ST_GeogFromText('POLYGON((180 0,90 0,0 0,0 -90,180 -90,180 0))')
+      ELSE COALESCE(
+            SAFE.ST_MAKEPOLYGON(
+                ST_MAKELINE([
+                    ST_GEOGPOINT(
+                        `@@BQ_PREFIX@@quadkey.__BBOX_E`(quadint),
+                        `@@BQ_PREFIX@@quadkey.__BBOX_N`(quadint)),
+                    ST_GEOGPOINT(
+                        `@@BQ_PREFIX@@quadkey.__BBOX_E`(quadint),
+                        `@@BQ_PREFIX@@quadkey.__BBOX_S`(quadint)),
+                    ST_GEOGPOINT(
+                        `@@BQ_PREFIX@@quadkey.__BBOX_W`(quadint),
+                        `@@BQ_PREFIX@@quadkey.__BBOX_S`(quadint)),
+                    ST_GEOGPOINT(
+                        `@@BQ_PREFIX@@quadkey.__BBOX_W`(quadint),
+                        `@@BQ_PREFIX@@quadkey.__BBOX_N`(quadint)),
+                    ST_GEOGPOINT(
+                        `@@BQ_PREFIX@@quadkey.__BBOX_E`(quadint),
+                        `@@BQ_PREFIX@@quadkey.__BBOX_N`(quadint))
+                    ])
+                ),
+            NULL
+        )
+    END
 );
