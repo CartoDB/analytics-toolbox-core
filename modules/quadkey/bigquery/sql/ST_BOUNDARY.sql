fce7ab08861ca2b5fb88641c87b1a87407d3f2d6
François BAPTISTE <7817985+francois-baptiste@users.noreply.github.com>
2021-09-30 19:28:54 +0200

    fix web mercator limit

--- modules/quadkey/bigquery/sql/ST_BOUNDARY.sql
+++ modules/quadkey/bigquery/sql/ST_BOUNDARY.sql
@@ -9,12 +9,100 @@ AS (
     -- Deal with level 0 boundary issue.
       WHEN quadint=0 THEN ST_GeogFromText('FULLGLOBE')
     -- Deal with level 1. Prevent error from antipodal vertices.
-      WHEN quadint=1 THEN ST_GeogFromText('POLYGON((0 0,0 90,-180 90,-180 0,-90 0,0 0))')
-      WHEN quadint=33 THEN ST_GeogFromText('POLYGON((180 0,180 90,0 90,0 0,90 0,180 0))')
-      WHEN quadint=65 THEN ST_GeogFromText('POLYGON((0 0,-90 0,-180 0,-180 -90,0 -90,0 0))')
-      WHEN quadint=97 THEN ST_GeogFromText('POLYGON((180 0,90 0,0 0,0 -90,180 -90,180 0))')
+      WHEN quadint=1 THEN
+            ST_MAKEPOLYGON(
+                ST_MAKELINE([
+                    ST_GEOGPOINT(
+                        0,
+                        0),
+                    ST_GEOGPOINT(
+                        0,
+                        (360/acos(-1)*atan(pow(exp(1),acos(-1))))-90),
+                    ST_GEOGPOINT(
+                        -180,
+                        (360/acos(-1)*atan(pow(exp(1),acos(-1))))-90),
+                    ST_GEOGPOINT(
+                        -180,
+                        0,
+                    ST_GEOGPOINT(
+                        -90,
+                        0),
+                    ST_GEOGPOINT(
+                        0,
+                        0)
+                    ])
+                )
+      WHEN quadint=33 THEN
+            ST_MAKEPOLYGON(
+                ST_MAKELINE([
+                    ST_GEOGPOINT(
+                        180,
+                        0),
+                    ST_GEOGPOINT(
+                        180,
+                        (360/acos(-1)*atan(pow(exp(1),acos(-1))))-90),
+                    ST_GEOGPOINT(
+                        0,
+                        (360/acos(-1)*atan(pow(exp(1),acos(-1))))-90),
+                    ST_GEOGPOINT(
+                        0,
+                        0),
+                    ST_GEOGPOINT(
+                        90,
+                        0),
+                    ST_GEOGPOINT(
+                        180,
+                        0)
+                    ])
+                )
+      WHEN quadint=65 THEN
+            ST_MAKEPOLYGON(
+                ST_MAKELINE([
+                    ST_GEOGPOINT(
+                        0,
+                        0),
+                    ST_GEOGPOINT(
+                        -90,
+                        0,
+                    ST_GEOGPOINT(
+                        -180,
+                        0),
+                    ST_GEOGPOINT(
+                        -180,
+                        90-(360/acos(-1)*atan(pow(exp(1),acos(-1))))),
+                    ST_GEOGPOINT(
+                        0,
+                        90-(360/acos(-1)*atan(pow(exp(1),acos(-1))))),
+                    ST_GEOGPOINT(
+                        180,
+                        0)
+                    ])
+                )
+      WHEN quadint=97 THEN
+            ST_MAKEPOLYGON(
+                ST_MAKELINE([
+                    ST_GEOGPOINT(
+                        180,
+                        0),
+                    ST_GEOGPOINT(
+                        90,
+                        0,
+                    ST_GEOGPOINT(
+                        0,
+                        0),
+                    ST_GEOGPOINT(
+                        0,
+                        90-(360/acos(-1)*atan(pow(exp(1),acos(-1))))),
+                    ST_GEOGPOINT(
+                        180,
+                        90-(360/acos(-1)*atan(pow(exp(1),acos(-1))))),
+                    ST_GEOGPOINT(
+                        180,
+                        0)
+                    ])
+                )
       ELSE COALESCE(
-            SAFE.ST_MAKEPOLYGON(
+            ST_MAKEPOLYGON(
                 ST_MAKELINE([
                     ST_GEOGPOINT(
                         `@@BQ_PREFIX@@quadkey.__BBOX_E`(quadint),
@@ -33,7 +121,7 @@ AS (
                         `@@BQ_PREFIX@@quadkey.__BBOX_N`(quadint))
                     ])
                 ),
-            NULL
+            ERROR('NULL argument passed to UDF')
         )
     END
 );
