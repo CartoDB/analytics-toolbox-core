f635642a8760df3d61ce152708de25ecd0e9115a
Alex Graciano <agraciano@cartodb.com>
2021-10-05 15:37:33 +0200

    Fix linter

--- modules/processing/redshift/test/integration/test_VERSION.py
+++ modules/processing/redshift/test/integration/test_VERSION.py
@@ -5,4 +5,4 @@ exec(open('./lib/_version.py').read())
 
 def test_version():
     result = run_query('SELECT @@RS_PREFIX@@processing.VERSION()')
-    assert result[0][0] == __version__
+    assert result[0][0] == __version__ # noqa
