bb43915f88ec54f442b6667d7ecff912ec65033b
Alex Graciano <agraciano@cartodb.com>
2021-10-05 17:22:21 +0200

    Added suggestions

--- modules/transformations/redshift/lib/__init__.py
+++ modules/transformations/redshift/lib/__init__.py
@@ -1,31 +1,16 @@
 from ._version import __version__  # noqa
+from .center_lib import center_mean, center_median, centroid
+from .center_lib.centroid import centroid_polygon, centroid_linestring
+from .destination import destination
+from .great_circle import great_circle
 
-
-def center_mean(geog):
-    from center_lib import center_mean
-
-    return center_mean(geog)
-
-
-def center_median(geog, n_iter):
-    from center_lib import center_median
-
-    return center_median(geog, n_iter)
-
-
-def centroid(geog):
-    from center_lib import centroid
-
-    return centroid(geog)
-
-
-def great_circle(start_point, end_point, n_points):
-    from great_circle import great_circle
-
-    return great_circle(start_point, end_point, n_points)
-
-
-def destination(geog, distance, bearing, units):
-    from destination import destination
-
-    return destination(geog, distance, bearing, units)
+__all__ = [
+    '__version__',
+    'center_mean',
+    'center_median',
+    'centroid',
+    'centroid_polygon',
+    'centroid_linestring',
+    'great_circle',
+    'destination',
+]
--- modules/transformations/redshift/test/unit/test_init.py
+++ modules/transformations/redshift/test/unit/test_init.py
@@ -9,3 +9,23 @@ def test_init():
     assert hasattr(transformationsLib, 'centroid')
     assert hasattr(transformationsLib, 'destination')
     assert hasattr(transformationsLib, 'great_circle')
+
+
+def test_centroid_linestring():
+    linestring1 = [[0, 0], [10, 0], [10, 10]]
+    linestring2 = [[4, 4], [14, 2], [14, 8]]
+    centroid1 = transformationsLib.centroid_linestring(linestring1)
+    centroid2 = transformationsLib.centroid_linestring(linestring2)
+
+    assert str(centroid1) == '{"coordinates": [7.5, 2.5], "type": "Point"}'
+    assert str(centroid2) == '{"coordinates": [10.852076, 3.74083], "type": "Point"}'
+
+
+def test_centroid_polygon():
+    polygon1 = [[0, 0], [10, 0], [10, 10], [0, 10], [0, 0]]
+    polygon2 = [[4, 4], [14, 2], [14, 8], [4, 6], [4, 4]]
+    centroid1 = transformationsLib.centroid_polygon(polygon1)
+    centroid2 = transformationsLib.centroid_polygon(polygon2)
+
+    assert str(centroid1) == '{"coordinates": [5.0, 5.0], "type": "Point"}'
+    assert str(centroid2) == '{"coordinates": [9.833333, 5.0], "type": "Point"}'
