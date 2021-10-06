3caaab7a534c5ded6ec335d263c7b50d4063741a
Alex Graciano <agraciano@cartodb.com>
2021-10-06 11:36:06 +0200

    Fix precision in unit tests

--- modules/transformations/redshift/test/unit/test_init.py
+++ modules/transformations/redshift/test/unit/test_init.py
@@ -14,18 +14,20 @@ def test_init():
 def test_centroid_linestring():
     linestring1 = [[0, 0], [10, 0], [10, 10]]
     linestring2 = [[4, 4], [14, 2], [14, 8]]
-    centroid1 = transformationsLib.centroid_linestring(linestring1)
-    centroid2 = transformationsLib.centroid_linestring(linestring2)
+    n_precision = transformationsLib.PRECISION
+    centroid1 = transformationsLib.centroid_linestring(linestring1, n_precision)
+    centroid2 = transformationsLib.centroid_linestring(linestring2, n_precision)
 
     assert str(centroid1) == '{"coordinates": [7.5, 2.5], "type": "Point"}'
-    assert str(centroid2) == '{"coordinates": [10.852076, 3.74083], "type": "Point"}'
+    assert str(centroid2) == '{"coordinates": [10.852076041405398, 3.740830416562159], "type": "Point"}'
 
 
 def test_centroid_polygon():
     polygon1 = [[0, 0], [10, 0], [10, 10], [0, 10], [0, 0]]
     polygon2 = [[4, 4], [14, 2], [14, 8], [4, 6], [4, 4]]
-    centroid1 = transformationsLib.centroid_polygon(polygon1)
-    centroid2 = transformationsLib.centroid_polygon(polygon2)
+    n_precision = transformationsLib.PRECISION
+    centroid1 = transformationsLib.centroid_polygon(polygon1, n_precision)
+    centroid2 = transformationsLib.centroid_polygon(polygon2, n_precision)
 
     assert str(centroid1) == '{"coordinates": [5.0, 5.0], "type": "Point"}'
-    assert str(centroid2) == '{"coordinates": [9.833333, 5.0], "type": "Point"}'
+    assert str(centroid2) == '{"coordinates": [9.833333333333334, 5.0], "type": "Point"}'
