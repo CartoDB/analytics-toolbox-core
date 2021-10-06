8cd0d1f8e6488f6e166262174861646e12160532
vdelacruzb <vdelacruzb@gmail.com>
2021-10-06 15:14:12 +0200

    use relative paths on lib

--- modules/constructors/redshift/lib/__init__.py
+++ modules/constructors/redshift/lib/__init__.py
@@ -1,8 +1,8 @@
 # Copyright (c) 2021, CARTO
 
 from ._version import __version__  # noqa
-from ellipse import ellipse
-from bezier_spline import bezier_spline
+from .ellipse import ellipse
+from .bezier_spline import bezier_spline
 
 
 __all__ = ['__version__', 'ellipse', 'bezier_spline']
