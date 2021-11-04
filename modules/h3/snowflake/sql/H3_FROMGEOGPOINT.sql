523445326782d48d99fb328f1fba9802f6925888
vdelacruzb <vdelacruzb@gmail.com>
2021-11-04 09:51:54 +0100

    merge master

--- modules/h3/snowflake/sql/H3_FROMGEOGPOINT.sql
+++ modules/h3/snowflake/sql/H3_FROMGEOGPOINT.sql
@@ -7,12 +7,7 @@ CREATE OR REPLACE SECURE FUNCTION H3_FROMGEOGPOINT
 RETURNS STRING
 IMMUTABLE
 AS $$
-<<<<<<< HEAD:modules/h3/snowflake/sql/H3_FROMGEOGPOINT.sql
     IFF(ST_NPOINTS(geog) = 1, 
         H3_FROMLONGLAT(ST_X(GEOG), ST_Y(GEOG), CAST(RESOLUTION AS DOUBLE)),
-=======
-    IFF(ST_NPOINTS(geog) = 1,
-        @@SF_PREFIX@@h3.LONGLAT_ASH3(ST_X(GEOG), ST_Y(GEOG), CAST(RESOLUTION AS DOUBLE)),
->>>>>>> master:modules/h3/snowflake/sql/ST_ASH3.sql
         null)
 $$;
