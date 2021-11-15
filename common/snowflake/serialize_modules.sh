8acff1c6fd19ca6720bc31027f009ff4dd056925
vdelacruzb <vdelacruzb@gmail.com>
2021-11-15 09:47:15 +0100

    custom installation package file name

--- common/snowflake/serialize_modules.sh
+++ common/snowflake/serialize_modules.sh
@@ -9,13 +9,14 @@ SCRIPT_DIR=$( dirname "$0" )
 
 SOURCE_DIR=$1
 DIST_DIR=$2
+INSTALLATION_FILE="sf_installation_package.sql"
 
 rm -rf $DIST_DIR
 mkdir -p $DIST_DIR
 
 for module in `cd $SOURCE_DIR; node scripts/modulesort.js`; do
     echo "> Module $module/$CLOUD"
-    make -C $SOURCE_DIR/modules/$module/$CLOUD serialize-module || exit 1
-    cat $SOURCE_DIR/modules/$module/$CLOUD/dist/module.sql >> $DIST_DIR/modules.sql
-    rm -f $SOURCE_DIR/modules/$module/$CLOUD/dist/module.sql
+    INSTALLATION_FILE=$INSTALLATION_FILE make -C $SOURCE_DIR/modules/$module/$CLOUD serialize-module || exit 1
+    cat $SOURCE_DIR/modules/$module/$CLOUD/dist/$INSTALLATION_FILE >> $DIST_DIR/$INSTALLATION_FILE
+    rm -f $SOURCE_DIR/modules/$module/$CLOUD/dist/$INSTALLATION_FILE
 done
