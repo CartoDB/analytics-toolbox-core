3a1917b352e3c2ea7a9aca6c7fd602e00baa5cdf
vdelacruzb <vdelacruzb@gmail.com>
2021-11-22 13:07:52 +0100

    unify release_installation_package

--- common/snowflake/release_installation_package.sh
+++ common/snowflake/release_installation_package.sh
@@ -12,10 +12,17 @@ PACKAGE_VERSION=$(date +%Y.%m.%d)
 
 echo "Releasing installation package "$PACKAGE_VERSION" in $PACKAGE_BUCKET"
 
-SCRIPT_DIR=$(dirname "$0")
-
-ROOT_DIR=$SCRIPT_DIR/../..
-DIST_DIR=$ROOT_DIR/dist
+SCRIPT_DIR=$( dirname "$0" )
+SCRIPTS_DIR=$SCRIPT_DIR/../../scripts
+echo ${SCRIPTS_DIR}
+if [ -z ${ROOT_DIR} ];
+then
+    ROOT_DIR=$SCRIPT_DIR/../..
+fi
+if [ -z ${DIST_DIR} ];
+then
+    DIST_DIR=$ROOT_DIR/dist
+fi
 
 rm -rf $DIST_DIR
 mkdir -p $DIST_DIR
@@ -26,7 +33,7 @@ echo $PACKAGE_VERSION > $DIST_DIR/version
 # Generate modules.sql
 export CLOUD=snowflake
 export GIT_DIFF=off
-for module in `cd $ROOT_DIR; node scripts/modulesort.js`; do
+for module in `node ${SCRIPTS_DIR}/modulesort.js`; do
     echo -e "\n> Module $module/$CLOUD"
     make -C $ROOT_DIR/modules/$module/$CLOUD serialize-module \
         PACKAGE_VERSION=$PACKAGE_VERSION VERSION_FUNCTION=VERSION_$PACKAGE_TYPE || exit 1
