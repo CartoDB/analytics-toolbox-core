447c1b1f712beff1c20d933ef546870faa8670d0
vdelacruzb <vdelacruzb@gmail.com>
2021-11-23 14:38:47 +0100

    add create_installation_package

--- common/redshift/create_installation_package.sh
+++ common/redshift/create_installation_package.sh
@@ -7,8 +7,7 @@ PACKAGE_NAME="carto-spatial-extension-redshift"
 echo "Creating installation package $PACKAGE_NAME.zip";
 
 SCRIPT_DIR=$( dirname "$0" )
-SCRIPTS_DIR=$SCRIPT_DIR/../../SCRIPTS_DIR
-
+SCRIPTS_DIR=$SCRIPT_DIR/../../scripts
 if [ -z ${ROOT_DIR} ];
 then
     ROOT_DIR=$SCRIPT_DIR/../..
@@ -21,25 +20,14 @@ fi
 rm -rf $DIST_DIR
 mkdir -p $DIST_DIR
 
-
+# Generate modules.sql
+export CLOUD=redshift
+export GIT_DIFF=off
 for module in `node ${SCRIPTS_DIR}/modulesort.js`; do
     echo -e "\n> Module $module/$CLOUD"
     make -C $ROOT_DIR/modules/$module/$CLOUD serialize-module || exit 1
     mv $ROOT_DIR/modules/$module/$CLOUD/dist/*.zip $DIST_DIR
     cat $ROOT_DIR/modules/$module/$CLOUD/dist/module.sql >> $DIST_DIR/modules.sql
     rm -f $ROOT_DIR/modules/$module/$CLOUD/dist/module.sql
-    # mkdir -p $DIST_DIR/lib/$module
+
 done
-
-
-# Generate core modules
-# $SCRIPT_DIR/serialize_modules.sh $ROOT_DIR/core $DIST_DIR/core
-# mv $DIST_DIR/core/*.zip $DIST_DIR
-# cat $DIST_DIR/core/modules.sql >> $DIST_DIR/modules.sql
-# rm -rf $DIST_DIR/core
-
-# # Generate advanced modules
-# $SCRIPT_DIR/serialize_modules.sh $ROOT_DIR $DIST_DIR/advanced
-# mv $DIST_DIR/advanced/*.zip $DIST_DIR
-# cat $DIST_DIR/advanced/modules.sql >> $DIST_DIR/modules.sql
-# rm -rf $DIST_DIR/advanced
