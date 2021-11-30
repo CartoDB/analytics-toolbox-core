3097e780077bb811456cc84249f9fe1354609ff3
vdelacruzb <vdelacruzb@gmail.com>
2021-11-30 11:30:06 +0100

    merge advanced test-utils

--- common/bigquery/test-utils.js
+++ common/bigquery/test-utils.js
@@ -1,16 +1,74 @@
+const fs = require('fs');
+const path = require('path');
+
 const { BigQuery } = require('@google-cloud/bigquery');
 
+const MODULE = process.env.MODULE;
 const BQ_PROJECT = process.env.BQ_PROJECT;
 const BQ_DATASET_PREFIX = process.env.BQ_DATASET_PREFIX;
 const BQ_PREFIX = `${BQ_PROJECT}.${BQ_DATASET_PREFIX}`;
+const BQ_DATASET = `${BQ_DATASET_PREFIX}${MODULE}`;
 
 const client = new BigQuery({ projectId: `${BQ_PROJECT}` });
 
 async function runQuery (query, options) {
     options = Object.assign({}, { 'timeoutMs' : 30000 }, options);
-    query = query.replace(/@@BQ_PREFIX@@/g, BQ_PREFIX);
+    query = replaceBQPrefix(query);
     const [rows] = await client.query(query, options);
-    return rows
+    return rows;
+}
+
+function replaceBQPrefix(text) {
+    return text.replace(/@@BQ_PREFIX@@/g, BQ_PREFIX)
+        .replace(/@@BQ_PROJECT@@/g, BQ_PROJECT)
+        .replace(/@@BQ_DATASET_PREFIX@@/g, BQ_DATASET_PREFIX);
+}
+
+async function loadTable (tablename, filepath, columns, viewName) {
+    const metadata = {
+        sourceFormat: 'CSV',
+        skipLeadingRows: 1,
+        schema: { fields: columns },
+        writeDisposition: 'WRITE_TRUNCATE'
+    };
+
+    await client.dataset(BQ_DATASET).table(tablename).load(filepath, metadata);
+    if (viewName) {
+        await createView(viewName, `SELECT * FROM \`${BQ_DATASET}.${tablename}\``);
+    }
+}
+
+async function createView (viewName, query) {
+    const options = {
+        view: query
+    };
+    await client.dataset(BQ_DATASET).createTable(viewName, options);
+}
+
+async function deleteTable (tablename) {
+    const table = client.dataset(BQ_DATASET).table(tablename);
+    const exists = await table.exists();
+    if (exists[0]) {
+        await table.delete();
+    }
+}
+
+function readJSONFixture (name) {
+    const filepath = path.join('.', 'test', 'integration', 'fixtures', `${name}.json`);
+    return JSON.parse(fs.readFileSync(filepath));
+}
+
+function writeJSONFixture (name, json) {
+    const filepath = path.join('.', 'test', 'integration', 'fixtures', `${name}.json`);
+    const data = JSON.stringify(json);
+    fs.writeFileSync(filepath, data);
+}
+
+async function cancelJob (jobId) {
+    const job = bigquery.job(jobId);
+    // Attempt to cancel job
+    const [apiResult] = await job.cancel();
+    return apiResult;
 }
 
 function sortByKey (list, key) {
@@ -19,5 +77,11 @@ function sortByKey (list, key) {
 
 module.exports = {
     runQuery,
+    loadTable,
+    createView,
+    deleteTable,
+    readJSONFixture,
+    writeJSONFixture,
+    cancelJob,
     sortByKey
-}
+};
