503f15e3b7f981b159395a8917efde8192c6d956
vdelacruzb <vdelacruzb@gmail.com>
2021-12-02 19:35:52 +0100

    merge remove spatial catalog initialization

--- common/bigquery/sync_spatial_catalog.sh
+++
@@ -1,6 +0,0 @@
-SCRIPT_DIR=$( dirname "$0" )
-
-for f in $SCRIPT_DIR/spatial_catalog/*.sql; do \
-  echo 'Executing ' $f
-  sed -e "s!@@BQ_PROJECT@@!$BQ_PROJECT!g" -e "s!@@BQ_CONNECTION_SPATIAL_CATALOG@@!$BQ_CONNECTION_SPATIAL_CATALOG!g" $f | bq query --project_id=$BQ_PROJECT --use_legacy_sql=false
-done
