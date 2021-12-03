62a6f57d6504fc665eb91538e8a2cb4a054d0ec2
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2021-12-03 06:52:52 +0100

    Include drop procedures in installation package

--- common/snowflake/create_installation_package.sh
+++ common/snowflake/create_installation_package.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 
-# Script to release an Analytics Toolbox package for Snowflake
+# Script to create an installation package for Snowflake
 
 # * PACKAGE_TYPE
-# * PACKAGE_BUCKET
 
 #PACKAGE_TYPE=CORE
-#PACKAGE_BUCKET=gs://carto-analytics-toolbox-core/snowflake
 
 PACKAGE_VERSION=$(date +%Y.%m.%d)
 
@@ -39,12 +37,3 @@ for module in `node ${SCRIPTS_DIR}/modulesort.js`; do
     cat $ROOT_DIR/modules/$module/$CLOUD/dist/module.sql >> $DIST_DIR/modules.sql
     rm -f $ROOT_DIR/modules/$module/$CLOUD/dist/module.sql
 done
-
-if [ -n ${MAKE_RELEASE} ];
-then
-    # Upload the package to the bucket
-    gsutil -h "Content-Type:text/plain" cp $DIST_DIR/version $PACKAGE_BUCKET/latest/
-    gsutil -h "Content-Type:application/sql" cp $DIST_DIR/modules.sql $PACKAGE_BUCKET/latest/
-    #gsutil -h "Content-Type:text/plain" cp $DIST_DIR/version $PACKAGE_BUCKET/$PACKAGE_VERSION/
-    #gsutil -h "Content-Type:application/sql" cp $DIST_DIR/modules.sql $PACKAGE_BUCKET/$PACKAGE_VERSION/
-fi
