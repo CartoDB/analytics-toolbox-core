4cdf4a31b3b335f87e87dbeb81f7bd7b1394b73f
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2021-12-15 08:47:31 +0100

    Set default PACKAGE_TYPE

--- common/snowflake/create_installation_package.sh
+++ common/snowflake/create_installation_package.sh
@@ -2,6 +2,7 @@
 
 # Script to create an installation package for Snowflake
 
+export PACKAGE_TYPE=${PACKAGE_TYPE:=CORE}
 PACKAGE_VERSION=$(date +%Y.%m.%d)
 
 echo "Creating installation package $PACKAGE_VERSION"
@@ -30,7 +31,10 @@ for module in `node ${SCRIPTS_DIR}/modulesort.js`; do
     echo -e "\n> Module $module/$CLOUD"
     make -C $ROOT_DIR/modules/$module/$CLOUD serialize-module \
         PACKAGE_VERSION=$PACKAGE_VERSION || exit 1
-    cat $ROOT_DIR/modules/$module/$CLOUD/dist/module-header.sql > $DIST_DIR/modules-header.sql
+    touch $DIST_DIR/modules-header.sql
+    if [ "$PACKAGE_TYPE" = "CORE" ]; then
+        cat $ROOT_DIR/modules/$module/$CLOUD/dist/module-header.sql > $DIST_DIR/modules-header.sql
+    fi
     cat $ROOT_DIR/modules/$module/$CLOUD/dist/module.sql >> $DIST_DIR/modules-content.sql
     cat $ROOT_DIR/modules/$module/$CLOUD/dist/module-footer.sql > $DIST_DIR/modules-footer.sql
 done
