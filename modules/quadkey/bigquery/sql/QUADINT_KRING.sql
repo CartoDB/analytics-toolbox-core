8c26ea339a98503c781d648785ebaa64c969ac5e
François BAPTISTE <7817985+francois-baptiste@users.noreply.github.com>
2022-01-05 10:27:04 +0100

    fix wrap error for kring, convert in pure sql

--- modules/quadkey/bigquery/sql/QUADINT_KRING.sql
+++ modules/quadkey/bigquery/sql/QUADINT_KRING.sql
@@ -2,18 +2,21 @@
 -- Copyright (C) 2021 CARTO
 ----------------------------
 
+CREATE OR REPLACE FUNCTION `@@BQ_PREFIX@@carto.__ZXY_KRING`
+  (origin STRUCT<z INT64, x INT64, y INT64>, size INT64)
+AS ((
+    SELECT
+      ARRAY_AGG((`@@BQ_PREFIX@@carto.QUADINT_FROMZXY`(origin.z,
+              IF(origin.x+dx<0,origin.x+dx+(1 << origin.z),origin.x+dx),
+              origin.y+dy)))
+    FROM
+      UNNEST(GENERATE_ARRAY(-size,size)) dx,
+      UNNEST(GENERATE_ARRAY(-size,size)) dy
+    WHERE origin.y+dy >= 0 and origin.y+dy < (1 << origin.z)
+));
+
 CREATE OR REPLACE FUNCTION `@@BQ_PREFIX@@carto.QUADINT_KRING`
 (origin INT64, size INT64)
-RETURNS ARRAY<INT64>
-DETERMINISTIC
-LANGUAGE js
-OPTIONS (library=["@@BQ_LIBRARY_BUCKET@@"])
-AS """
-    if (origin == null || origin <= 0) {
-        throw new Error('Invalid input origin')
-    }
-    if (size == null || size < 0) {
-        throw new Error('Invalid input size')
-    }
-    return quadkeyLib.kRing(origin, Number(size));
-""";
+AS (
+    `@@BQ_PREFIX@@carto.__ZXY_KRING`(`@@BQ_PREFIX@@carto.QUADINT_TOZXY`(origin),size)
+);
