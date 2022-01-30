f0d26ecddece678337a16b1dbbc528846151306a
François BAPTISTE <7817985+francois-baptiste@users.noreply.github.com>
2022-01-31 00:26:54 +0100

    add H3_CENTER

--- modules/h3/bigquery/lib/index.js
+++ modules/h3/bigquery/lib/index.js
@@ -1,5 +1,5 @@
 import { version }  from '../package.json';
-import { geoToH3, compact, h3Distance, h3GetResolution, h3IsValid, hexRing, h3IsPentagon, kRing, kRingDistances, polyfill, h3ToGeoBoundary, h3ToChildren, h3ToParent, uncompact } from 'h3-js';
+import { geoToH3, compact, h3Distance, h3GetResolution, h3IsValid, hexRing, h3IsPentagon, kRing, kRingDistances, polyfill, h3ToGeo, h3ToGeoBoundary, h3ToChildren, h3ToParent, uncompact } from 'h3-js';
 
 export default {
     geoToH3,
@@ -12,6 +12,7 @@ export default {
     kRing,
     kRingDistances,
     polyfill,
+    h3ToGeo,
     h3ToGeoBoundary,
     h3ToChildren,
     h3ToParent,
