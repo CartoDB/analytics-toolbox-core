df790f21856e3b41659605758246bc86c085caa6
arredond <alvaroarredondoparra@gmail.com>
2022-02-15 13:00:44 +0100

    add H3_RESOLUTION function (#208)
    Co-authored-by: François BAPTISTE <7817985+francois-baptiste@users.noreply.github.com>
    Co-authored-by: vdelacruzb <vdelacruzb@gmail.com>
    Co-authored-by: Jesús Arroyo Torrens <jarroyo@cartodb.com>

--- modules/h3/bigquery/CHANGELOG.md
+++ modules/h3/bigquery/CHANGELOG.md
@@ -3,6 +3,12 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
 
+## [1.2.0] - 2022-02-15
+
+### Added
+- Add H3_CENTER function.
+- Add H3_RESOLUTION function.
+
 ## [1.1.0] - 2021-12-16
 
 ### Changed
--- modules/h3/bigquery/lib/index.js
+++ modules/h3/bigquery/lib/index.js
@@ -1,16 +1,18 @@
 import { version }  from '../package.json';
-import { geoToH3, compact, h3Distance, h3IsValid, hexRing, h3IsPentagon, kRing, kRingDistances, polyfill, h3ToGeoBoundary, h3ToChildren, h3ToParent, uncompact } from 'h3-js';
+import { geoToH3, compact, h3Distance, h3GetResolution, h3IsValid, hexRing, h3IsPentagon, kRing, kRingDistances, polyfill, h3ToGeo, h3ToGeoBoundary, h3ToChildren, h3ToParent, uncompact } from 'h3-js';
 
 export default {
     geoToH3,
     compact,
     h3Distance,
+    h3GetResolution,
     h3IsValid,
     hexRing,
     h3IsPentagon,
     kRing,
     kRingDistances,
     polyfill,
+    h3ToGeo,
     h3ToGeoBoundary,
     h3ToChildren,
     h3ToParent,
