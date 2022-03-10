9daf6c68fb4d513cd930afaa9148d565d490a8ae
Valentin de la Cruz Barquero <6054336+vdelacruzb@users.noreply.github.com>
2022-03-10 14:44:07 +0100

    St tileenvelope returning json varchar in redshift (#223)

--- modules/quadkey/redshift/sql/QUADINT_BOUNDARY.sql
+++ modules/quadkey/redshift/sql/QUADINT_BOUNDARY.sql
@@ -8,21 +8,20 @@ RETURNS VARCHAR(MAX)
 STABLE
 AS $$
     from @@RS_PREFIX@@quadkeyLib import quadint_to_geojson
-    
+    import json
+
     if quadint is None:
         raise Exception('NULL argument passed to UDF')
 
     geojson = quadint_to_geojson(quadint)['geometry']
-    return str(geojson)
+    return json.dumps(geojson)
 $$ LANGUAGE plpythonu;
 
 CREATE OR REPLACE FUNCTION @@RS_PREFIX@@carto.QUADINT_BOUNDARY
 (BIGINT)
 -- (quadint)
-RETURNS VARCHAR
--- RETURNS GEOMETRY
+RETURNS VARCHAR(MAX)
 STABLE
 AS $$
     SELECT @@RS_PREFIX@@carto.__QUADINT_BOUNDARY($1)
-    -- SELECT ST_GEOMFROMGEOJSON(@@RS_PREFIX@@carto.__QUADINT_BOUNDARY($1))
 $$ LANGUAGE sql;
