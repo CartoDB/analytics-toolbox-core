574f8f96381acad0695fb3636e6af6d25d8b6059
vdelacruzb <vdelacruzb@gmail.com>
2022-04-22 17:58:40 +0200

    add drop_functions.sql

---
+++ common/postgres/DROP_FUNCTIONS.sql
@@ -0,0 +1,17 @@
+CREATE OR REPLACE PROCEDURE @@PG_PREFIX@@vdelacruz_carto.__DROP_FUNCTIONS
+(schema_name TEXT)
+AS $$
+DECLARE
+	row RECORD;
+begin
+	FOR row IN SELECT 'DROP ' || routines.routine_type || ' ' || schema_name || '.' || routines.routine_name || '(' || COALESCE(STRING_AGG(parameters.parameter_mode || ' ' || parameters.udt_name,',' ORDER BY parameters.ordinal_position), '') || ');' AS drop_command
+	FROM information_schema.routines
+	    LEFT JOIN information_schema.parameters ON routines.specific_name=parameters.specific_name
+	WHERE routines.specific_schema=schema_name
+	GROUP by routines.routine_type, routines.specific_name, routines.routine_name
+	LOOP
+		EXECUTE row.drop_command;
+	END LOOP;
+END;
+$$ LANGUAGE plpgsql;
+CALL @@PG_PREFIX@@carto.__DROP_FUNCTIONS('@@PG_PREFIX@@carto');
