cd412d72654f998bcf5458cf5443d482af15cd53
Javier Goizueta <jgoizueta@gmail.com>
2022-05-11 21:53:30 +0200

    Remove quadkey unit tests

--- modules/quadkey/bigquery/test/unit/index.test.js
+++ modules/quadkey/bigquery/test/unit/index.test.js
@@ -2,13 +2,5 @@ const quadkeyLib = require('../../dist/index');
 const version = require('../../package.json').version;
 
 test('quadkey library defined', () => {
-    expect(quadkeyLib.bbox).toBeDefined();
-    expect(quadkeyLib.toChildren).toBeDefined();
-    expect(quadkeyLib.quadkeyFromQuadint).toBeDefined();
-    expect(quadkeyLib.quadintFromQuadkey).toBeDefined();
-    expect(quadkeyLib.quadintFromLocation).toBeDefined();
-    expect(quadkeyLib.quadintToGeoJSON).toBeDefined();
-    expect(quadkeyLib.geojsonToQuadints).toBeDefined();
-    expect(quadkeyLib.ZXYFromQuadint).toBeDefined();
     expect(quadkeyLib.version).toBe(version);
 });
