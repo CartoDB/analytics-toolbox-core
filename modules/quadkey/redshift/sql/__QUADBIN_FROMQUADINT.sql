ef4e721b41a92eb9fe9fb2854a7607ba3131ed6a
vdelacruzb <vdelacruzb@gmail.com>
2022-06-13 19:00:18 +0200

    add __quadbin_fromquadint

---
+++ modules/quadkey/redshift/sql/__QUADBIN_FROMQUADINT.sql
@@ -0,0 +1,27 @@
+
+----------------------------
+-- Copyright (C) 2022 CARTO
+----------------------------
+
+CREATE OR REPLACE FUNCTION @@RS_PREFIX@@carto.__QUADBIN_FROMZXY
+(zxy VARCHAR(MAX))
+-- (quadint)
+RETURNS BIGINT
+STABLE
+AS $$
+    from @@RS_PREFIX@@tilerLib import quadbin_from_zxy
+    import json
+
+    zxy = json.loads(zxy)
+
+    return quadbin_from_zxy(zxy['z'], zxy['x'], zxy['y'])
+$$ LANGUAGE plpythonu;
+
+CREATE OR REPLACE FUNCTION @@RS_PREFIX@@carto.__QUADBIN_FROMQUADINT
+(BIGINT)
+-- (quadint)
+RETURNS BIGINT
+STABLE
+AS $$
+    SELECT @@RS_PREFIX@@carto.__QUADBIN_FROMZXY(JSON_SERIALIZE(@@RS_PREFIX@@carto.QUADINT_TOZXY($1)))
+$$ LANGUAGE sql;
