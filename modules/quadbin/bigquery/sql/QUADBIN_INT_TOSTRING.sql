4f9ddc0f5c306550cd1ee4c77141d4e665737a67
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2022-06-21 21:58:33 +0200

    Add int/string conversion functions

---
+++ modules/quadbin/bigquery/sql/QUADBIN_INT_TOSTRING.sql
@@ -0,0 +1,10 @@
+----------------------------
+-- Copyright (C) 2022 CARTO
+----------------------------
+
+CREATE OR REPLACE FUNCTION `@@BQ_PREFIX@@carto.QUADBIN_INT_TOSTRING`
+(quadbin INT64)
+RETURNS STRING
+AS ((
+    SELECT FORMAT('%x', quadbin)
+));
--- modules/quadbin/bigquery/sql/QUADBIN_TOPARENT.sql
+++ modules/quadbin/bigquery/sql/QUADBIN_TOPARENT.sql
@@ -6,5 +6,5 @@ CREATE OR REPLACE FUNCTION `@@BQ_PREFIX@@carto.QUADBIN_TOPARENT`
 (quadbin INT64, resolution INT64)
 RETURNS INT64
 AS ((
-    SELECT (quadbin & ~(0x1F << 52)) | (resolution << 52) | (4503599627370495 >> (resolution * 2))
+    SELECT (quadbin & ~(0x1F << 52)) | (resolution << 52) | (0xFFFFFFFFFFFFF >> (resolution * 2))
 ));
