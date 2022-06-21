d2479081eb7e490c3d5fe0e56d373b718306d33e
Javier Goizueta <jgoizueta@gmail.com>
2022-06-21 18:28:21 +0200

    More test cases

--- modules/quadbin/postgres/test/integration/test_QUADBIN_FROMZXY.py
+++ modules/quadbin/postgres/test/integration/test_QUADBIN_FROMZXY.py
@@ -2,7 +2,17 @@ import pytest
 from test_utils import run_query
 import json
 
-def test_quadbin_fromzxy():
-    """Computes quadbin for root tile"""
-    result = run_query("""SELECT QUADBIN_FROMZXY(0,0,0)""")
-    assert result[0][0] == 5192650370358181887
+quadbins = [
+    # z, x, y, quadbin
+    ( 0, 0, 0, 5192650370358181887 ),
+    ( 1, 0, 0, 5193776270265024511 ),
+    ( 1, 0, 1, 5196028070078709759 ),
+    ( 1, 1, 0, 5194902170171867135 ),
+    ( 1, 1, 1, 5197153969985552383 )
+]
+
+@pytest.mark.parametrize('z, x, y, quadbin', quadbins)
+def test_quadbin_fromzxy(z, x, y, quadbin):
+    """Computes quadbin for z, x, y"""
+    result = run_query(f"""SELECT QUADBIN_FROMZXY({z},{x},{y})""")
+    assert result[0][0] == quadbin
