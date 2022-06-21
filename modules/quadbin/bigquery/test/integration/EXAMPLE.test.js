c63e5984aa4ebf001998719df5e64162e4e478a7
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2022-06-21 21:46:13 +0200

    Update test

--- modules/quadbin/bigquery/test/integration/EXAMPLE.test.js
+++
@@ -1,8 +0,0 @@
-const { runQuery } = require('../../../../../common/bigquery/test-utils');
-
-test('Example of running a query', async () => {
-    const query = 'SELECT 123 as v';
-    const rows = await runQuery(query);
-    expect(rows.length).toEqual(1);
-    expect(rows[0].v).toEqual(123);
-});
--- modules/quadbin/bigquery/test/integration/QUADBIN_FROMZXY.test.js
+++ modules/quadbin/bigquery/test/integration/QUADBIN_FROMZXY.test.js
@@ -1,8 +1,8 @@
 const { runQuery } = require('../../../../../common/bigquery/test-utils');
 
 test('QUADBIN_FROMZXY should work', async () => {
-    const query = 'SELECT `@@BQ_PREFIX@@carto.QUADBIN_FROMZXY`(z,x,y) AS output';
+    const query = 'SELECT CAST(`@@BQ_PREFIX@@carto.QUADBIN_FROMZXY`(10, 200, 391) AS STRING) AS output';
     const rows = await runQuery(query);
     expect(rows.length).toEqual(1);
-    expect(rows[0].output).toEqual();
+    expect(rows[0].output).toEqual('5233974874938015743');
 });
