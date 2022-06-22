c4886fb9dc5c221c0b54d19822476306001d1ad7
Javier Goizueta <jgoizueta@gmail.com>
2022-06-22 18:18:26 +0200

    Add QUADBIN_INT_TOSTRING

---
+++ modules/quadbin/postgres/test/integration/test_QUADBIN_INT_TOSTRING.py
@@ -0,0 +1,7 @@
+import pytest
+from test_utils import run_query
+
+def test_quadbin_int_tostring():
+    """Computes string quadbin"""
+    result = run_query(f"""SELECT QUADBIN_INT_TOSTRING({5209574053332910079})""")
+    assert result[0][0] == '484c1fffffffffff'
