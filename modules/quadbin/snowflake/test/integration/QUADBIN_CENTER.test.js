874d6c4e1c8bb828edfc6884e92e19d12617fdfe
volaya <volayaf@gmail.com>
2022-06-22 15:28:03 +0200

    inting fixes

--- modules/quadbin/snowflake/test/integration/QUADBIN_CENTER.test.js
+++ modules/quadbin/snowflake/test/integration/QUADBIN_CENTER.test.js
@@ -4,6 +4,6 @@ test('QUADBIN_CENTER should work', async () => {
     const query = 'SELECT QUADBIN_CENTER(5209574053332910079) AS OUTPUT';
     const rows = await runQuery(query);
     expect(rows.length).toEqual(1);
-    expect(rows[0].OUTPUT).toEqual({"coordinates": [33.75, -11.17840187371178], "type": "Point"}
+    expect(rows[0].OUTPUT).toEqual({ 'coordinates': [33.75, -11.17840187371178], 'type': 'Point' }
     );
 });
--- modules/quadbin/snowflake/test/integration/QUADBIN_TOZXY.test.js
+++ modules/quadbin/snowflake/test/integration/QUADBIN_TOZXY.test.js
@@ -4,5 +4,5 @@ test('QUADBIN_TOZXY should work', async () => {
     const query = 'SELECT QUADBIN_TOZXY(5209574053332910079) AS OUTPUT';
     const rows = await runQuery(query);
     expect(rows.length).toEqual(1);
-    expect(rows[0].OUTPUT).toEqual({"z":4,"x":9,"y":8});
+    expect(rows[0].OUTPUT).toEqual({ 'z':4,'x':9,'y':8 });
 });
