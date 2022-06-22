c869227b208f1c5f37144fd4324b0d60a2983cd0
Javier Goizueta <jgoizueta@gmail.com>
2022-06-22 22:32:28 +0200

    Add QUADBIN_RESOLUTION

---
+++ modules/quadbin/postgres/sql/QUADBIN_RESOLUTION.sql
@@ -0,0 +1,13 @@
+----------------------------
+-- Copyright (C) 2022 CARTO
+----------------------------
+
+CREATE OR REPLACE FUNCTION QUADBIN_RESOLUTION(
+  quadbin BIGINT
+)
+RETURNS INT
+ AS
+$BODY$
+    SELECT ((quadbin >> 52) & 31)::INT;
+$BODY$
+  LANGUAGE SQL;
---
+++ modules/quadbin/postgres/test/integration/test_QUADBIN_RESOLUTION.py
@@ -0,0 +1,7 @@
+import pytest
+from test_utils import run_query
+
+def test_quadbin_resolution():
+    """Computes resolution from quadbin"""
+    result = run_query(f"""SELECT QUADBIN_RESOLUTION({5209574053332910079})""")
+    assert result[0][0] == 4
