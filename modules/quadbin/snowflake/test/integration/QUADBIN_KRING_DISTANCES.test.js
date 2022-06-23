322c132cfa1122ec49974e07e476962cef8280ca
volaya <volayaf@gmail.com>
2022-06-23 16:51:37 +0200

    fixed tests

--- modules/quadbin/snowflake/test/integration/QUADBIN_KRING_DISTANCES.test.js
+++ modules/quadbin/snowflake/test/integration/QUADBIN_KRING_DISTANCES.test.js
@@ -1,8 +1,17 @@
 const { runQuery } = require('../../../../../common/snowflake/test-utils');
 
 test('QUADBIN_KRING_DISTANCES should work', async () => {
-    const query = 'SELECT TO_JSON(QUADBIN_KRING_DISTANCES(5209574053332910079, 1)) AS OUTPUT';
+    const query = 'SELECT QUADBIN_KRING_DISTANCES(5209574053332910079, 1) AS OUTPUT';
     const rows = await runQuery(query);
     expect(rows.length).toEqual(1);
-    expect(rows[0].OUTPUT).toEqual('[{"distance":1.000000000000000e+00,"index":5208043533147045887},{"distance":1.000000000000000e+00,"index":5208061125333090303},{"distance":1.000000000000000e+00,"index":5208113901891223551},{"distance":1.000000000000000e+00,"index":5209556461146865663},{"distance":0.000000000000000e+00,"index":5209574053332910079},{"distance":1.000000000000000e+00,"index":5209626829891043327},{"distance":1.000000000000000e+00,"index":5209609237704998911},{"distance":1.000000000000000e+00,"index":5209662014263132159},{"distance":1.000000000000000e+00,"index":5209591645518954495}]');
+    expect(rows[0].OUTPUT.sort()).toEqual([
+        {"distance":1,"index":5208043533147045887},
+        {"distance":1,"index":5208061125333090303},
+        {"distance":1,"index":5208113901891223551},
+        {"distance":1,"index":5209556461146865663},
+        {"distance":0,"index":5209574053332910079},
+        {"distance":1,"index":5209626829891043327},
+        {"distance":1,"index":5209609237704998911},
+        {"distance":1,"index":5209662014263132159},
+        {"distance":1,"index":5209591645518954495}].sort());
 });
--- modules/quadbin/snowflake/test/integration/QUADBIN_TOCHILDREN.test.js
+++ modules/quadbin/snowflake/test/integration/QUADBIN_TOCHILDREN.test.js
@@ -4,5 +4,6 @@ test('QUADBIN_TOCHILDREN should work', async () => {
     const query = 'SELECT QUADBIN_TOCHILDREN(5209574053332910079, 5) AS OUTPUT';
     const rows = await runQuery(query);
     expect(rows.length).toEqual(1);
-    expect(rows[0].OUTPUT).toEqual([5214064458820747263,5214068856867258367,5214073254913769471,5214077652960280575]);
+    expect(rows[0].OUTPUT.sort()).toEqual(
+        [5214064458820747263, 5214068856867258367, 5214073254913769471, 5214077652960280575].sort());
 });
