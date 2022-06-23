d2c150da468f56f2973e905585fc4750711ce79d
Javier Goizueta <jgoizueta@gmail.com>
2022-06-23 08:26:45 +0200

    Add QUADBIN_STRING_TOINT

---
+++ modules/quadbin/postgres/test/integration/test_QUADBIN_SIBLING.py
@@ -0,0 +1,27 @@
+import pytest
+from test_utils import run_query
+
+def test_quadbin_sibling_up():
+    """Computes top sibling"""
+    result = run_query(f"""SELECT QUADBIN_SIBLING(5209574053332910079, 'up')""")
+    assert result[0][0] == 5208061125333090303
+
+def test_quadbin_sibling_down():
+    """Computes bottom sibling"""
+    result = run_query(f"""SELECT QUADBIN_SIBLING(5209574053332910079, 'down')""")
+    assert result[0][0] == 5209609237704998911
+
+def test_quadbin_sibling_left():
+    """Computes left sibling"""
+    result = run_query(f"""SELECT QUADBIN_SIBLING(5209574053332910079, 'left')""")
+    assert result[0][0] == 5209574053332910079
+
+def test_quadbin_sibling_right():
+    """Computes left sibling"""
+    result = run_query(f"""SELECT QUADBIN_SIBLING(5209574053332910079, 'left')""")
+    assert result[0][0] == 5209626829891043327
+
+def test_quadbin_sibling_null():
+    """Returns NULL if the sibling does not exist"""
+    result = run_query(f"""SELECT QUADBIN_SIBLING(5192650370358181887, 'up')""")
+    assert result[0][0] == 5209626829891043327
