c1f877d645a411e49501d5ac53f5531f1aeb9363
Javier Goizueta <jgoizueta@gmail.com>
2022-06-24 07:10:03 +0200

    Make QUADBIN_INT_TOSTRING/_STRING_TOINT private

--- modules/quadbin/postgres/test/integration/test_QUADBIN_STRING_TOINT.py
+++ modules/quadbin/postgres/test/integration/test_QUADBIN_STRING_TOINT.py
@@ -3,5 +3,5 @@ from test_utils import run_query
 
 def test_quadbin_string_toint():
     """Computes int quadbin"""
-    result = run_query(f"""SELECT QUADBIN_STRING_TOINT('484c1fffffffffff')""")
+    result = run_query(f"""SELECT __QUADBIN_STRING_TOINT('484c1fffffffffff')""")
     assert result[0][0] == 5209574053332910079
