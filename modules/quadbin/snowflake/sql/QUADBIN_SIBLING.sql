c1453c1ab6a595e0cb5a64470a867eb7b644a280
Javier Goizueta <jgoizueta@gmail.com>
2022-06-24 10:08:10 +0200

    Refactor QUADBIN_SIBLING to avoid SF crashes

--- modules/quadbin/snowflake/sql/QUADBIN_SIBLING.sql
+++ modules/quadbin/snowflake/sql/QUADBIN_SIBLING.sql
@@ -6,16 +6,27 @@ CREATE OR REPLACE FUNCTION QUADBIN_SIBLING
 (quadbin INT, direction STRING)
 RETURNS INT
 AS $$
-    CASE
-        WHEN direction = 'left' THEN
-            _QUADBIN_SIBLING(quadbin, -1, 0)
-        WHEN direction = 'right' THEN
-            _QUADBIN_SIBLING(quadbin, 1, 0)
-        WHEN direction = 'up' THEN
-            _QUADBIN_SIBLING(quadbin, 0, -1)
-        WHEN direction = 'down' THEN
-            _QUADBIN_SIBLING(quadbin, 0, 1)
-        ELSE 
+  (WITH _dxy AS (
+    SELECT CASE direction
+        WHEN 'left' THEN
+            OBJECT_CONSTRUCT('x', -1, 'y', 0)
+        WHEN 'right' THEN
+            OBJECT_CONSTRUCT('x', 1, 'y', 0)
+        WHEN 'up' THEN
+            OBJECT_CONSTRUCT('x', 0, 'y', -1)
+        WHEN 'down' THEN
+            OBJECT_CONSTRUCT('x', 0, 'y', 1)
+        ELSE
             NULL
-    END
+    END AS d, QUADBIN_TOZXY(quadbin) AS t
+  )
+  SELECT IFF(t:y + d:y >= 0 AND t:y + d:y < BITSHIFTLEFT(1, t:z),
+        QUADBIN_FROMZXY(
+            t:z,
+            MOD(t:x + d:x, BITSHIFTLEFT(1, t:z)) + IFF(t:x + d:x < 0, BITSHIFTLEFT(1, t:z), 0),
+            t:y + d:y
+        ),
+        NULL
+    )
+   FROM _dxy)
 $$;
