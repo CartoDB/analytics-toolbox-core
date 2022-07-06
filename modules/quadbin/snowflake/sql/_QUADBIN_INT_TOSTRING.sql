99501daf8e6d04ebfc1ddc92ef7195eabc39c37c
volaya <volayaf@gmail.com>
2022-07-06 09:53:59 +0200

    use lowercase for quadbin values in SF

--- modules/quadbin/snowflake/sql/_QUADBIN_INT_TOSTRING.sql
+++ modules/quadbin/snowflake/sql/_QUADBIN_INT_TOSTRING.sql
@@ -6,5 +6,5 @@ CREATE OR REPLACE FUNCTION _QUADBIN_INT_TOSTRING
 (quadbin BIGINT)
 RETURNS STRING
 AS $$
-    TO_VARCHAR(quadbin, 'XXXXXXXXXXXXXXXX')
+    TO_VARCHAR(quadbin, 'xxxxxxxxxxxxxxxx')
 $$;
