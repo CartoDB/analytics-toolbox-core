052cb1d35ad47d12af95fc8d7ca92d82ee7b8f8e
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2022-07-11 18:30:24 +0200

    Remove log

--- modules/quadbin/bigquery/test/integration/__QUADBIN_FROMLONGLAT_ZOOMRANGE.test.js
+++ modules/quadbin/bigquery/test/integration/__QUADBIN_FROMLONGLAT_ZOOMRANGE.test.js
@@ -4,7 +4,6 @@ test('__QUADBIN_FROMLONGLAT_ZOOMRANGE should work', async () => {
     const query = 'SELECT TO_JSON_STRING(`@@BQ_PREFIX@@carto.__QUADBIN_FROMLONGLAT_ZOOMRANGE`(40.4168, -3.7038, 3, 6, 1, 4)) AS output';
     const rows = await runQuery(query);
     expect(rows.length).toEqual(1);
-    console.log(JSON.stringify(rows))
     expect(rows[0].output).toEqual('[{"id":"5223073582220836863","z":3,"x":4,"y":4},{"id":"5227576975689777151","z":4,"x":9,"y":8},{"id":"5232080575317147647","z":5,"x":19,"y":16},{"id":"5236584162059616255","z":6,"x":39,"y":32}]');
 });
 
