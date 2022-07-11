dcf6ac6ef73531847ebcced3f22d62f64c767416
alex-graciano <79551116+alex-graciano@users.noreply.github.com>
2022-07-11 18:34:54 +0200

    Add auxiliary __QUADBIN_FROMLONGLAT_ZOOMRANGE (#266)
    Co-authored-by: Jesús Arroyo Torrens <jarroyo@cartodb.com>

---
+++ modules/quadbin/bigquery/test/integration/__QUADBIN_FROMQUADINT.test.js
@@ -0,0 +1,8 @@
+const { runQuery } = require('../../../../../common/bigquery/test-utils');
+
+test('__QUADBIN_FROMQUADINT.test should work', async () => {
+    const query = 'SELECT CAST(`@@BQ_PREFIX@@carto.__QUADBIN_FROMQUADINT`(12521547919) AS STRING) AS output';
+    const rows = await runQuery(query);
+    expect(rows.length).toEqual(1);
+    expect(rows[0].output).toEqual('5256684166837174271');
+});
--- modules/quadbin/bigquery/test/integration/__QUADBIN_INT_TOSTRING.test.js
+++ modules/quadbin/bigquery/test/integration/__QUADBIN_INT_TOSTRING.test.js
@@ -1,6 +1,6 @@
 const { runQuery } = require('../../../../../common/bigquery/test-utils');
 
-test('QUADBIN_INT_TOSTRING should work', async () => {
+test('__QUADBIN_INT_TOSTRING should work', async () => {
     const query = 'SELECT `@@BQ_PREFIX@@carto.__QUADBIN_INT_TOSTRING`(5209574053332910079) AS output';
     const rows = await runQuery(query);
     expect(rows.length).toEqual(1);
