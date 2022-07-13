44393b1ececc2158768b7c7c376b84c20fbc8ad4
Alex Graciano <agraciano@cartodb.com>
2022-07-13 10:10:33 +0200

    Add boundary and bbox js functions as private

--- modules/quadbin/snowflake/sql/QUADBIN_BOUNDARY.sql
+++ modules/quadbin/snowflake/sql/QUADBIN_BOUNDARY.sql
@@ -2,39 +2,70 @@
 -- Copyright (C) 2022 CARTO
 ----------------------------
 
-
-CREATE OR REPLACE FUNCTION _GET_QUADBIN_POLYGON
-(index STRING)
-RETURNS ARRAY
-LANGUAGE JAVASCRIPT
-IMMUTABLE
-AS $$
-    if (!INDEX) {
-        return [];
-    }
-
-    @@SF_LIBRARY_CONTENT@@
-
-    return quadbinLib.getQuadbinPolygon(INDEX);
-$$;
-
-
-CREATE OR REPLACE SECURE FUNCTION QUADBIN_BOUNDARY
-(quadbin BIGINT)
+CREATE
+OR REPLACE FUNCTION QUADBIN_BOUNDARY(quadbin BIGINT)
 RETURNS GEOGRAPHY
-IMMUTABLE
 AS $$
-    WITH array_polygon AS (
-        SELECT _GET_QUADBIN_POLYGON(TO_VARCHAR(QUADBIN, 'xxxxxxxxxxxxxxxx')) p
+CASE quadbin
+    WHEN NULL THEN NULL -- Deal with level 0 boundary issue.
+    WHEN 5192650370358181887 THEN TO_GEOGRAPHY(
+        '{"coordinates":[[[-180,85.0511287798066],[-180,-85.0511287798066],[180,-85.0511287798066],[180,85.0511287798066],[-180,85.0511287798066]]],"type":"Polygon"}'
+    ) -- Deal with level 1. Prevent error from antipodal vertices.
+    WHEN 5193776270265024511 THEN TO_GEOGRAPHY (
+        'POLYGON((0 0, 0 85.0511287798066, -180 85.0511287798066, -180 0, -90 0, 0 0))'
     )
-    SELECT ST_MAKEPOLYGON(
-        TRY_TO_GEOGRAPHY(
-            'LINESTRING(' ||
-            GET(p, 0) || ' ' || GET(p, 1) || ',' ||
-            GET(p, 2) || ' ' || GET(p, 3) || ',' ||
-            GET(p, 4) || ' ' || GET(p, 5) || ',' ||
-            GET(p, 6) || ' ' || GET(p, 7) || ',' ||
-            GET(p, 8) || ' ' || GET(p, 9) ||
-            ')'))
-    FROM array_polygon
+    WHEN 5194902170171867135 THEN TO_GEOGRAPHY (
+        'POLYGON((180 0, 180 85.0511287798066, 0 85.0511287798066, 0 0, 90 0, 180 0))'
+    )
+    WHEN 5196028070078709759 THEN TO_GEOGRAPHY (
+        'POLYGON((0 0, -90 0, 180 0, -180 -85.0511287798066, 0 -85.0511287798066, 0 0))'
+    )
+    WHEN 5197153969985552383 THEN TO_GEOGRAPHY (
+        'POLYGON((180 0, 90 0, 0 0, 0 -85.0511287798066, 180 -85.0511287798066, 180 0))'
+    )
+    ELSE (
+        WITH __bbox AS (
+            SELECT
+                QUADBIN_BBOX(quadbin) AS b
+        ),
+        __line_a AS (
+            SELECT
+                ST_MAKELINE(
+                    ST_POINT(b [0], b [3]),
+                    ST_POINT(b [0], b [1])
+                ) as line
+            FROM
+                __bbox
+        ),
+        __line_b AS (
+            SELECT
+                ST_MAKELINE(
+                    ST_POINT(b [2], b [1]),
+                    ST_POINT(b [2], b [3])
+                ) as line
+            FROM
+                __bbox
+        ),
+        __line_c AS (
+            SELECT
+                ST_MAKELINE(
+                    __line_a.line,
+                    __line_b.line
+                ) as line
+            FROM
+                __line_a,
+                __line_b
+        )
+        SELECT
+            ST_MAKEPOLYGON(
+                ST_MAKELINE(
+                    __line_c.line,
+                    ST_POINT(b [0], b [3])
+                )
+            )
+        FROM
+            __bbox,
+            __line_c
+    )
+END
 $$;
---
+++ modules/quadbin/snowflake/sql/_QUADBIN_BOUNDARY.sql
@@ -0,0 +1,39 @@
+----------------------------
+-- Copyright (C) 2022 CARTO
+----------------------------
+
+CREATE OR REPLACE FUNCTION _GET_QUADBIN_POLYGON
+(index STRING)
+RETURNS ARRAY
+LANGUAGE JAVASCRIPT
+IMMUTABLE
+AS $$
+    if (!INDEX) {
+        return [];
+    }
+
+    @@SF_LIBRARY_CONTENT@@
+
+    return quadbinLib.getQuadbinPolygon(INDEX);
+$$;
+
+
+CREATE OR REPLACE SECURE FUNCTION _QUADBIN_BOUNDARY
+(quadbin BIGINT)
+RETURNS GEOGRAPHY
+IMMUTABLE
+AS $$
+    WITH array_polygon AS (
+        SELECT _GET_QUADBIN_POLYGON(TO_VARCHAR(QUADBIN, 'xxxxxxxxxxxxxxxx')) p
+    )
+    SELECT ST_MAKEPOLYGON(
+        TRY_TO_GEOGRAPHY(
+            'LINESTRING(' ||
+            GET(p, 0) || ' ' || GET(p, 1) || ',' ||
+            GET(p, 2) || ' ' || GET(p, 3) || ',' ||
+            GET(p, 4) || ' ' || GET(p, 5) || ',' ||
+            GET(p, 6) || ' ' || GET(p, 7) || ',' ||
+            GET(p, 8) || ' ' || GET(p, 9) ||
+            ')'))
+    FROM array_polygon
+$$;
