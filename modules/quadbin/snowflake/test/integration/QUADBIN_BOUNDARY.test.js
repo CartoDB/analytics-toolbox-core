33453ca8a12a97bae7f9064b480405c2bffdf7c2
Alex Graciano <agraciano@cartodb.com>
2022-07-13 08:14:36 +0200

    Update QUADBIN_BOUNDARY UDF

--- modules/quadbin/snowflake/test/integration/QUADBIN_BOUNDARY.test.js
+++ modules/quadbin/snowflake/test/integration/QUADBIN_BOUNDARY.test.js
@@ -4,5 +4,5 @@ test('QUADBIN_BOUNDARY should work', async () => {
     const query = 'SELECT QUADBIN_BOUNDARY(5209574053332910079) AS OUTPUT';
     const rows = await runQuery(query);
     expect(rows.length).toEqual(1);
-    expect(rows[0].OUTPUT).toEqual({ 'coordinates': [[[22.5, 0], [22.5, -21.94304553343817], [45, -21.94304553343817], [45, 0], [22.5, 0]]], 'type': 'Polygon' });
+    expect(rows[0].OUTPUT).toEqual({ 'coordinates':[[[45,0],[45,-21.943045533],[22.5,-21.943045533],[22.5,0],[45,0]]],'type':'Polygon' });
 });
