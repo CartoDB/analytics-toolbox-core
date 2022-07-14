5547aa9ce5bea4249c981c4e2dc7d84ab3813df0
Alex Graciano <agraciano@cartodb.com>
2022-07-14 20:10:29 +0200

    Fix tests

--- modules/quadbin/snowflake/test/integration/_QUADBIN_BOUNDARY.test.js
+++
@@ -1,8 +0,0 @@
-const { runQuery } = require('../../../../../common/snowflake/test-utils');
-
-test('_QUADBIN_BOUNDARY should work', async () => {
-    const query = 'SELECT _QUADBIN_BOUNDARY(5209574053332910079) AS OUTPUT';
-    const rows = await runQuery(query);
-    expect(rows.length).toEqual(1);
-    expect(rows[0].OUTPUT).toEqual({ 'coordinates':[[[45,0],[45,-21.943045533],[22.5,-21.943045533],[22.5,0],[45,0]]],'type':'Polygon' });
-});
