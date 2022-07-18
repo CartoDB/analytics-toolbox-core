ed4d6350f7a4aba0f9e5466caf6ef26374f50f8c
Alex Graciano <agraciano@cartodb.com>
2022-07-18 08:59:33 +0200

    Add clarification in comments

--- modules/quadbin/snowflake/sql/QUADBIN_KRING_DISTANCES.sql
+++ modules/quadbin/snowflake/sql/QUADBIN_KRING_DISTANCES.sql
@@ -2,7 +2,12 @@
 -- Copyright (C) 2022 CARTO
 ----------------------------
 
--- FIXME: slow
+-- The function returns a STRING for two main issues related with Snowflake limitations
+-- 1. Snowflake has a native support of BigInt numbers, however, if the UDF
+-- returns this data type the next Snowflake internal error is raised:
+-- SQL execution internal error: Processing aborted due to error 300010:3321206824
+-- 2. If the UDF returns the hex codification of the quadbin to be parsed in a SQL
+-- higher level by using the _QUADBIN_STRING_TOINT UDF a non-correlated query can be produced.
 
 CREATE OR REPLACE FUNCTION QUADBIN_KRING_DISTANCES
 (origin BIGINT, size INT)
