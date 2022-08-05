521f8b9ea42dc6a5b0a219f1e514cb84c7cdcf0c
François BAPTISTE <7817985+francois-baptiste@users.noreply.github.com>
2022-08-05 09:35:00 +0200

    adds S2_CENTER to the s2 module (#282)
    Co-authored-by: Juan Luis Ruiz-Tagle Oriol <juanluis.rto@gmail.com>

---
+++ modules/s2/bigquery/test/integration/S2_CENTER.test.js
@@ -0,0 +1,8 @@
+const { runQuery } = require('../../../../../common/bigquery/test-utils');
+
+test('S2_CENTER should work', async () => {
+    const query = 'SELECT ST_ASTEXT(`@@BQ_PREFIX@@carto.S2_CENTER`(1735346007979327488)) as id';
+    const rows = await runQuery(query);
+    expect(rows.length).toEqual(1);
+    expect(rows[0].id.toString()).toEqual('POINT(40.4720004343497 -3.72646193231851)');
+});
