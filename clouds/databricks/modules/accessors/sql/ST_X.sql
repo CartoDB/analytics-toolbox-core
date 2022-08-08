dbeb37467611293892451b29b5463e0067245fdd
Ángel Arroyo <aarroyo@cartodb.com>
2022-08-08 17:28:21 +0200

    fix st_x sql

--- clouds/databricks/modules/accessors/sql/ST_X.sql
+++ clouds/databricks/modules/accessors/sql/ST_X.sql
@@ -1,2 +1,1 @@
--- Dummy Function
-CREATE OR REPLACE FUNCTION dummy_function as 'com.carto.analyticstoolbox.modules.dummy.DummyFunction';
+CREATE OR REPLACE FUNCTION st_x as 'com.carto.analyticstoolbox.modules.accessors.ST_X';
