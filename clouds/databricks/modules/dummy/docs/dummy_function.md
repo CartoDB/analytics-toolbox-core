a41ac4ee94c44d03e3aa9cf46d026b4f0eeacd9d
Ángel Arroyo <aarroyo@cartodb.com>
2022-08-09 12:28:04 +0200

    add pytest test and remove dummy function

--- clouds/databricks/modules/dummy/docs/dummy_function.md
+++
@@ -1,2 +0,0 @@
-### dummy_function
-This functions concats the input string to "hello "
--- clouds/databricks/modules/dummy/sql/dummy_function.sql
+++
@@ -1,2 +0,0 @@
--- Dummy Function
-CREATE OR REPLACE FUNCTION dummy_function as 'com.carto.analyticstoolbox.modules.dummy.DummyFunction';
