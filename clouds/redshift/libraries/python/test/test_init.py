8e0f6e5aa1746c73602c412f5d2d9f28cfee5265
vdelacruzb <vdelacruzb@gmail.com>
2022-08-23 09:33:30 +0200

    add transformations

--- clouds/redshift/libraries/python/test/test_init.py
+++ clouds/redshift/libraries/python/test/test_init.py
@@ -1,8 +1,14 @@
 import lib
 from lib import quadbin
+from lib import transformations
 
 
 def test_init():
     assert hasattr(lib, '__version__')
     assert hasattr(quadbin, 'tile_to_cell')
     assert hasattr(quadbin, 'cell_to_bounding_box')
+    assert hasattr(transformations, 'center_mean')
+    assert hasattr(transformations, 'center_median')
+    assert hasattr(transformations, 'centroid')
+    assert hasattr(transformations, 'destination')
+    assert hasattr(transformations, 'great_circle')
