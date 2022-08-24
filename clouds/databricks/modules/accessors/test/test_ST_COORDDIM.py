90f11adde2209b6b2c3c23101acc90a82dd6f698
Ángel Arroyo <aarroyo@cartodb.com>
2022-08-24 08:27:45 +0200

    finish integration test

--- clouds/databricks/modules/accessors/test/test_ST_COORDDIM.py
+++ clouds/databricks/modules/accessors/test/test_ST_COORDDIM.py
@@ -4,7 +4,7 @@ from test_utils.utils import run_query
 def test_st_coorddim_success():
     query = "SELECT @@DB_SCHEMA@@.ST_COORDDIM(@@DB_SCHEMA@@.ST_MAKEPOINTM(1, 2, 3));"
     result = run_query(query)
-    assert result[0][0] == 4
+    assert result[0][0] == 3
 
 
 
