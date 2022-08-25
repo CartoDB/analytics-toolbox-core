32e90cbf8f6cdb86f2e3a3929c1a505fe9847d53
François BAPTISTE <7817985+francois-baptiste@users.noreply.github.com>
2022-08-25 16:04:00 +0200

    add H3_COVERINGCELLIDS

---
+++ modules/h3/bigquery/sql/H3_COVERINGCELLIDS.sql
@@ -0,0 +1,29 @@
+----------------------------
+-- Copyright (C) 2021 CARTO
+----------------------------
+
+CREATE OR REPLACE FUNCTION `@@BQ_PREFIX@@carto.H3_COVERINGCELLIDS`
+(geog GEOGRAPHY, resolution INT64)
+  AS (( ARRAY (
+      WITH
+        T2 AS (
+        SELECT
+          `@@BQ_PREFIX@@carto.__S2_CENTER`(s2_index) h3_lonlat
+        FROM
+          UNNEST(S2_CoveringCellIDs(geog,
+              max_level => [3,5,6,8,9,10,12,13,14,16,17,18,20,21,23,24][SAFE_OFFSET(resolution)],
+              min_level => [3,5,6,8,9,10,12,13,14,16,17,18,20,21,23,24][SAFE_OFFSET(resolution)],
+              max_cells => 1000000)) s2_index ),
+        T3 AS (
+        SELECT
+          `@@BQ_PREFIX@@carto.H3_FROMLONGLAT`(h3_lonlat.lng, h3_lonlat.lat, resolution) h3_cell
+        FROM
+          T2 )
+      SELECT
+        DISTINCT h3_cell
+      FROM
+        T3
+      WHERE
+        ST_INTERSECTS(`@@BQ_PREFIX@@carto.H3_BOUNDARY`(h3_cell),geog)
+      GROUP BY
+        h3_cell ) ));
--- modules/s2/bigquery/sql/S2_CENTER.sql
+++ modules/s2/bigquery/sql/S2_CENTER.sql
@@ -2,24 +2,19 @@
 -- Copyright (C) 2022 CARTO
 ----------------------------
 
-CREATE OR REPLACE FUNCTION `@@BQ_PREFIX@@carto.__S2_CENTER`
-(id INT64)
-RETURNS STRING
-DETERMINISTIC
-LANGUAGE js
+CREATE OR REPLACE FUNCTION `@@BQ_PREFIX@@carto.__S2_CENTER`(id INT64)
+RETURNS STRUCT<lng FLOAT64,lat FLOAT64> DETERMINISTIC LANGUAGE js
 OPTIONS (library=["@@BQ_LIBRARY_BUCKET@@"])
 AS R"""
 if (id == null) {
         throw new Error('NULL argument passed to UDF');
     }
-    const latlng = s2Lib.idToLatLng(String(id));
-    const wkt = `POINT(` + latlng.lng + ` ` + latlng.lat + `)`;
-    return wkt;
+    return s2Lib.idToLatLng(String(id));
 """;
 
 CREATE OR REPLACE FUNCTION `@@BQ_PREFIX@@carto.S2_CENTER`
 (id INT64)
 RETURNS GEOGRAPHY
 AS (
-    ST_GEOGFROMTEXT(`@@BQ_PREFIX@@carto.__S2_CENTER`(id))
+    ST_GEOGPOINT(`@@BQ_PREFIX@@carto.__S2_CENTER`(id).lng,`@@BQ_PREFIX@@carto.__S2_CENTER`(id).lat)
 );
