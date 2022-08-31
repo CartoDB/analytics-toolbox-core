c1a758e716f108e28921df36da58ac99eeb02b0b
vdelacruzb <vdelacruzb@gmail.com>
2022-08-31 19:15:47 +0200

    update drop functions

--- clouds/bigquery/common/DROP_FUNCTIONS.sql
+++ clouds/bigquery/common/DROP_FUNCTIONS.sql
@@ -2,85 +2,13 @@
 -- Copyright (C) 2022 CARTO
 ----------------------------
 
-CREATE OR REPLACE PROCEDURE @@SF_SCHEMA@@._DROP_FUNCTIONS()
-RETURNS VARCHAR
-LANGUAGE javascript
-IMMUTABLE
-AS $$
-    let rs = snowflake.execute({
-        sqlText: 'SHOW USER FUNCTIONS IN SCHEMA @@SF_SCHEMA@@;'
-    });
-    let cmd1_dict = {
-      sqlText: `
-        SELECT CONCAT(
-            'DROP FUNCTION ',
-            "catalog_name",
-            '.',
-            "schema_name",
-            '.',
-            REGEXP_REPLACE("arguments", ' RETURN .*$', ''),
-            ';'
-        ) AS drop_command
-        FROM TABLE(RESULT_SCAN(LAST_QUERY_ID()));`
-    };
-    let stmt = snowflake.createStatement(cmd1_dict);
-    rs = stmt.execute();
-    let s = '';
-    let CONTINUEONERROR = false; // Default to false for overloaded function
-    while (rs && rs.next()) {
-        try {
-            cmd2_dict = { sqlText: rs.getColumnValue('DROP_COMMAND') };
-            stmtEx = snowflake.createStatement(cmd2_dict);
-            stmtEx.execute();
-            s += rs.getColumnValue(1) + ' --Succeeded' + '\n';
-        }
-        catch (err) {
-            s += rs.getColumnValue(1) + ' --Failed: ' + err.message.replace(/\n/g, ' ') + '\n';
-            if (!CONTINUEONERROR) return s;
-        }
-    }
-    if (s) return s;
-$$;
+CREATE OR REPLACE PROCEDURE `@@BQ_DATASET@@.DROP_FUNCTIONS`()
+BEGIN
+    FOR record IN
+    (SELECT CONCAT('DROP ', routine_type, ' `', specific_catalog, '.', specific_schema, '.', specific_name,'`;') as drop_command from `@@BQ_PREFIX@@.carto.INFORMATION_SCHEMA.ROUTINES`)
+    DO
+        EXECUTE IMMEDIATE record.drop_command;
+    END FOR;
+END;
 
-CREATE OR REPLACE PROCEDURE @@SF_SCHEMA@@._DROP_PROCEDURES()
-RETURNS VARCHAR
-LANGUAGE javascript
-IMMUTABLE
-AS $$
-    let rs = snowflake.execute({
-        sqlText: 'SHOW USER PROCEDURES IN SCHEMA @@SF_SCHEMA@@;'
-    });
-    let cmd1_dict = {
-      sqlText: `
-        SELECT CONCAT(
-            'DROP PROCEDURE ',
-            "catalog_name",
-            '.',
-            "schema_name",
-            '.',
-            REGEXP_REPLACE("arguments", ' RETURN .*$', ''),
-            ';'
-        ) AS drop_command
-        FROM TABLE(RESULT_SCAN(LAST_QUERY_ID()));`
-    };
-    let stmt = snowflake.createStatement(cmd1_dict);
-    rs = stmt.execute();
-    let s = '';
-    let CONTINUEONERROR = false; // Default to false for overloaded function
-    while (rs && rs.next()) {
-        try {
-            cmd2_dict = { sqlText: rs.getColumnValue('DROP_COMMAND') };
-            stmtEx = snowflake.createStatement(cmd2_dict);
-            stmtEx.execute();
-            s += rs.getColumnValue(1) + ' --Succeeded' + '\n';
-        }
-        catch (err) {
-            s += rs.getColumnValue(1) + ' --Failed: ' + err.message.replace(/\n/g, ' ') + '\n';
-            if (!CONTINUEONERROR) return s;
-        }
-    }
-    if (s) return s;
-$$;
-
-CALL @@SF_SCHEMA@@._DROP_FUNCTIONS();
-CALL @@SF_SCHEMA@@._DROP_PROCEDURES();
+CALL `@@BQ_DATASET@@.DROP_FUNCTIONS`();
