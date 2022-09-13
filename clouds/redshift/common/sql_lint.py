784c6304b6fa8c839b322daa0302a1ab15c51c4d
vdelacruzb <vdelacruzb@gmail.com>
2022-09-13 14:49:44 +0200

    update sql_fluff

---
+++ clouds/redshift/common/sql_lint.py
@@ -0,0 +1,15 @@
+import sqlfluff
+import sys
+
+scripts = sys.argv[1].split(' ')
+for script in scripts:
+    content = ''
+    with open(script, 'r') as file:
+        content = file.read().replace('@', '_sqlfluff_')
+    fixed_content = (
+        sqlfluff.fix(content, dialect='redshift')
+        .replace('_sqlfluff_', '@')
+        .replace('_SQLFLUFF_', '@')
+    )
+    with open(script, 'w') as file:
+        file.write(fixed_content)
