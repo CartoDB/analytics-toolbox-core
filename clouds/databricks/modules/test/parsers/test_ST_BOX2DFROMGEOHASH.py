97b2b38477ae140882bf2fa90a94e54969d51413
Ángel Arroyo <aarroyo@cartodb.com>
2022-09-14 18:28:14 +0200

    fix tests

--- clouds/databricks/modules/test/parsers/test_ST_BOX2DFROMGEOHASH.py
+++ clouds/databricks/modules/test/parsers/test_ST_BOX2DFROMGEOHASH.py
@@ -3,7 +3,7 @@ from python_utils.test_utils import run_query
 
 def test_st_box2dfromgeohash_success():
     query = """WITH t AS (
-  SELECT @@DB_SCHEMA@@.ST_ASGEOHASH(@@DB_SCHEMA@@.ST_GEOMFROMWKT('POINT(-76.09130 18.42750)'), 8) AS geohash
+  SELECT @@DB_SCHEMA@@.ST_GEOHASH(@@DB_SCHEMA@@.ST_GEOMFROMWKT('POINT(-76.09130 18.42750)'), 8) AS geohash
 )
 SELECT @@DB_SCHEMA@@.ST_ASTEXT(@@DB_SCHEMA@@.ST_BOX2DFROMGEOHASH(geohash, 5)) FROM t;"""
     result = run_query(query)
--- clouds/databricks/modules/test/parsers/test_ST_POINTFROMGEOHASH.py
+++ clouds/databricks/modules/test/parsers/test_ST_POINTFROMGEOHASH.py
@@ -3,7 +3,7 @@ from python_utils.test_utils import run_query
 
 def test_st_pointfromgeohash_success():
     query = """WITH t AS (
-  SELECT @@DB_SCHEMA@@.ST_ASGEOHASH(@@DB_SCHEMA@@.ST_GEOMFROMWKT('POINT(-76.09130 18.42750)'), 8) AS geohash
+  SELECT @@DB_SCHEMA@@.ST_GEOHASH(@@DB_SCHEMA@@.ST_GEOMFROMWKT('POINT(-76.09130 18.42750)'), 8) AS geohash
 )
 SELECT @@DB_SCHEMA@@.ST_ASTEXT(@@DB_SCHEMA@@.ST_POINTFROMGEOHASH(geohash, 5)) FROM t;"""
     result = run_query(query)
