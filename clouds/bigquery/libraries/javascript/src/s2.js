df26d31c5f899259c3a46256b92867ab305bdbfb
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2022-09-15 15:55:19 +0200

    Migrate _H3_COVERINGCELLIDS,S2_CENTER

--- clouds/bigquery/libraries/javascript/src/s2.js
+++ clouds/bigquery/libraries/javascript/src/s2.js
@@ -4,5 +4,6 @@ export default {
     keyToId: S2.keyToId,
     idToKey: S2.idToKey,
     latLngToKey: S2.latLngToKey,
-    FromHilbertQuadKey: S2.S2Cell.FromHilbertQuadKey
+    FromHilbertQuadKey: S2.S2Cell.FromHilbertQuadKey,
+    idToLatLng: S2.idToLatLng
 };
