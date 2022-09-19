07989238ad36ca573f791cf639bcd95d41e5d925
vdelacruzb <vdelacruzb@gmail.com>
2022-09-19 17:55:22 +0200

    raise error and return file

--- clouds/bigquery/common/sql_lint.py
+++ clouds/bigquery/common/sql_lint.py
@@ -1,7 +1,16 @@
-import sqlfluff
+import logging
 import sys
+import sqlfluff
 
 # Lint the sql files passed as input
+
+current_script = ''
+def _logging_handle(self, record):
+    print(current_script)
+    raise Exception('ERROR: ' + record.msg + '\nPlease check '+ current_script)
+
+stream_handler = logging.StreamHandler
+stream_handler.handle = _logging_handle
 
 scripts = sys.argv[1].split(' ')
 ignored_files = sys.argv[3]
@@ -9,9 +18,10 @@ if ignored_files:
     with open(ignored_files, 'r') as ignored_file:
         ignored_scripts = ignored_file.read().split('\n')
     for ignored_script in ignored_scripts:
-        scripts = list(filter (lambda x:not x.endswith(ignored_script) , scripts))
+        scripts = list(filter(lambda x: not x.endswith(ignored_script), scripts))
 
 for script in scripts:
+    current_script = script
     content = ''
     with open(script, 'r') as file:
         content = file.read().replace('@@BQ_DATASET@@', '_sqlfluffdataset_').replace('@', '_sqlfluff_')
--- clouds/postgres/common/sql_lint.py
+++ clouds/postgres/common/sql_lint.py
@@ -1,7 +1,16 @@
-import sqlfluff
+import logging
 import sys
+import sqlfluff
 
 # Lint the sql files passed as input
+
+current_script = ''
+def _logging_handle(self, record):
+    print(current_script)
+    raise Exception('ERROR: ' + record.msg + '\nPlease check '+ current_script)
+
+stream_handler = logging.StreamHandler
+stream_handler.handle = _logging_handle
 
 scripts = sys.argv[1].split(' ')
 ignored_files = sys.argv[3]
@@ -12,6 +21,7 @@ if ignored_files:
         scripts = list(filter(lambda x: not x.endswith(ignored_script), scripts))
 
 for script in scripts:
+    current_script = script
     content = ''
     with open(script, 'r') as file:
         content = (
