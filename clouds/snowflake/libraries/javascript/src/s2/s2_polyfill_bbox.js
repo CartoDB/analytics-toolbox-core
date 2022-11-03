0900f5a5c053005b7b7274fd40be57f6f6820dc8
vdelacruzb <vdelacruzb@gmail.com>
2022-11-03 10:50:55 +0100

    s2_polyfill_bbox working with equal values

--- clouds/snowflake/libraries/javascript/src/s2/s2_polyfill_bbox.js
+++ clouds/snowflake/libraries/javascript/src/s2/s2_polyfill_bbox.js
@@ -5,6 +5,19 @@ import {
 import { S2 } from './index';
 
 export function polyfillBbox (minLng, maxLng, minLat, maxLat, resolution) {
+    // The bounds cannot be aproximately the same in order createPolygonListFromBounds to work
+    // for this reason we ensure a gap between min and max
+    const decimalsToRound = 5
+    if (Math.round(minLng * Math.pow(10, decimalsToRound)) === Math.round(maxLng * Math.pow(10, decimalsToRound)))
+    {
+        minLng -= 1 / Math.pow(10, decimalsToRound)
+        maxLng += 1 / Math.pow(10, decimalsToRound)
+    }
+    if (Math.round(minLat * Math.pow(10, decimalsToRound)) === Math.round(maxLat * Math.pow(10, decimalsToRound)))
+    {
+        minLat -= 1 / Math.pow(10, decimalsToRound)
+        maxLat += 1 / Math.pow(10, decimalsToRound)
+    }
     const polygonList = createPolygonListFromBounds({
         bounds: [[minLng, minLat], [maxLng, maxLat]],
         level: resolution
