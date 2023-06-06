f6bf76229c1731749121c06eccdd9262a4519196
Luigi Pirelli <luipir@gmail.com>
2023-06-06 15:56:48 +0200

    ported PY formula to SQL + related tests

--- clouds/bigquery/modules/sql/quadbin/QUADBIN_FROMLONGLAT.sql
+++ clouds/bigquery/modules/sql/quadbin/QUADBIN_FROMLONGLAT.sql
@@ -12,36 +12,55 @@ AS ((
             ERROR('Invalid resolution; should be between 0 and 26'), (
                 WITH
                 __params AS (
-                    SELECT
+                    select
+                        longitude,
                         resolution AS z,
+                        (1 << resolution) AS __z2,
                         ACOS(-1) AS pi,
                         GREATEST(-85.05, LEAST(85.05, latitude)) AS latitude
+                    from __values
+                ),
+
+                ___sinlat AS (
+                    select SIN(latitude * pi / 180.0) as __sinlat from __params
+                ),
+
+                ___x AS (
+                    SELECT
+                        CAST(
+                            -- floor before cast to avoid up rounding to the next tile
+                            FLOOR(
+                                __z2 * ((longitude / 360.0) + 0.5)
+                            ) AS INT64
+                        ) & (__z2 - 1)  -- bitwise way to calc MOD
+                        AS __x
+                    FROM
+                        __params
                 ),
 
                 __zxy AS (
                     SELECT
                         z,
+                        CASE
+                            WHEN __x < 0 THEN __x + __z2
+                            ELSE __x
+                        END AS x,
                         CAST(
+                            -- floor before cast to avoid up rounding to the next tiLe
                             FLOOR(
-                                (1 << z) * ((longitude / 360.0) + 0.5)
-                            ) AS INT64
-                        ) & ((1 << z) - 1) AS x,
-                        CAST(
-                            FLOOR(
-                                (
-                                    1 << z
-                                ) * (
-                                    0.5 - (
-                                        LN(
-                                            TAN(
-                                                pi / 4.0 + latitude / 2.0 * pi / 180.0
-                                            )
-                                        ) / (2 * pi)
-                                    )
+                                __z2 * (
+                                    0.5 - 0.25 * 
+                                    LN(
+                                        (1 + __sinlat) / (1 - __sinlat)
+                                    ) / pi
                                 )
-                            ) AS INT64
-                        ) & ((1 << z) - 1) AS y
-                    FROM __params
+                            ) AS int64
+                        ) AS y
+                    FROM
+                        __params,
+                        ___sinlat,
+                        ___x
+
                 )
 
                 SELECT `@@BQ_DATASET@@.QUADBIN_FROMZXY`(
