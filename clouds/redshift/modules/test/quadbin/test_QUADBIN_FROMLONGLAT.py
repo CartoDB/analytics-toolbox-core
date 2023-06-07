c735bb937601d78e20c8224864a60e85da6ac453
Luigi Pirelli <luipir@gmail.com>
2023-06-07 17:07:49 +0200

    added more tests and set values aligned to quadbin-py 0.2.0

--- clouds/redshift/modules/test/quadbin/test_QUADBIN_FROMLONGLAT.py
+++ clouds/redshift/modules/test/quadbin/test_QUADBIN_FROMLONGLAT.py
@@ -10,17 +10,6 @@ def test_quadbin_fromlonglat():
     assert result[0][0] == 5209574053332910079
 
 
-def test_quadbin_longlat_highest_resolution():
-    """Computes quadbin for longitude latitude at highest resolution.
-    This test is useful to get a reference value to build test and check SQL
-    implementation against this python implementation of quadbin
-    """
-    result = run_query(
-        'SELECT @@RS_SCHEMA@@.QUADBIN_FROMLONGLAT(40.413365349070865, -3.71219873428345, 26)'
-    )
-    assert result[0][0] == 5308641755410858449
-
-
 def test_quadbin_fromlonglat_null():
     result = run_query(
         """
@@ -46,3 +35,86 @@ def test_quadbin_fromlonglat_resolution_overflow_failure():
     error = 'Invalid resolution: should be between 0 and 26'
     with pytest.raises(ProgrammingError, match=error):
         run_query('SELECT @@RS_SCHEMA@@.QUADBIN_FROMLONGLAT(40.4168, -3.7038, 27)')
+
+
+def test_quadbin_longlat_highest_resolution():
+    """Computes quadbin for longitude latitude at highest resolution.
+
+    This test is useful to get a reference value to build test and check SQL
+    implementation against this python implementation of quadbin
+    """
+    result = run_query(
+        """
+        SELECT @@RS_SCHEMA@@.QUADBIN_FROMLONGLAT(
+            40.413365349070865,
+            -3.71219873428345,
+            26)
+        """
+    )
+    assert result[0][0] == 5308641755410858449
+
+    query = """
+        SELECT @@RS_SCHEMA@@.QUADBIN_FROMLONGLAT(
+            -3.71219873428345,
+            40.413365349070865,
+            26)
+    """
+    result = run_query(query)
+    assert len(result[0]) == 1
+    assert result[0][0] == 5306319089810035706
+
+    query = """
+        SELECT @@RS_SCHEMA@@.QUADBIN_FROMLONGLAT(
+            40.413365349070865,
+            -3.71219873428345,
+            26)
+    """
+    result = run_query(query)
+    assert len(result[0]) == 1
+    assert result[0][0] == 5308641755410858449
+
+    query = 'SELECT @@RS_SCHEMA@@.QUADBIN_FROMLONGLAT(0.0, 3.552713678800501e-15, 26)'
+    result = run_query(query)
+    assert len(result[0]) == 1
+    assert result[0][0] == 5308618060762972160
+
+    query = 'SELECT @@RS_SCHEMA@@.QUADBIN_FROMLONGLAT(0.0, -3.552713678800501e-15, 26)'
+    result = run_query(query)
+    assert len(result[0]) == 1
+    assert result[0][0] == 5308618060762972160
+
+    query = """
+        SELECT @@RS_SCHEMA@@.QUADBIN_FROMLONGLAT(
+            -89.71219873428345,
+            -84.413365349070865,
+            26)
+    """
+    result = run_query(query)
+    assert len(result[0]) == 1
+    assert result[0][0] == 5308521992464067502
+
+    # set of call giving the same result with slightly different lat
+    query = 'SELECT @@RS_SCHEMA@@.QUADBIN_FROMLONGLAT(0.0, 5.3644180297851546e-06, 26)'
+    result = run_query(query)
+    assert len(result[0]) == 1
+    assert result[0][0] == 5307116860887181994
+    query = 'SELECT @@RS_SCHEMA@@.QUADBIN_FROMLONGLAT(0.0, 5.364418029785155e-06, 26)'
+    result = run_query(query)
+    assert len(result[0]) == 1
+    assert result[0][0] == 5307116860887181994
+    query = 'SELECT @@RS_SCHEMA@@.QUADBIN_FROMLONGLAT(0.0, 5.364418029785156e-06, 26)'
+    result = run_query(query)
+    assert len(result[0]) == 1
+    assert result[0][0] == 5307116860887181994
+    query = 'SELECT @@RS_SCHEMA@@.QUADBIN_FROMLONGLAT(0.0, 5.364418029785157e-06, 26)'
+    result = run_query(query)
+    assert len(result[0]) == 1
+    assert result[0][0] == 5307116860887181994
+    query = 'SELECT @@RS_SCHEMA@@.QUADBIN_FROMLONGLAT(0.0, 5.364418029785158e-06, 26)'
+    result = run_query(query)
+    assert len(result[0]) == 1
+    assert result[0][0] == 5307116860887181994
+    query = 'SELECT @@RS_SCHEMA@@.QUADBIN_FROMLONGLAT(0.0, 5.364418029785156e-06, 26)'
+    result = run_query(query)
+    assert len(result[0]) == 1
+    assert result[0][0] == 5307116860887181994
