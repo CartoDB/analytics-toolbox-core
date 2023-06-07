81a1af5cef24e54e34005cae3efc34d98634f46c
Luigi Pirelli <luipir@gmail.com>
2023-06-07 11:04:04 +0200

    ported BG tests for higest res to python for PG

--- clouds/postgres/modules/test/quadbin/test_QUADBIN_FROMLONGLAT.py
+++ clouds/postgres/modules/test/quadbin/test_QUADBIN_FROMLONGLAT.py
@@ -33,3 +33,55 @@ def test_quadbin_longlat_large_resolution():
     """Throws error for large resolution."""
     with pytest.raises(Exception):
         run_query('SELECT @@PG_SCHEMA@@.QUADBIN_FROMLONGLAT(40.4168, -3.7038, 27)')
+
+def test_quadbin_longlat_highest_resolution():
+    query = 'SELECT @@PG_SCHEMA@@.QUADBIN_FROMLONGLAT(-3.71219873428345, 40.413365349070865, 26)'
+    result = run_query(query)
+    assert len(result[0]) == 1
+    assert result[0][0] == 5306319089810037072
+
+    query = 'SELECT @@PG_SCHEMA@@.QUADBIN_FROMLONGLAT(40.413365349070865, -3.71219873428345, 26)'
+    result = run_query(query)
+    assert len(result[0]) == 1
+    assert result[0][0] == 5308641755410858449
+
+    query = 'SELECT @@PG_SCHEMA@@.QUADBIN_FROMLONGLAT(0.0, 3.552713678800501e-15, 26)'
+    result = run_query(query)
+    assert len(result[0]) == 1
+    assert result[0][0] == 5308618060762972160
+
+    query = 'SELECT @@PG_SCHEMA@@.QUADBIN_FROMLONGLAT(0.0, -3.552713678800501e-15, 26)'
+    result = run_query(query)
+    assert len(result[0]) == 1
+    assert result[0][0] == 5308618060762972160
+
+    query = 'SELECT @@PG_SCHEMA@@.QUADBIN_FROMLONGLAT(-89.71219873428345, -84.413365349070865, 26)'
+    result = run_query(query)
+    assert len(result[0]) == 1
+    assert result[0][0] == 5308521992464067502
+
+    # set of call giving the same result with slightly different lat
+    query = 'SELECT @@PG_SCHEMA@@.QUADBIN_FROMLONGLAT(0.0, 5.3644180297851546e-06, 26)'
+    result = run_query(query)
+    assert len(result[0]) == 1
+    assert result[0][0] == 5307116860887181994
+    query = 'SELECT @@PG_SCHEMA@@.QUADBIN_FROMLONGLAT(0.0, 5.364418029785155e-06, 26)'
+    result = run_query(query)
+    assert len(result[0]) == 1
+    assert result[0][0] == 5307116860887181994
+    query = 'SELECT @@PG_SCHEMA@@.QUADBIN_FROMLONGLAT(0.0, 5.364418029785156e-06, 26)'
+    result = run_query(query)
+    assert len(result[0]) == 1
+    assert result[0][0] == 5307116860887181994
+    query = 'SELECT @@PG_SCHEMA@@.QUADBIN_FROMLONGLAT(0.0, 5.364418029785157e-06, 26)'
+    result = run_query(query)
+    assert len(result[0]) == 1
+    assert result[0][0] == 5307116860887181994
+    query = 'SELECT @@PG_SCHEMA@@.QUADBIN_FROMLONGLAT(0.0, 5.364418029785158e-06, 26)'
+    result = run_query(query)
+    assert len(result[0]) == 1
+    assert result[0][0] == 5307116860887181994
+    query = 'SELECT @@PG_SCHEMA@@.QUADBIN_FROMLONGLAT(0.0, 5.364418029785156e-06, 26)'
+    result = run_query(query)
+    assert len(result[0]) == 1
+    assert result[0][0] == 5307116860887181994
