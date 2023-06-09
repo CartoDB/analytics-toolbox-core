dd0eeda3d06b9859222a51cde4906d6f07b5c1b0
Luigi Pirelli <luipir@gmail.com>
2023-06-09 11:18:58 +0200

    adapted tests because order ot array (as string) changed

--- clouds/bigquery/modules/test/quadbin/QUADBIN_TOCHILDREN.test.js
+++ clouds/bigquery/modules/test/quadbin/QUADBIN_TOCHILDREN.test.js
@@ -4,7 +4,7 @@ test('QUADBIN_TOCHILDREN should work', async () => {
     const query = 'SELECT TO_JSON_STRING(`@@BQ_DATASET@@.QUADBIN_TOCHILDREN`(5209574053332910079, 5)) AS output';
     const rows = await runQuery(query);
     expect(rows.length).toEqual(1);
-    expect(rows[0].output).toEqual('["5214064458820747263","5214073254913769471","5214068856867258367","5214077652960280575"]');
+    expect(rows[0].output).toEqual('["5214064458820747263","5214068856867258367","5214073254913769471","5214077652960280575"]');
 });
 
 test('QUADBIN_TOCHILDREN should throw an error for negative resolution', async () => {
