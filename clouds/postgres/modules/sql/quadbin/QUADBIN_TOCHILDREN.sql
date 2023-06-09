6b02e40df4e5f3a138bcb9aadcf78b18b2a30f3f
Luigi Pirelli <luipir@gmail.com>
2023-06-09 17:05:47 +0200

    tochildren O(n) porting quadbin-py code to sql for PG

--- clouds/postgres/modules/sql/quadbin/QUADBIN_TOCHILDREN.sql
+++ clouds/postgres/modules/sql/quadbin/QUADBIN_TOCHILDREN.sql
@@ -3,24 +3,48 @@
 ----------------------------
 
 CREATE OR REPLACE FUNCTION @@PG_SCHEMA@@.QUADBIN_TOCHILDREN(
-    quadbin BIGINT,
-    resolution INT
+    cell BIGINT,
+    children_resolution INT
 )
 RETURNS BIGINT[]
 AS
 $BODY$
     SELECT CASE
-    WHEN resolution < 0 OR resolution > 26 OR resolution < ((quadbin >> 52) & 31)
+    WHEN children_resolution < 0 OR children_resolution > 26 OR children_resolution < ((cell >> 52) & 31)
     THEN @@PG_SCHEMA@@.__CARTO_ERROR('Invalid resolution')::BIGINT[]
     ELSE (
-      WITH _zxy AS (
-          SELECT @@PG_SCHEMA@@.QUADBIN_TOZXY(quadbin) AS tile
-      )
-      SELECT ARRAY_AGG(@@PG_SCHEMA@@.QUADBIN_FROMZXY(resolution, xs, ys))
-      FROM _zxy,
-        GENERATE_SERIES((tile->>'x')::INT << (resolution - (tile->>'z')::INT), (((tile->>'x')::INT + 1) << (resolution - (tile->>'z')::INT)) - 1) as xs,
-        GENERATE_SERIES((tile->>'y')::INT << (resolution - (tile->>'z')::INT), (((tile->>'y')::INT + 1) << (resolution - (tile->>'z')::INT)) - 1) as ys
-    )
+        WITH
+        __0xValues as (
+            select
+                31 as zoom_shifted_mask  -- decimal of 0x1F
+        ),
+        __constants AS (
+            SELECT
+                ~(zoom_shifted_mask::bigint << 52) AS zoom_level_mask,
+                ((cell >> 52) & zoom_shifted_mask)::int AS tile_z
+            FROM __0xValues
+        ),
+        __block_constants AS (
+            SELECT
+            (children_resolution - tile_z) AS resolution_diff,
+            (1::bigint << ((children_resolution - tile_z) << 1)::int) AS block_range,
+            (52 - (children_resolution << 1)) AS block_shift
+            FROM __constants
+        ),
+        __childbase_constants AS (
+            SELECT
+                ((cell & zoom_level_mask) | (children_resolution::bigint << 52)) & ~((block_range - 1) << block_shift)
+                    AS child_base
+            FROM __block_constants,
+                __constants
+        )
+        SELECT
+            array_agg(child_base | (block << block_shift))
+        FROM
+            __block_constants,
+            __childbase_constants,
+            generate_series(0, block_range-1) as block
+        )
     END
 $BODY$
 LANGUAGE sql IMMUTABLE PARALLEL SAFE;
