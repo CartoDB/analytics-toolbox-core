07ef96dd806fd941692f92087d313b39004d3833
Luigi Pirelli <luipir@gmail.com>
2023-06-12 16:06:02 +0200

    chages to overcome some resource limits + mods due to review

--- clouds/bigquery/modules/sql/quadbin/QUADBIN_TOCHILDREN.sql
+++ clouds/bigquery/modules/sql/quadbin/QUADBIN_TOCHILDREN.sql
@@ -3,42 +3,53 @@
 ----------------------------
 
 CREATE OR REPLACE FUNCTION `@@BQ_DATASET@@.QUADBIN_TOCHILDREN`
-(cell INT64, children_resolution INT64)
+(quadbin INT64, resolution INT64)
 RETURNS ARRAY<INT64>
 AS ((
-    IF(children_resolution < 0 OR children_resolution > 26 OR children_resolution < ((children_resolution >> 52) & 0x1F),
+    IF(resolution < 0 OR resolution > 26 OR resolution < ((resolution >> 52) & 0x1F),
         ERROR('Invalid resolution'),
         (
             WITH
             __constants AS (
               SELECT
                 ~(0x1F << 52) AS zoom_level_mask,
-                ((cell >> 52) & 0x1F) AS tile_z
+                ((quadbin >> 52) & 0x1F) AS parent_resolution
             ),
             __block_constants AS (
               SELECT
-                (children_resolution - tile_z) AS resolution_diff,
-                (1 << ((children_resolution - tile_z) << 1)) AS block_range,
-                (52 - (children_resolution << 1)) AS block_shift
+                (resolution - parent_resolution) AS resolution_diff,
+                (1 << ((resolution - parent_resolution) << 1)) AS block_range,
+                CAST(SQRT((1 << ((resolution - parent_resolution) << 1))) as int) AS sqrt_block_range,
+                (52 - (resolution << 1)) AS block_shift
               FROM
                   __constants
             ),
             __childbase_constants AS (
               SELECT
-                ((cell & zoom_level_mask) | (children_resolution << 52)) & ~((block_range - 1) << block_shift)
+                ((quadbin & zoom_level_mask) | (resolution << 52)) & ~((block_range - 1) << block_shift)
                     AS child_base
               FROM
                 __block_constants,
                 __constants
+            ),
+            __block_indexes AS (
+                SELECT
+                    (block_raw * sqrt_block_range + block_column) AS block_index
+                FROM
+                    __block_constants,
+                    -- avoid to generate array from 0 to block_range that can be huge! =>
+                    -- splitting it in rows and columns
+                    UNNEST(GENERATE_ARRAY(0, sqrt_block_range - 1)) AS block_raw,
+                    UNNEST(GENERATE_ARRAY(0, sqrt_block_range - 1)) AS block_column
             )
             SELECT
               ARRAY(
                 SELECT
-                    child_base | (block << block_shift)
+                    child_base | (block_index << block_shift)
                 FROM
                     __block_constants,
                     __childbase_constants,
-                    UNNEST(GENERATE_ARRAY(0, block_range - 1)) AS block
+                    __block_indexes
               )
         )
     )
