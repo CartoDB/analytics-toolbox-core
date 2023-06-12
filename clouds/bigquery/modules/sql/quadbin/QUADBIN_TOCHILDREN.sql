aa59b21ccddf5b17635543d1f9087177de4bdb57
Luigi Pirelli <luipir@gmail.com>
2023-06-12 16:21:28 +0200

    compact and reduce CTEs

--- clouds/bigquery/modules/sql/quadbin/QUADBIN_TOCHILDREN.sql
+++ clouds/bigquery/modules/sql/quadbin/QUADBIN_TOCHILDREN.sql
@@ -13,43 +13,27 @@ AS ((
             __constants AS (
               SELECT
                 ~(0x1F << 52) AS zoom_level_mask,
-                ((quadbin >> 52) & 0x1F) AS parent_resolution
-            ),
-            __block_constants AS (
-              SELECT
-                (resolution - parent_resolution) AS resolution_diff,
-                (1 << ((resolution - parent_resolution) << 1)) AS block_range,
-                CAST(SQRT((1 << ((resolution - parent_resolution) << 1))) as int) AS sqrt_block_range,
+                (resolution - ((quadbin >> 52) & 0x1F)) AS resolution_diff,
+                (1 << ((resolution - ((quadbin >> 52) & 0x1F)) << 1)) AS block_range,
+                CAST(SQRT((1 << ((resolution - ((quadbin >> 52) & 0x1F)) << 1))) as int) AS sqrt_block_range,
                 (52 - (resolution << 1)) AS block_shift
-              FROM
-                  __constants
             ),
             __childbase_constants AS (
               SELECT
                 ((quadbin & zoom_level_mask) | (resolution << 52)) & ~((block_range - 1) << block_shift)
                     AS child_base
               FROM
-                __block_constants,
                 __constants
-            ),
-            __block_indexes AS (
-                SELECT
-                    (block_raw * sqrt_block_range + block_column) AS block_index
-                FROM
-                    __block_constants,
-                    -- avoid to generate array from 0 to block_range that can be huge! =>
-                    -- splitting it in rows and columns
-                    UNNEST(GENERATE_ARRAY(0, sqrt_block_range - 1)) AS block_raw,
-                    UNNEST(GENERATE_ARRAY(0, sqrt_block_range - 1)) AS block_column
             )
             SELECT
               ARRAY(
                 SELECT
-                    child_base | (block_index << block_shift)
+                    child_base | ((block_raw * sqrt_block_range + block_column) << block_shift)
                 FROM
-                    __block_constants,
+                    __constants,
                     __childbase_constants,
-                    __block_indexes
+                    UNNEST(GENERATE_ARRAY(0, sqrt_block_range - 1)) AS block_raw,
+                    UNNEST(GENERATE_ARRAY(0, sqrt_block_range - 1)) AS block_column
               )
         )
     )
