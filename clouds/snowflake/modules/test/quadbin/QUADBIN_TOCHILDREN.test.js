beb98d33b09be49e635197736e2b0f928ef75e0e
Luigi Pirelli <luipir@gmail.com>
2023-06-15 13:02:48 +0200

    avoid to fail test due to array order

--- clouds/snowflake/modules/test/quadbin/QUADBIN_TOCHILDREN.test.js
+++ clouds/snowflake/modules/test/quadbin/QUADBIN_TOCHILDREN.test.js
@@ -4,7 +4,8 @@ test('QUADBIN_TOCHILDREN should work', async () => {
     const query = 'SELECT ARRAY_TO_STRING(QUADBIN_TOCHILDREN(5209574053332910079, 5), \',\') AS OUTPUT';
     const rows = await runQuery(query);
     expect(rows.length).toEqual(1);
-    expect(rows[0].OUTPUT).toEqual('5214064458820747263,5214073254913769471,5214068856867258367,5214077652960280575');
+    const out = rows[0].OUTPUT.split(',').sort().join(',')
+    expect(rows[0].OUTPUT).toEqual('5214064458820747263,5214068856867258367,5214073254913769471,5214077652960280575');
 
 });
 
