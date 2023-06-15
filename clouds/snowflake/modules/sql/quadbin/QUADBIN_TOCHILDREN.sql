9c05c1fa32f6d0300bb2089dfa11fa89b2b87854
Luigi Pirelli <luipir@gmail.com>
2023-06-15 11:49:22 +0200

    a working verion in console but need some fix via API

--- clouds/snowflake/modules/sql/quadbin/QUADBIN_TOCHILDREN.sql
+++ clouds/snowflake/modules/sql/quadbin/QUADBIN_TOCHILDREN.sql
@@ -2,55 +2,73 @@
 -- Copyright (C) 2022 CARTO
 ----------------------------
 
-CREATE OR REPLACE FUNCTION @@SF_SCHEMA@@._QUADBIN_TOCHILDREN
-(index STRING, resolution DOUBLE)
-RETURNS STRING
-LANGUAGE JAVASCRIPT
-IMMUTABLE
-AS $$
-
-    @@SF_LIBRARY_QUADBIN@@
-
-    const res = Number(RESOLUTION);
-
-    if (INDEX == null || res == null) {
-        throw new Error('NULL argument passed to UDF');
-    }
-
-    if (res < 0 || res > 26) {
-        throw new Error('Invalid resolution, it must be between 0 and 26.');
-    }
-
-    const tile = quadbinLib.quadbinToTile(INDEX);
-
-    if (res < tile.z) {
-        throw new Error('Invalid resolution, it should be higher than the quadbin level.');
-    }
-
-    const xmin = tile.x << (res - tile.z);
-    const xmax = ((tile.x + 1) << (res - tile.z)) - 1;
-    const ymin = tile.y << (res - tile.z);
-    const ymax = ((tile.y + 1) << (res - tile.z)) - 1;
-
-    const children = [];
-
-    for (let dx=xmin; dx<=xmax; dx+=1) {
-        for (let dy=ymin; dy<=ymax; dy+=1) {
-            const child = quadbinLib.tileToQuadbin(
-                {z: res, x: dx, y: dy}
-            );
-            children.push(String(BigInt(`0x${child}`)));
-        }
-    }
-
-    return '[' + children.join(',') + ']';
-$$;
-
-
 CREATE OR REPLACE SECURE FUNCTION @@SF_SCHEMA@@.QUADBIN_TOCHILDREN
 (quadbin BIGINT, resolution INT)
 RETURNS ARRAY
 IMMUTABLE
 AS $$
-    TO_ARRAY(PARSE_JSON(@@SF_SCHEMA@@._QUADBIN_TOCHILDREN(TO_VARCHAR(QUADBIN, 'xxxxxxxxxxxxxxxx'), RESOLUTION)))
+    --TO_ARRAY(PARSE_JSON(@@SF_SCHEMA@@._QUADBIN_TOCHILDREN(TO_VARCHAR(QUADBIN, 'xxxxxxxxxxxxxxxx'), RESOLUTION)))
+    WITH
+    __constants AS (
+    SELECT
+        BITNOT(BITSHIFTLEFT(31, 52)) AS zoom_level_mask,
+        BITSHIFTLEFT(
+            1,
+            BITSHIFTLEFT(
+                (resolution - 
+                BITAND(
+                    BITSHIFTRIGHT(quadbin, 52),
+                    31
+                )),
+                1
+            )
+        ) AS block_range,
+        BITSHIFTLEFT(
+            1,
+            (resolution -
+            BITAND(
+                BITSHIFTRIGHT(quadbin, 52),
+                31
+            ))
+        ) AS sqrt_block_range,
+        (52 - BITSHIFTLEFT(resolution, 1)) AS block_shift
+    ),
+    __childbase_constants AS (
+    SELECT
+        BITAND(
+            BITOR(
+                BITAND(
+                    quadbin,
+                    zoom_level_mask
+                ),
+                BITSHIFTLEFT(resolution, 52)
+            ),
+            BITNOT(BITSHIFTLEFT((block_range - 1), block_shift))
+        ) AS child_base
+    FROM
+        __constants
+    ),
+    __indexes AS (
+        SELECT
+            ARRAY_GENERATE_RANGE(0, sqrt_block_range ) AS block_rows,
+            ARRAY_GENERATE_RANGE(0, sqrt_block_range ) AS block_columns
+        FROM
+            __constants
+    )
+    SELECT
+        ARRAY_AGG(
+            BITOR(
+                child_base,
+                BITSHIFTLEFT(
+                    (block_row.index * sqrt_block_range + block_column.index),
+                    block_shift
+                )
+            )
+        )
+    FROM
+        __constants,
+        __childbase_constants,
+        __indexes,
+        table(flatten( input => block_rows)) block_row,
+        table(flatten( input => block_columns)) block_column
 $$;
