4e2b7e59aec8c771c3669de60f0a66b6bcf12cd5
Luigi Pirelli <luipir@gmail.com>
2023-06-15 18:14:03 +0200

    removed CTE with hex constants that make SF fail internally

--- clouds/snowflake/modules/sql/quadbin/QUADBIN_FROMZXY.sql
+++ clouds/snowflake/modules/sql/quadbin/QUADBIN_FROMZXY.sql
@@ -8,18 +8,6 @@ RETURNS BIGINT
 IMMUTABLE
 AS $$
     WITH
-    __hexConstants AS (
-        SELECT
-            31 AS _0X1F,
-            4503599627370495    AS _0x000FFFFFFFFFFFFF,
-            6148914691236517205 AS _0x5555555555555555, -- 0101
-            3689348814741910323 AS _0x3333333333333333, -- 0011
-            1085102592571150095 AS _0x0F0F0F0F0F0F0F0F, -- 1111
-            71777214294589695   AS _0x00FF00FF00FF00FF,
-            281470681808895     AS _0x0000FFFF0000FFFF,
-            4294967295          AS _0x00000000FFFFFFFF,
-            4611686018427387904 AS _0x4000000000000000
-    ),
     __ints AS (
         SELECT
             z AS zz,
@@ -28,52 +16,52 @@ AS $$
     ),
     __interleaved1 AS (
         SELECT
-            BITAND(BITOR(xx, BITSHIFTLEFT(xx, 16)), _0x0000FFFF0000FFFF) AS xx,
-            BITAND(BITOR(yy, BITSHIFTLEFT(yy, 16)), _0x0000FFFF0000FFFF) AS yy,
+            BITAND(BITOR(xx, BITSHIFTLEFT(xx, 16)), 281470681808895) AS xx,
+            BITAND(BITOR(yy, BITSHIFTLEFT(yy, 16)), 281470681808895) AS yy,
             zz
-        FROM __ints, __hexConstants
+        FROM __ints
     ),
     __interleaved2 AS (
         SELECT
-            BITAND(BITOR(xx, BITSHIFTLEFT(xx, 8)), _0x00FF00FF00FF00FF) AS xx,
-            BITAND(BITOR(yy, BITSHIFTLEFT(yy, 8)), _0x00FF00FF00FF00FF) AS yy,
+            BITAND(BITOR(xx, BITSHIFTLEFT(xx, 8)), 71777214294589695) AS xx,
+            BITAND(BITOR(yy, BITSHIFTLEFT(yy, 8)), 71777214294589695) AS yy,
             zz
-        FROM __interleaved1, __hexConstants
+        FROM __interleaved1
     ),
     __interleaved3 AS (
         SELECT
-            BITAND(BITOR(xx, BITSHIFTLEFT(xx, 4)), _0x0F0F0F0F0F0F0F0F) AS xx,
-            BITAND(BITOR(yy, BITSHIFTLEFT(yy, 4)), _0x0F0F0F0F0F0F0F0F) AS yy,
+            BITAND(BITOR(xx, BITSHIFTLEFT(xx, 4)), 1085102592571150095) AS xx,
+            BITAND(BITOR(yy, BITSHIFTLEFT(yy, 4)), 1085102592571150095) AS yy,
             zz
-        FROM __interleaved2, __hexConstants
+        FROM __interleaved2
     ),
     __interleaved4 AS (
         SELECT
-            BITAND(BITOR(xx, BITSHIFTLEFT(xx, 2)), _0x3333333333333333) AS xx,
-            BITAND(BITOR(yy, BITSHIFTLEFT(yy, 2)), _0x3333333333333333) AS yy,
+            BITAND(BITOR(xx, BITSHIFTLEFT(xx, 2)), 3689348814741910323) AS xx,
+            BITAND(BITOR(yy, BITSHIFTLEFT(yy, 2)), 3689348814741910323) AS yy,
             zz
-        FROM __interleaved3, __hexConstants
+        FROM __interleaved3
     ),
     __interleaved5 AS (
         SELECT
-            BITAND(BITOR(xx, BITSHIFTLEFT(xx, 1)), _0x5555555555555555) AS xx,
-            BITAND(BITOR(yy, BITSHIFTLEFT(yy, 1)), _0x5555555555555555) AS yy,
+            BITAND(BITOR(xx, BITSHIFTLEFT(xx, 1)), 6148914691236517205) AS xx,
+            BITAND(BITOR(yy, BITSHIFTLEFT(yy, 1)), 6148914691236517205) AS yy,
             zz
-        FROM __interleaved4, __hexConstants
+        FROM __interleaved4
     )
     SELECT
         BITOR(
-            _0x4000000000000000,
+            4611686018427387904,
             BITOR(
                 BITSHIFTLEFT(1, 59), -- | (mode << 59) | (mode_dep << 57)
                 BITOR(
                     BITSHIFTLEFT(zz, 52),
                     BITOR(
                         BITSHIFTRIGHT(BITOR(xx, BITSHIFTLEFT(yy, 1)), 12),
-                        BITSHIFTRIGHT(_0x000FFFFFFFFFFFFF, (zz * 2))
+                        BITSHIFTRIGHT(4503599627370495, (zz * 2))
                     )
                 )
             )
         )
-    FROM __interleaved5, __hexConstants
+    FROM __interleaved5
  $$;
