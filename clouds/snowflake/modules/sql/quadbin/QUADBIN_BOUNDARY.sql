acb570b4f78fc94d93eef3a182237223de7de856
Luigi Pirelli <luipir@gmail.com>
2023-06-16 17:15:02 +0200

    moved QUADBIN_BOUNDARY from JS to SQL and adapted test to lesser accuracy (sub mm)

--- clouds/snowflake/modules/sql/quadbin/QUADBIN_BOUNDARY.sql
+++ clouds/snowflake/modules/sql/quadbin/QUADBIN_BOUNDARY.sql
@@ -1,28 +1,6 @@
 ----------------------------
--- Copyright (C) 2022 CARTO
+-- Copyright (C) 2023 CARTO
 ----------------------------
-
-CREATE OR REPLACE FUNCTION @@SF_SCHEMA@@._GET_QUADBIN_POLYGON
-(index STRING)
-RETURNS STRING
-LANGUAGE JAVASCRIPT
-IMMUTABLE
-AS $$
-    if (!INDEX) {
-        return [];
-    }
-
-    @@SF_LIBRARY_QUADBIN@@
-
-    const arrayPolygon = quadbinLib.getQuadbinPolygon(INDEX);
-    return 'LINESTRING(' +
-        arrayPolygon[0] + ' ' + arrayPolygon[1] + ',' +
-        arrayPolygon[2] + ' ' + arrayPolygon[3] + ',' +
-        arrayPolygon[4] + ' ' + arrayPolygon[5] + ',' +
-        arrayPolygon[6] + ' ' + arrayPolygon[7] + ',' +
-        arrayPolygon[8] + ' ' + arrayPolygon[9] +
-    ')';
-$$;
 
 
 CREATE OR REPLACE SECURE FUNCTION @@SF_SCHEMA@@.QUADBIN_BOUNDARY
@@ -30,7 +8,21 @@ CREATE OR REPLACE SECURE FUNCTION @@SF_SCHEMA@@.QUADBIN_BOUNDARY
 RETURNS GEOGRAPHY
 IMMUTABLE
 AS $$
-    SELECT ST_MAKEPOLYGON(
-        TRY_TO_GEOGRAPHY(@@SF_SCHEMA@@._GET_QUADBIN_POLYGON(TO_VARCHAR(QUADBIN, 'xxxxxxxxxxxxxxxx')))
+    WITH
+    __bbox AS (
+        SELECT
+            @@SF_SCHEMA@@.QUADBIN_BBOX(quadbin) AS corners
     )
+    SELECT
+        ST_MAKEPOLYGON(
+            TRY_TO_GEOGRAPHY(
+                'LINESTRING(' || 
+                corners[2]::DECIMAL(18,15) ||  ' ' ||  corners[3]::DECIMAL(18,15) ||  ',' || 
+                corners[2]::DECIMAL(18,15) ||  ' ' ||  corners[1]::DECIMAL(18,15) ||  ',' || 
+                corners[0]::DECIMAL(18,15) ||  ' ' ||  corners[1]::DECIMAL(18,15) ||  ',' || 
+                corners[0]::DECIMAL(18,15) ||  ' ' ||  corners[3]::DECIMAL(18,15) ||  ',' || 
+                corners[2]::DECIMAL(18,15) ||  ' ' ||  corners[3]::DECIMAL(18,15) || ')'
+            )
+        )
+    FROM __bbox
 $$;
