da91a36b4ab0ac49b25fef7c8f0ddd5e025c812e
Luigi Pirelli <luipir@gmail.com>
2023-06-16 16:47:34 +0200

    moved from JS to pure SQL for QUADBIN_BBOX

--- clouds/snowflake/modules/sql/quadbin/QUADBIN_BBOX.sql
+++ clouds/snowflake/modules/sql/quadbin/QUADBIN_BBOX.sql
@@ -1,34 +1,50 @@
 ----------------------------
--- Copyright (C) 2022 CARTO
+-- Copyright (C) 2023 CARTO
 ----------------------------
-
-CREATE OR REPLACE FUNCTION @@SF_SCHEMA@@._QUADBIN_BBOX
-(index STRING)
-RETURNS ARRAY
-LANGUAGE JAVASCRIPT
-IMMUTABLE
-AS $$
-    if (!INDEX) {
-        return [];
-    }
-
-    @@SF_LIBRARY_QUADBIN@@
-
-    return quadbinLib.getQuadbinBoundingBox(INDEX);
-$$;
 
 CREATE OR REPLACE SECURE FUNCTION @@SF_SCHEMA@@.QUADBIN_BBOX
 (quadbin BIGINT)
 RETURNS ARRAY
 IMMUTABLE
 AS $$
-    WITH array_polygon AS (
-        SELECT @@SF_SCHEMA@@._QUADBIN_BBOX(TO_VARCHAR(QUADBIN, 'xxxxxxxxxxxxxxxx')) p
-    )
-    SELECT
-        CASE
-            WHEN ARRAY_SIZE(p) = 0 THEN NULL
-            ELSE p
-        END
-    FROM array_polygon
+    CASE quadbin
+        WHEN NULL THEN
+            NULL
+        ELSE (
+            WITH
+            __zxy AS (
+                SELECT
+                    @@SF_SCHEMA@@.QUADBIN_TOZXY(
+                        quadbin
+                    ) AS tile,
+                    ACOS(-1) AS pi
+            )
+            SELECT [
+                180 * (2.0 * tile:x / BITSHIFTLEFT(1, tile:z)::FLOAT8 - 1.0),
+                360 * (
+                    ATAN(
+                        EXP(
+                            -(
+                                2.0 * (
+                                    tile:y + 1
+                                ) / BITSHIFTLEFT(1, tile:z)::FLOAT8 - 1
+                            ) * pi
+                        )
+                    ) / pi - 0.25
+                ),
+                180 * (
+                    2.0 * (tile:x + 1) / BITSHIFTLEFT(1, tile:z)::FLOAT8 - 1.0
+                ),
+                360 * (
+                    ATAN(
+                        EXP(
+                            -( 2.0 * tile:y / BITSHIFTLEFT(1, tile:z)::FLOAT8 - 1
+                             ) * pi
+                        )
+                    ) / pi - 0.25
+                )
+            ]    
+            FROM __zxy
+        )
+    END
 $$;
