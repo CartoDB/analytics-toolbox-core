0b51003b927ad0d3e46a6d9ee56bb29a802e4464
Luigi Pirelli <luipir@gmail.com>
2023-06-22 15:49:05 +0200

    almost ported QUADBIN_FROMQUADKEY from JS to SQL

--- clouds/snowflake/modules/sql/quadbin/QUADBIN_FROMQUADKEY.sql
+++ clouds/snowflake/modules/sql/quadbin/QUADBIN_FROMQUADKEY.sql
@@ -1,26 +1,37 @@
 ----------------------------
--- Copyright (C) 2022 CARTO
+-- Copyright (C) 2023 CARTO
 ----------------------------
-
-CREATE OR REPLACE FUNCTION @@SF_SCHEMA@@._QUADBIN_FROMQUADKEY
-(quadkey STRING)
-RETURNS STRING
-LANGUAGE JAVASCRIPT
-IMMUTABLE
-AS $$
-    const z = BigInt(QUADKEY.length);
-    const xy = BigInt(parseInt(QUADKEY, 4) || 0);
-    return (0x4000000000000000n |
-        (1n << 59n) |
-        (z << 52n) |
-        (xy << (52n - z*2n)) |
-        (0xFFFFFFFFFFFFFn >> (z*2n))).toString();
-$$;
 
 CREATE OR REPLACE FUNCTION @@SF_SCHEMA@@.QUADBIN_FROMQUADKEY
 (quadkey STRING)
 RETURNS BIGINT
 IMMUTABLE
 AS $$
-    SELECT CAST(@@SF_SCHEMA@@._QUADBIN_FROMQUADKEY(QUADKEY) AS BIGINT)
+    WITH
+    __params AS (
+        SELECT
+            LEN(quadkey) AS z,
+            (CASE
+                WHEN TRY_TO_NUMBER(quadkey) IS NULL THEN 
+                    0
+                ELSE
+                    TO_NUMBER(
+                        @@SF_SCHEMA@@._FROM_BASE(TRY_TO_NUMBER(quadkey), 4)
+                    )
+            END) AS xy
+    ),
+    __prepared AS (
+        SELECT
+            BITSHIFTLEFT(1, 59) AS param1,
+            BITSHIFTLEFT(z, 52) AS param2,
+            BITSHIFTLEFT(xy, (52-(z*2))) AS param3,
+            BITSHIFTRIGHT(4503599627370495, (z*2)) AS param4
+        FROM __params
+    )
+    SELECT
+        BITOR(4611686018427387904,
+            BITOR(param1,
+                BITOR(param2,
+                    BITOR(param3, param4))))
+    FROM __prepared
 $$;
