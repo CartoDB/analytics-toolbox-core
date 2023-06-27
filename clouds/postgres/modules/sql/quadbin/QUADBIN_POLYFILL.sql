0207001ba57e4b439f2148d3682abb7ee07f1b6f
Luigi Pirelli <luipir@gmail.com>
2023-06-27 18:11:13 +0200

    aligned casting to the original version + reduced CTE dpendencies

--- clouds/postgres/modules/sql/quadbin/QUADBIN_POLYFILL.sql
+++ clouds/postgres/modules/sql/quadbin/QUADBIN_POLYFILL.sql
@@ -10,18 +10,16 @@ RETURNS BIGINT[]
 AS
 $BODY$
   SELECT CASE
-    WHEN resolution < 0 OR resolution > 26
-        THEN @@PG_SCHEMA@@.__CARTO_ERROR(FORMAT('Invalid resolution "%s"; should be between 0 and 26', resolution))::BIGINT[]
-    WHEN resolution IS NULL OR geom IS NULL
-        THEN NULL::BIGINT[]
+    WHEN resolution < 0 OR resolution > 26 THEN @@PG_SCHEMA@@.__CARTO_ERROR(FORMAT('Invalid resolution "%s"; should be between 0 and 26', resolution))::BIGINT[]
+    WHEN resolution IS NULL OR geom IS NULL THEN NULL::BIGINT[]
     ELSE (
         WITH
         __geom4326 AS (
             select
-                CASE ST_SRID(geom)
+                (CASE ST_SRID(geom)
                     WHEN 0 THEN ST_SETSRID(geom, 4326)
                     ELSE ST_TRANSFORM(geom, 4326)
-                END AS geom4326
+                END) AS geom4326
         ),
         __bbox AS (
             SELECT geom4326, BOX2D(geom4326) AS b FROM __geom4326
@@ -34,14 +32,13 @@ $BODY$
                 ST_XMIN(b) AS minlon,
                 ST_YMIN(b) AS minlat,
                 ST_XMAX(b) AS maxlon,
-                ST_YMAX(b) AS maxlat,
-                ACOS(-1) AS pi
+                ST_YMAX(b) AS maxlat
             FROM __bbox
         ),
         __sinlat AS (
             SELECT
-                SIN(minlat * pi / 180.0) AS sinlat_min,
-                SIN(maxlat * pi / 180.0) AS sinlat_max
+                SIN(minlat * PI() / 180.0) AS sinlat_min,
+                SIN(maxlat * PI() / 180.0) AS sinlat_max
             FROM __params
         ),
         __xs AS (
@@ -56,49 +53,50 @@ $BODY$
         ),
         __tile_coords_range AS (
             SELECT
+                geom4326,
                 z,
 
-                CASE
+                (CASE
                     WHEN xmin < 0 THEN xmin + z2
                     ELSE xmin
-                END
-                AS xmin,
+                END)::INT AS xmin,
 
                 -- floor before cast to avoid up rounding to the next tile
                 FLOOR(
                     z2 * (
                         0.5 - 0.25 * LN(
                             (1 + sinlat_max) / (1 - sinlat_max)
-                        ) / pi
+                        ) / PI()
                     )
-                )::BIGINT AS ymin,
+                )::INT AS ymin,
 
-                CASE
+                (CASE
                     WHEN xmax < 0 THEN xmax + z2
                     ELSE xmax
-                END
-                AS xmax,
+                END)::INT AS xmax,
 
                 -- floor before cast to avoid up rounding to the next tiLe
                 FLOOR(
                     z2 * (
                         0.5 - 0.25 * LN(
                             (1 + sinlat_min) / (1 - sinlat_min)
-                        ) / pi
+                        ) / PI()
                     )
-                )::BIGINT AS ymax
+                )::INT AS ymax
 
             FROM __params, __xs, __sinlat
         ),
         __cells AS (
-            SELECT @@PG_SCHEMA@@.QUADBIN_FROMZXY(z::INT, x::INT, y::INT) AS quadbin
-            FROM __tile_coords_range as t,
-                generate_series(t.xmin, t.xmax) AS x,
-                generate_series(t.ymin, t.ymax) AS y
+            SELECT
+                geom4326,
+                @@PG_SCHEMA@@.QUADBIN_FROMZXY(z::INT, x::INT, y::INT) AS quadbin
+            FROM __tile_coords_range,
+                 generate_series(xmin, xmax) AS x,
+                 generate_series(ymin, ymax) AS y
         )
         SELECT ARRAY_AGG(quadbin)
-        FROM __cells, __params
-        WHERE ST_INTERSECTS(@@PG_SCHEMA@@.QUADBIN_BOUNDARY(quadbin), geom4326)
+        FROM __cells
+        WHERE ST_INTERSECTS(geom4326, @@PG_SCHEMA@@.QUADBIN_BOUNDARY(quadbin))
     )
     END;
 $BODY$
@@ -115,19 +113,21 @@ $BODY$
     WITH
     __geom4326 AS (
         select
-            CASE ST_SRID(geom)
+            (CASE ST_SRID(geom)
                 WHEN 0 THEN ST_SETSRID(geom, 4326)
                 ELSE ST_TRANSFORM(geom, 4326)
-            END AS geom4326
+            END) AS geom4326
     ),
-    cells AS (
-        SELECT quadbin
-        FROM
-            UNNEST(@@PG_SCHEMA@@.__QUADBIN_POLYFILL_INIT(geom, (resolution / 2)::INT)) AS parent,
-            UNNEST(@@PG_SCHEMA@@.QUADBIN_TOCHILDREN(parent, resolution)) AS quadbin
+    __cells AS (
+        SELECT
+            geom4326,
+            quadbin
+        FROM __geom4326,
+             UNNEST(@@PG_SCHEMA@@.__QUADBIN_POLYFILL_INIT(geom4326, (resolution / 2)::INT)) AS parent,
+             UNNEST(@@PG_SCHEMA@@.QUADBIN_TOCHILDREN(parent, resolution)) AS quadbin
     )
     SELECT ARRAY_AGG(quadbin)
-    FROM cells, __geom4326
+    FROM __cells
     WHERE ST_INTERSECTS(geom4326, @@PG_SCHEMA@@.QUADBIN_BOUNDARY(quadbin));
 $BODY$
 LANGUAGE sql IMMUTABLE PARALLEL SAFE;
@@ -145,14 +145,16 @@ $BODY$
                 ELSE ST_TRANSFORM(geom, 4326)
             END AS geom4326
     ),
-    cells AS (
-        SELECT quadbin
-        FROM
-            UNNEST(@@PG_SCHEMA@@.__QUADBIN_POLYFILL_INIT(geom, (resolution / 2)::INT)) AS parent,
-            UNNEST(@@PG_SCHEMA@@.QUADBIN_TOCHILDREN(parent, resolution)) AS quadbin
+    __cells AS (
+        SELECT
+            geom4326,
+            quadbin
+        FROM __geom4326,
+             UNNEST(@@PG_SCHEMA@@.__QUADBIN_POLYFILL_INIT(geom4326, (resolution / 2)::INT)) AS parent,
+             UNNEST(@@PG_SCHEMA@@.QUADBIN_TOCHILDREN(parent, resolution)) AS quadbin
     )
     SELECT ARRAY_AGG(quadbin)
-    FROM cells, __geom4326
+    FROM __cells
     WHERE ST_CONTAINS(geom4326, @@PG_SCHEMA@@.QUADBIN_BOUNDARY(quadbin))
 $BODY$
 LANGUAGE sql IMMUTABLE PARALLEL SAFE;
@@ -165,20 +167,22 @@ $BODY$
     WITH
     __geom4326 AS (
         SELECT
-            CASE ST_SRID(geom)
+            (CASE ST_SRID(geom)
                 WHEN 0 THEN ST_SETSRID(geom, 4326)
                 ELSE ST_TRANSFORM(geom, 4326)
-            END AS geom4326
+            END) AS geom4326
     ),
-    cells AS (
-        SELECT quadbin
-        FROM
-            UNNEST(@@PG_SCHEMA@@.__QUADBIN_POLYFILL_INIT(geom, (resolution / 2)::INT)) AS parent,
-            UNNEST(@@PG_SCHEMA@@.QUADBIN_TOCHILDREN(parent, resolution)) AS quadbin
+    __cells AS (
+        SELECT
+            geom4326,
+            quadbin
+        FROM __geom4326,
+             UNNEST(@@PG_SCHEMA@@.__QUADBIN_POLYFILL_INIT(geom4326, (resolution / 2)::INT)) AS parent,
+             UNNEST(@@PG_SCHEMA@@.QUADBIN_TOCHILDREN(parent, resolution)) AS quadbin
     )
     SELECT ARRAY_AGG(quadbin)
-    FROM cells, __geom4326
-    WHERE ST_INTERSECTS(geom, @@PG_SCHEMA@@.QUADBIN_CENTER(quadbin))
+    FROM __cells
+    WHERE ST_INTERSECTS(geom4326, @@PG_SCHEMA@@.QUADBIN_CENTER(quadbin))
 $BODY$
 LANGUAGE sql IMMUTABLE PARALLEL SAFE;
 
