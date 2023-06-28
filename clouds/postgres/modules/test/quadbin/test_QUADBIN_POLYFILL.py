9f4d0d6a7d34926b508fadbb3a5dac641d806770
Luigi Pirelli <luipir@gmail.com>
2023-06-28 18:16:54 +0200

    added test for different modes

--- clouds/postgres/modules/test/quadbin/test_QUADBIN_POLYFILL.py
+++ clouds/postgres/modules/test/quadbin/test_QUADBIN_POLYFILL.py
@@ -1,9 +1,10 @@
 # flake8: noqa
+import pytest
 from test_utils import run_query
 
 
 def test_quadbin_polyfill():
-    """Computes polyfill."""
+    """Computes polyfill. by default intersect mode"""
     result = run_query(
         """
       SELECT @@PG_SCHEMA@@.QUADBIN_POLYFILL(
@@ -24,3 +25,53 @@ def test_quadbin_polyfill():
         ]
     )
     assert sorted(result[0][0]) == expected
+
+
+def test_quadbin_polyfill_center():
+    """Computes polyfill center mode"""
+    result = run_query(
+        """
+      SELECT @@PG_SCHEMA@@.QUADBIN_POLYFILL_MODE(
+        ST_GEOMFROMTEXT(
+          'POLYGON ((-3.71219873428345 40.413365349070865, -3.7144088745117 40.40965661286395, -3.70659828186035 40.409525904775634, -3.71219873428345 40.413365349070865))'
+        )
+        ,17, 'center'
+      )"""
+    )
+    expected = sorted(
+        [
+            5265786693163941887,
+            5265786693164466175,
+            5265786693164728319
+        ]
+    )
+    assert sorted(result[0][0]) == expected
+
+
+def test_quadbin_polyfill_contains():
+    """Computes polyfill contain mode"""
+    result = run_query(
+        """
+      SELECT @@PG_SCHEMA@@.QUADBIN_POLYFILL_MODE(
+        ST_GEOMFROMTEXT(
+          'POLYGON ((-3.71219873428345 40.413365349070865, -3.7144088745117 40.40965661286395, -3.70659828186035 40.409525904775634, -3.71219873428345 40.413365349070865))'
+        )
+        ,18, 'contains'
+      )"""
+    )
+    expected = [5270290292791705599]
+    assert sorted(result[0][0]) == expected
+
+
+def test_quadbin_polyfill_wrong_mode():
+    """Computes polyfill contain mode"""
+    with pytest.raises(Exception):
+      run_query(
+          """
+        SELECT @@PG_SCHEMA@@.QUADBIN_POLYFILL_MODE(
+          ST_GEOMFROMTEXT(
+            'POLYGON ((-3.71219873428345 40.413365349070865, -3.7144088745117 40.40965661286395, -3.70659828186035 40.409525904775634, -3.71219873428345 40.413365349070865))'
+          )
+          ,17, 'wrongvalue'
+        )"""
+      )
