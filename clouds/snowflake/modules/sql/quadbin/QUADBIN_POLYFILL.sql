0554c7707a208c131b868ca3795f859fb7b71d81
Luigi Pirelli <luipir@gmail.com>
2023-07-06 17:39:22 +0200

    a working pure SQL version of polyfill optimization

--- clouds/snowflake/modules/sql/quadbin/QUADBIN_POLYFILL.sql
+++ clouds/snowflake/modules/sql/quadbin/QUADBIN_POLYFILL.sql
@@ -2,7 +2,8 @@
 -- Copyright (C) 2021 CARTO
 ----------------------------
 
-CREATE OR REPLACE FUNCTION @@SF_SCHEMA@@._QUADBIN_POLYFILL
+
+CREATE OR REPLACE FUNCTION @@SF_SCHEMA@@._QUADBIN_POLYFILL_JSINIT
 (geojson STRING, resolution DOUBLE)
 RETURNS STRING
 LANGUAGE JAVASCRIPT
@@ -30,10 +31,203 @@ AS $$
     return '[' + quadbins.join(',') + ']';
 $$;
 
-CREATE OR REPLACE SECURE FUNCTION @@SF_SCHEMA@@.QUADBIN_POLYFILL
-(geo GEOGRAPHY, resolution INT)
+
+CREATE OR REPLACE SECURE FUNCTION @@SF_SCHEMA@@._QUADBIN_POLYFILL_INIT
+(geog GEOGRAPHY, resolution NUMBER)
 RETURNS ARRAY
-IMMUTABLE
 AS $$
-    TO_ARRAY(PARSE_JSON(@@SF_SCHEMA@@._QUADBIN_POLYFILL(CAST(ST_ASGEOJSON(GEO) AS STRING),CAST(RESOLUTION AS DOUBLE))))
+    IFF(geog IS NULL OR resolution IS NULL,
+        NULL, (
+        IFF(resolution < 0 OR resolution > 26,
+            NULL, (
+            WITH
+            __params AS (
+                SELECT
+                    resolution AS z,
+                    ST_XMIN(geog) AS minlon,
+                    ST_YMIN(geog) AS minlat,
+                    ST_XMAX(geog) AS maxlon,
+                    ST_YMAX(geog) AS maxlat,
+                    BITSHIFTLEFT(1::NUMBER, resolution) AS z2,
+                    ACOS(-1) AS pi
+            ),
+            __sinlat AS (
+                SELECT
+                    SIN(minlat * pi / 180.0) AS sinlat_min,
+                    SIN(maxlat * pi / 180.0) AS sinlat_max
+                FROM __params
+            ),
+            __xs AS (
+                -- precalculate Xs to allow simple use of
+                -- CASE in the next CTE
+                SELECT
+                    BITAND(
+                        CAST(
+                            FLOOR(z2 * ((minlon / 360.0) + 0.5)) AS NUMBER
+                        ),
+                        (z2 - 1)   -- bitwise way to calc MODULO
+                    ) AS xmin,
+                    BITAND(
+                        CAST(
+                            FLOOR(z2 * ((maxlon / 360.0) + 0.5)) AS NUMBER
+                        ),
+                        (z2 - 1)   -- bitwise way to calc MODULO
+                    ) AS xmax
+                FROM __params
+            ),
+            __tile_coords_range AS (
+                SELECT
+                    z,
+
+                    CASE
+                        WHEN xmin < 0 THEN xmin + z2
+                        ELSE xmin
+                    END
+                    AS xmin,
+
+                    CAST(
+                        -- floor before cast to avoid up rounding to the next tile
+                        FLOOR(
+                            z2 * (
+                                0.5 - 0.25 * LN(
+                                    (1 + sinlat_max) / (1 - sinlat_max)
+                                ) / pi
+                            )
+                        ) AS NUMBER
+                    )
+                    AS ymin,
+
+                    CASE
+                        WHEN xmax < 0 THEN xmax + z2
+                        ELSE xmax
+                    END
+                    AS xmax,
+
+                    CAST(
+                        -- floor before cast to avoid up rounding to the next tiLe
+                        FLOOR(
+                            z2 * (
+                                0.5 - 0.25 * LN(
+                                    (1 + sinlat_min) / (1 - sinlat_min)
+                                ) / pi
+                            )
+                        ) AS NUMBER
+                    )
+                    AS ymax
+
+                FROM __params, __xs, __sinlat
+            ),
+            __cells AS (
+                SELECT @@SF_SCHEMA@@.QUADBIN_FROMZXY(z, x.value, y.value) AS quadbin
+                FROM __tile_coords_range,
+                    lateral FLATTEN(ARRAY_GENERATE_RANGE(xmin, xmax)) AS x,
+                    lateral FLATTEN(ARRAY_GENERATE_RANGE(ymin, ymax)) AS y
+            )
+            SELECT ARRAY_AGG(quadbin)
+            FROM __cells
+            WHERE ST_INTERSECTS(@@SF_SCHEMA@@.QUADBIN_BOUNDARY(quadbin), geog)
+            )
+        ))
+    )
+$$;
+
+-- Utility function to optimize polyfill unzooming and getting childrens
+-- of unzoomed parents. Thios would allow server side SQL parallelization
+-- NOTE! no intersection is done due to generation of internale error =>
+-- cleaning of quadbins have to be done extenally
+CREATE OR REPLACE FUNCTION @@SF_SCHEMA@@._QUADBIN_POLYFILL_CHILDRENS
+(geog GEOGRAPHY, resolution INT)
+RETURNS ARRAY
+AS $$
+    WITH
+    _init AS (
+        SELECT
+            // TO_ARRAY(
+            //     PARSE_JSON(
+            //         @@SF_SCHEMA@@._QUADBIN_POLYFILL_JSINIT(
+            //             CAST(ST_ASGEOJSON(GEOG) AS STRING),
+            //             GREATEST(0, CAST(RESOLUTION-2 AS DOUBLE))
+            //         )
+            //     )
+            // ) AS quadbins_array
+            @@SF_SCHEMA@@._QUADBIN_POLYFILL_INIT(
+                        GEOG,
+                        GREATEST(0, resolution - 2)
+            ) AS quadbins_array
+    ),
+    _parents AS (
+        SELECT
+            cast(res.value as bigint) AS parent_quadbin
+        FROM
+            _init,
+            lateral FLATTEN(quadbins_array) as res
+    ),
+    _childrens AS (
+        SELECT
+            @@SF_SCHEMA@@.QUADBIN_TOCHILDREN(parent_quadbin, resolution) as child
+        FROM _parents
+    )
+    SELECT ARRAY_AGG(child[0])
+    FROM _childrens
+$$;
+
+CREATE OR REPLACE FUNCTION @@SF_SCHEMA@@._QUADBIN_POLYFILL_CHILDREN_INTERSECTS
+(geog GEOGRAPHY, resolution INT)
+RETURNS ARRAY
+AS $$
+    WITH
+    _childrens AS (
+        SELECT
+            @@SF_SCHEMA@@._QUADBIN_POLYFILL_CHILDRENS(geog, resolution) as child
+    )
+    SELECT ARRAY_AGG(child[0])
+    FROM _childrens
+    WHERE ST_INTERSECTS(geog, @@SF_SCHEMA@@.QUADBIN_BOUNDARY(child[0]))
+$$;
+
+CREATE OR REPLACE FUNCTION @@SF_SCHEMA@@._QUADBIN_POLYFILL_CHILDREN_CONTAINS
+(geog GEOGRAPHY, resolution INT)
+RETURNS ARRAY
+AS $$
+    WITH
+    _childrens AS (
+        SELECT
+            @@SF_SCHEMA@@._QUADBIN_POLYFILL_CHILDRENS(geog, resolution) as child
+    )
+    SELECT ARRAY_AGG(child[0])
+    FROM _childrens
+    WHERE ST_CONTAINS(geog, @@SF_SCHEMA@@.QUADBIN_BOUNDARY(child[0]))
+$$;
+
+CREATE OR REPLACE FUNCTION @@SF_SCHEMA@@._QUADBIN_POLYFILL_CHILDREN_CENTER
+(geog GEOGRAPHY, resolution INT)
+RETURNS ARRAY
+AS $$
+    WITH
+    _childrens AS (
+        SELECT
+            @@SF_SCHEMA@@._QUADBIN_POLYFILL_CHILDRENS(geog, resolution) as child
+    )
+    SELECT ARRAY_AGG(child[0])
+    FROM _childrens
+    WHERE ST_INTERSECTS(geog, @@SF_SCHEMA@@.QUADBIN_CENTER(child[0]))
+$$;
+
+CREATE OR REPLACE FUNCTION @@SF_SCHEMA@@.QUADBIN_POLYFILL_MODE
+(geog GEOGRAPHY, resolution INT, mode STRING)
+RETURNS ARRAY
+AS $$
+    SELECT
+        CASE mode
+            WHEN 'intersects' THEN @@SF_SCHEMA@@._QUADBIN_POLYFILL_CHILDREN_INTERSECTS(geog, resolution)
+            WHEN 'contains' THEN @@SF_SCHEMA@@._QUADBIN_POLYFILL_CHILDREN_CONTAINS(geog, resolution)
+            WHEN 'center' THEN @@SF_SCHEMA@@._QUADBIN_POLYFILL_CHILDREN_CENTER(geog, resolution)
+        END
+$$;
+
+CREATE OR REPLACE SECURE FUNCTION @@SF_SCHEMA@@.QUADBIN_POLYFILL
+(geog GEOGRAPHY, resolution INT)
+RETURNS ARRAY
+AS $$
+    SELECT @@SF_SCHEMA@@._QUADBIN_POLYFILL_CHILDREN_INTERSECTS(geog, resolution)
 $$;
