e43edd8ff237bde3f6bb30beb53959262c665577
Luigi Pirelli <luipir@gmail.com>
2023-07-10 12:04:30 +0200

    tests for QUADBIN_POLYFILL_MODE for snowflake

--- clouds/snowflake/modules/test/quadbin/QUADBIN_POLYFILL_MODE.test.js
+++ clouds/snowflake/modules/test/quadbin/QUADBIN_POLYFILL_MODE.test.js
@@ -31,3 +31,82 @@ test('QUADBIN_POLYFILL_MODE all modes wrong input', async () => {
     }
 });
 
+test('H3_POLYFILL_MODE polygons multiple modes', async () => {
+    const modes = [
+        'intersects',
+        'contains',
+        'center'
+    ]
+    const inputs = [
+        // normal polygons
+        {
+            geom: "ST_GEOGFROMTEXT('POLYGON((-122.4089866999972145 37.813318999983238, -122.3805436999997056 37.7866302000007224, -122.3544736999993603 37.7198061999978478, -122.5123436999983966 37.7076131999975672, -122.5247187000021967 37.7835871999971715, -122.4798767000009008 37.8151571999998453, -122.4089866999972145 37.813318999983238))')",
+            resolution: 14,
+            center: 36,
+            intersects: 52,
+            contains: 24
+        },
+        {
+            geom: "ST_GEOGFROMTEXT('POLYGON((0 0, 0 10, 10 10, 10 0, 0 0))')",
+            resolution: 7,
+            center: 16,
+            intersects: 18,
+            contains: 4
+        },
+        {
+            geom: "ST_GEOGFROMTEXT('POLYGON((20 20, 20 30, 30 30, 30 20, 20 20))')",
+            resolution: 7,
+            center: 16,
+            intersects: 20,
+            contains: 6
+        },
+        // multipolygons
+        {
+            geom: "ST_GEOGFROMTEXT('MULTIPOLYGON(((0 0, 0 10, 10 10, 10 0, 0 0)), ((20 20, 20 30, 30 30, 30 20, 20 20)))')",
+            resolution: 7,
+            center: 32,
+            intersects: 38,
+            contains: 10
+        },
+        {
+            geom: "ST_GEOGFROMTEXT('GEOMETRYCOLLECTION(POLYGON((20 20, 20 30, 30 30, 30 20, 20 20)), POINT(0 10), LINESTRING(0 0, 1 1),MULTIPOLYGON(((-50 -50, -50 -40, -40 -40, -40 -50, -50 -50)), ((50 50, 50 40, 40 40, 40 50, 50 50))))')",
+            resolution: 7,
+            center: 56,
+            intersects: 70,
+            contains: 'raise'
+        },
+        // extreme resolutions
+        {
+            geom: "ST_GEOGFROMTEXT('POLYGON((0 0, 0 .0001, .0001 .0001, .0001 0, 0 0))')",
+            resolution: 26,
+            center: 361,
+            intersects: 364,
+            contains: 289
+        },
+        {
+            geom: "ST_GEOGFROMTEXT('POLYGON((0 0, 0 50, 50 50, 50 0, 0 0))')",
+            resolution:0,
+            center: 1,
+            intersects: 0,
+            contains: 0
+        }
+    ]
+    for await (const mode of modes) {
+        for await (const input of inputs) {
+            const query = `
+                SELECT
+                ARRAY_SIZE(
+                    QUADBIN_POLYFILL_MODE(${input.geom}, ${input.resolution}, '${mode}')
+                ) AS id_count
+            `;
+            if (input[mode] == 'raise') {
+                await expect( runQuery(query) ).rejects.toThrow(Error);
+            } else {
+                const rows = await runQuery(query);
+                expect(rows.length).toEqual(1);
+                expect(rows.map((r) => r.ID_COUNT)).toEqual([input[mode]]);
+            }
+        }
+    }
+});
+
