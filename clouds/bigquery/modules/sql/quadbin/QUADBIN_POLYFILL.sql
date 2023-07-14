b9cb65670cfcde8e5812a40366f6641d75db3022
Valentin de la Cruz Barquero <6054336+vdelacruzb@users.noreply.github.com>
2023-07-14 11:27:06 +0200

    release: 2023-07-11 (#435)

--- clouds/bigquery/modules/sql/quadbin/QUADBIN_POLYFILL.sql
+++ clouds/bigquery/modules/sql/quadbin/QUADBIN_POLYFILL.sql
@@ -10,79 +10,104 @@ AS ((
         NULL,
         IF(resolution < 0 OR resolution > 26,
             ERROR('Invalid resolution, should be between 0 and 26'), (
-                WITH
-                __bbox AS (
-                    SELECT ST_BOUNDINGBOX(geog) AS box
+            WITH
+            __bbox AS (
+                SELECT ST_BOUNDINGBOX(geog) AS box
+            ),
+
+            __params AS (
+                SELECT
+                    resolution AS z,
+                    box.xmin AS minlon,
+                    box.ymin AS minlat,
+                    box.xmax AS maxlon,
+                    box.ymax AS maxlat,
+                    (1 << resolution) AS z2,
+                    ACOS(-1) AS pi
+                FROM __bbox
+            ),
+
+            __sinlat AS (
+                SELECT
+                    SIN(minlat * pi / 180.0) AS sinlat_min,
+                    SIN(maxlat * pi / 180.0) AS sinlat_max
+                FROM __params
+            ),
+
+            __xs AS (
+                -- precalculate Xs to allow simple use of
+                -- CASE in the next CTE
+                SELECT
+                    CAST(
+                            FLOOR(z2 * ((minlon / 360.0) + 0.5)) AS INT64
+                        ) & (z2 - 1)   -- bitwise way to calc MODULO
+                    AS xmin,
+                    CAST(
+                            FLOOR(z2 * ((maxlon / 360.0) + 0.5)) AS INT64
+                        ) & (z2 - 1)   -- bitwise way to calc MODULO
+                    AS xmax
+                FROM
+                    __params
+            ),
+
+            __tile_coords_range AS (
+                SELECT
+                    z,
+
+                    CASE
+                        WHEN xmin < 0 THEN xmin + z2
+                        ELSE xmin
+                    END
+                    AS xmin,
+
+                    CAST(
+                        -- floor before cast to avoid up rounding to the next tiLe
+                        FLOOR(
+                            z2 * (
+                                0.5 - 0.25 * LN(
+                                    (1 + sinlat_max) / (1 - sinlat_max)
+                                ) / pi
+                            )
+                        ) AS INT64
+                    )
+                    AS ymin,
+
+                    CASE
+                        WHEN xmax < 0 THEN xmax + z2
+                        ELSE xmax
+                    END
+                    AS xmax,
+
+                    CAST(
+                        -- floor before cast to avoid up rounding to the next tiLe
+                        FLOOR(
+                            z2 * (
+                                0.5 - 0.25 * LN(
+                                    (1 + sinlat_min) / (1 - sinlat_min)
+                                ) / pi
+                            )
+                        ) AS INT64
+                    )
+                    AS ymax
+
+                FROM __params, __xs, __sinlat
+            ),
+
+            __cells AS (
+                SELECT `@@BQ_DATASET@@.QUADBIN_FROMZXY`(z, x, y) AS quadbin
+                FROM __tile_coords_range,
+                    UNNEST(GENERATE_ARRAY(xmin, xmax)) AS x,
+                    UNNEST(GENERATE_ARRAY(ymin, ymax)) AS y
+            )
+
+            SELECT ARRAY_AGG(quadbin)
+            FROM __cells
+            WHERE ST_INTERSECTS(
+                `@@BQ_DATASET@@.QUADBIN_BOUNDARY`(
+                    quadbin
                 ),
-
-                __params AS (
-                    SELECT
-                        resolution AS z,
-                        box.xmin AS minlon,
-                        box.ymin AS minlat,
-                        box.xmax AS maxlon,
-                        box.ymax AS maxlat,
-                        ACOS(-1) AS pi
-                    FROM __bbox
-                ),
-
-                __tile_coords_range AS (
-                    SELECT
-                        z,
-                        CAST(
-                            FLOOR((1 << z) * ((minlon / 360.0) + 0.5)) AS INT64
-                        ) AS xmin,
-                        CAST(
-                            FLOOR(
-                                (
-                                    1 << z
-                                ) * (
-                                    0.5 - (
-                                        LN(
-                                            TAN(
-                                                pi / 4.0 + maxlat / 2.0 * pi / 180.0
-                                            )
-                                        ) / (2 * pi)
-                                    )
-                                )
-                            ) AS INT64
-                        ) AS ymin,
-                        CAST(
-                            FLOOR((1 << z) * ((maxlon / 360.0) + 0.5)) AS INT64
-                        ) AS xmax,
-                        CAST(
-                            FLOOR(
-                                (
-                                    1 << z
-                                ) * (
-                                    0.5 - (
-                                        LN(
-                                            TAN(
-                                                pi / 4.0 + minlat / 2.0 * pi / 180.0
-                                            )
-                                        ) / (2 * pi)
-                                    )
-                                )
-                            ) AS INT64
-                        ) AS ymax
-                    FROM __params
-                ),
-
-                __cells AS (
-                    SELECT `@@BQ_DATASET@@.QUADBIN_FROMZXY`(z, x, y) AS quadbin
-                    FROM __tile_coords_range,
-                        UNNEST(GENERATE_ARRAY(xmin, xmax)) AS x,
-                        UNNEST(GENERATE_ARRAY(ymin, ymax)) AS y
-                )
-
-                SELECT ARRAY_AGG(quadbin)
-                FROM __cells
-                WHERE ST_INTERSECTS(
-                    `@@BQ_DATASET@@.QUADBIN_BOUNDARY`(
-                        quadbin
-                    ),
-                    geog
-                )
-            ))
+                geog
+            )
+        ))
     )
 ));
--- clouds/bigquery/modules/test/quadbin/QUADBIN_FROMLONGLAT.test.js
+++ clouds/bigquery/modules/test/quadbin/QUADBIN_FROMLONGLAT.test.js
@@ -28,3 +28,57 @@ test('QUADBIN_FROMLONGLAT should throw an error for resolution overflow', async 
     const query = 'SELECT `@@BQ_DATASET@@.QUADBIN_FROMLONGLAT`(40.4168, -3.7038, 27) AS output';
     await expect(runQuery(query)).rejects.toThrow();
 });
+
+test('QUADBIN_FROMLONGLAT highest resolution', async () => {
+    let query = 'SELECT CAST(`@@BQ_DATASET@@.QUADBIN_FROMLONGLAT`(-3.71219873428345, 40.413365349070865, 26) AS STRING) AS output';
+    let rows = await runQuery(query);
+    expect(rows.length).toEqual(1);
+    expect(rows[0].output).toEqual('5306319089810035706');
+
+    query = 'SELECT CAST(`@@BQ_DATASET@@.QUADBIN_FROMLONGLAT`(40.413365349070865, -3.71219873428345, 26) AS STRING) AS output';
+    rows = await runQuery(query);
+    expect(rows.length).toEqual(1);
+    expect(rows[0].output).toEqual('5308641755410858449');
+
+    query = 'SELECT CAST(`@@BQ_DATASET@@.QUADBIN_FROMLONGLAT`(0.0, 3.552713678800501e-15, 26) AS STRING) AS output';
+    rows = await runQuery(query);
+    expect(rows.length).toEqual(1);
+    expect(rows[0].output).toEqual('5308618060762972160');
+
+    query = 'SELECT CAST(`@@BQ_DATASET@@.QUADBIN_FROMLONGLAT`(0.0, -3.552713678800501e-15, 26) AS STRING) AS output';
+    rows = await runQuery(query);
+    expect(rows.length).toEqual(1);
+    expect(rows[0].output).toEqual('5308618060762972160');
+
+    query = 'SELECT CAST(`@@BQ_DATASET@@.QUADBIN_FROMLONGLAT`(-89.71219873428345, -84.413365349070865, 26) AS STRING) AS output';
+    rows = await runQuery(query);
+    expect(rows.length).toEqual(1);
+    expect(rows[0].output).toEqual('5308521992464067502');
+
+    // set of call giving the same result with slightly different lat
+    query = 'SELECT CAST(`@@BQ_DATASET@@.QUADBIN_FROMLONGLAT`(0.0, 5.3644180297851546e-06, 26) AS STRING) AS output';
+    rows = await runQuery(query);
+    expect(rows.length).toEqual(1);
+    expect(rows[0].output).toEqual('5307116860887181994');
+    query = 'SELECT CAST(`@@BQ_DATASET@@.QUADBIN_FROMLONGLAT`(0.0, 5.364418029785155e-06, 26) AS STRING) AS output';
+    rows = await runQuery(query);
+    expect(rows.length).toEqual(1);
+    expect(rows[0].output).toEqual('5307116860887181994');
+    query = 'SELECT CAST(`@@BQ_DATASET@@.QUADBIN_FROMLONGLAT`(0.0, 5.364418029785156e-06, 26) AS STRING) AS output';
+    rows = await runQuery(query);
+    expect(rows.length).toEqual(1);
+    expect(rows[0].output).toEqual('5307116860887181994');
+    query = 'SELECT CAST(`@@BQ_DATASET@@.QUADBIN_FROMLONGLAT`(0.0, 5.364418029785157e-06, 26) AS STRING) AS output';
+    rows = await runQuery(query);
+    expect(rows.length).toEqual(1);
+    expect(rows[0].output).toEqual('5307116860887181994');
+    query = 'SELECT CAST(`@@BQ_DATASET@@.QUADBIN_FROMLONGLAT`(0.0, 5.364418029785158e-06, 26) AS STRING) AS output';
+    rows = await runQuery(query);
+    expect(rows.length).toEqual(1);
+    expect(rows[0].output).toEqual('5307116860887181994');
+    query = 'SELECT CAST(`@@BQ_DATASET@@.QUADBIN_FROMLONGLAT`(0.0, 5.364418029785156e-06, 26) AS STRING) AS output';
+    rows = await runQuery(query);
+    expect(rows.length).toEqual(1);
+    expect(rows[0].output).toEqual('5307116860887181994');
+    
+});
