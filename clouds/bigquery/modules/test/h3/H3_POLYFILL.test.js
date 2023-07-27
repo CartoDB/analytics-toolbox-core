5a40724aa8ec7e24ac9cc068b12cd086b28b1b69
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2023-07-27 18:23:11 +0200

    Update tests

--- clouds/bigquery/modules/test/h3/H3_POLYFILL.test.js
+++ clouds/bigquery/modules/test/h3/H3_POLYFILL.test.js
@@ -8,17 +8,10 @@ const polygon = 'POLYGON ((-3.71219873428345 40.413365349070865,-3.7144088745117
 const multiPolygon = 'MULTIPOLYGON (((-3.7102890014648438 40.412768896581476,-3.7081432342529297 40.41124946964811,-3.707242012023926 40.41370014129302,-3.7102890014648438 40.412768896581476)),((-3.71219873428345 40.413365349070865,-3.7144088745117 40.40965661286395,-3.70659828186035 40.409525904775634,-3.71219873428345 40.413365349070865),(-3.7122470136178776 40.41158984452673,-3.710165619422321 40.41109970196702,-3.711882233191852 40.41018475963737,-3.7122470136178776 40.41158984452673)))'
 
 test.each([
-    ['Point', 9, point, '["89390cb1b4bffff"]'],
-    ['MultiPoint', 9, multiPoint, '["89390cb1b4bffff"]'],
-    ['MultiPoint', 10, multiPoint, '["8a390cb1b4a7fff","8a390cb1b4b7fff"]'],
-    ['MultiPoint', 11, multiPoint, '["8b390cb1b486fff","8b390cb1b4b0fff","8b390cb1b4a6fff"]'],
-    ['Line', 8, line, '["88390ca349fffff","88390cb1b5fffff"]'],
-    ['Line', 9, line, '["89390ca3497ffff","89390cb1b4bffff"]'],
-    ['Line', 10, line, '["8a390ca3496ffff","8a390ca34947fff","8a390cb1b4affff","8a390ca3494ffff","8a390cb1b4b7fff","8a390cb1b487fff","8a390ca3495ffff","8a390cb1b497fff"]'],
-    ['MultiLine', 8, multiLine, '["88390ca349fffff","88390cb1b5fffff"]'],
-    ['MultiLine', 9, multiLine, '["89390ca3487ffff","89390ca3497ffff","89390cb1b4bffff"]'],
-    ['Polygon', 9, polygon, '["89390cb1b5bffff","89390ca34b3ffff","89390ca3487ffff","89390ca3497ffff","89390cb1b4bffff","89390cb1b4fffff"]'],
-    ['MultiPolygon', 9, multiPolygon, '["89390cb1b5bffff","89390ca34b3ffff","89390ca3487ffff","89390cb1b43ffff","89390ca3497ffff","89390cb1b4bffff","89390cb1b4fffff"]']
+    ['Point', 9, point, 'null'],
+    ['Line', 8, line, 'null'],
+    ['Polygon', 9, polygon, '["89390cb1b4bffff"]'],
+    ['MultiPolygon', 9, multiPolygon, '["89390cb1b4bffff"]']
 ])('H3_POLYFILL should work with %p at resolution %p', async (_, resolution, geom, output) => {
     const query = `SELECT TO_JSON_STRING(\`@@BQ_DATASET@@.H3_POLYFILL\`(
         ST_GEOGFROMTEXT('${geom}'), ${resolution})) AS output`;
@@ -28,20 +21,26 @@ test.each([
 });
 
 test.each([
-    ['Point', 'intersects', 9, point, '["89390cb1b4bffff"]'],
-    ['Point', 'contains', 9, point, 'null'],
     ['Point', 'center', 9, point, 'null'],
-    ['Line', 'intersects', 8, line, '["88390ca349fffff","88390cb1b5fffff"]'],
-    ['Line', 'contains', 8, line, 'null'],
+    ['Point', 'contains', 9, point, 'null'],
+    ['Point', 'intersects', 9, point, '["89390cb1b4bffff"]'],
+    ['MultiPoint', 'intersects', 9, multiPoint, '["89390cb1b4bffff"]'],
+    ['MultiPoint', 'intersects', 10, multiPoint, '["8a390cb1b4a7fff","8a390cb1b4b7fff"]'],
+    ['MultiPoint', 'intersects', 11, multiPoint, '["8b390cb1b486fff","8b390cb1b4b0fff","8b390cb1b4a6fff"]'],
     ['Line', 'center', 8, line, 'null'],
+    ['Line', 'contains', 8, line, 'null'],
+    ['Line', 'intersects', 8, line, '["88390ca349fffff","88390cb1b5fffff"]'],
+    ['Line', 'intersects', 9, line, '["89390ca3497ffff","89390cb1b4bffff"]'],
+    ['Line', 'intersects', 10, line, '["8a390ca3496ffff","8a390ca34947fff","8a390cb1b4affff","8a390ca3494ffff","8a390cb1b4b7fff","8a390cb1b487fff","8a390ca3495ffff","8a390cb1b497fff"]'],
+    ['MultiLine', 'intersects', 8, multiLine, '["88390ca349fffff","88390cb1b5fffff"]'],
+    ['MultiLine', 'intersects', 9, multiLine, '["89390ca3487ffff","89390ca3497ffff","89390cb1b4bffff"]'],
     ['Polygon', 'wrong-mode', 4, polygon, 'null'],
-    ['Polygon', 'intersects', 9, polygon, '["89390cb1b5bffff","89390ca34b3ffff","89390ca3487ffff","89390ca3497ffff","89390cb1b4bffff","89390cb1b4fffff"]'],
-    ['Polygon', 'contains', 10, polygon, '["8a390cb1b4b7fff","8a390cb1b487fff"]'],
     ['Polygon', 'center', 9, polygon, '["89390cb1b4bffff"]'],
-    ['MultiPolygon', 'intersects', 9, multiPolygon, '["89390cb1b5bffff","89390ca34b3ffff","89390ca3487ffff","89390cb1b43ffff","89390ca3497ffff","89390cb1b4bffff","89390cb1b4fffff"]'],
+    ['Polygon', 'contains', 10, polygon, '["8a390cb1b4b7fff","8a390cb1b487fff"]'],
+    ['Polygon', 'intersects', 9, polygon, '["89390cb1b5bffff","89390ca34b3ffff","89390ca3487ffff","89390ca3497ffff","89390cb1b4bffff","89390cb1b4fffff"]'],
+    ['MultiPolygon', 'center', 9, multiPolygon, '["89390cb1b4bffff"]'],
     ['MultiPolygon', 'contains', 10, multiPolygon, 'null'],
-    ['MultiPolygon', 'center', 9, multiPolygon, '["89390cb1b4bffff"]']
-
+    ['MultiPolygon', 'intersects', 9, multiPolygon, '["89390cb1b5bffff","89390ca34b3ffff","89390ca3487ffff","89390cb1b43ffff","89390ca3497ffff","89390cb1b4bffff","89390cb1b4fffff"]']
 ])('H3_POLYFILL_MODE should work with %p mode %p at resolution %p', async (_, mode, resolution, geom, output) => {
     const query = `SELECT TO_JSON_STRING(\`@@BQ_DATASET@@.H3_POLYFILL_MODE\`(
         ST_GEOGFROMTEXT('${geom}'), ${resolution}, '${mode}')) AS output`;
