4cc352b9d667b18f696cca9eb89ec07dfa82899e
vdelacruzb <vdelacruzb@gmail.com>
2023-12-05 10:25:14 +0100

    update Makefiles

--- clouds/snowflake/common/native-app-setup.js
+++ clouds/snowflake/common/native-app-setup.js
@@ -42,7 +42,7 @@ function runSetRelease (err, stmt, rows) {
 function runDropPreviousVersionQuery (err, stmt, rows) {
     // Get the patches different of a given version
     patchesArr = rows.filter(obj => obj['version'] !== process.env.VERSION.toUpperCase())
-    if (patchesArr.length >0) {
+    if (patchesArr.length > 0) {
         // As there can only be two versions we take any patch
         const previousVersion = patchesArr[0]['version']
         const query = `
@@ -59,6 +59,20 @@ function runDropPreviousVersionQuery (err, stmt, rows) {
     }
 }
 
+function runCheckVersionExistenceQuery (err, stmt, rows) {
+    // Get the different patches for a given version
+    patchesArr = rows.filter(obj => obj['version'] === process.env.VERSION.toUpperCase())
+    if (patchesArr.length > 0) {
+        console.log('1')
+    } else {
+        console.log('0')
+    }
+}
+
+function runCountVersionsQuery (err, stmt, rows) {
+    uniqueVersions = new Set(rows.map(obj => obj['version']))
+    console.log(uniqueVersions.size)
+}
 
 function runGetVersionsQuery (callback) {
     const query = `SHOW VERSIONS IN APPLICATION PACKAGE ${process.env.APP_PACKAGE_NAME};`
@@ -68,12 +82,30 @@ function runGetVersionsQuery (callback) {
     });
 }
 
-if (process.env.SET_PACKAGE_RELEASE) {
-    runGetVersionsQuery(runSetRelease);
-}
-else {
-    if (process.env.DROP_PREVIOUS_VERSION) {
-        runGetVersionsQuery (runDropPreviousVersionQuery)
+function runCheckAppPackageExistenceQuery (err, stmt, rows) {
+    packagesArr = rows.filter(obj => obj['name'] === process.env.APP_PACKAGE_NAME.toUpperCase())
+    if (packagesArr.length > 0) {
+        console.log('1')
+    } else {
+        console.log('0')
     }
 }
-    
+
+function runGetAppPackagesQuery (callback) {
+    const query = 'SHOW APPLICATION PACKAGES;'
+    connection.execute({
+        sqlText: query,
+        complete: callback
+    });
+}
+if (process.env.SET_PACKAGE_RELEASE) {
+    runGetVersionsQuery(runSetRelease)
+} else if (process.env.DROP_PREVIOUS_VERSION) {
+    runGetVersionsQuery (runDropPreviousVersionQuery)
+} else if (process.env.CHECK_VERSION_EXISTENCE) {
+    runGetVersionsQuery (runCheckVersionExistenceQuery)
+} else if (process.env.COUNT_VERSIONS) {
+    runGetVersionsQuery (runCountVersionsQuery)
+} else if (process.env.CHECK_APP_PACKAGE_EXISTENCE) {
+    runGetAppPackagesQuery(runCheckAppPackageExistenceQuery)
+}
