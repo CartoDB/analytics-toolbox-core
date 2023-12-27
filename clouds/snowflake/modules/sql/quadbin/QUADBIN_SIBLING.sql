adbea11b5afed6091e4a15495e9d0801bd8868ca
vdelacruzb <vdelacruzb@gmail.com>
2023-12-27 13:49:38 +0100

    return null when any paremeter is null

--- clouds/snowflake/modules/sql/quadbin/QUADBIN_SIBLING.sql
+++ clouds/snowflake/modules/sql/quadbin/QUADBIN_SIBLING.sql
@@ -9,16 +9,19 @@ CREATE OR REPLACE SECURE FUNCTION @@SF_SCHEMA@@.QUADBIN_SIBLING
 RETURNS INT
 IMMUTABLE
 AS $$
-    CASE direction
-        WHEN 'left' THEN
-            @@SF_SCHEMA@@._QUADBIN_SIBLING(quadbin, -1, 0)
-        WHEN 'right' THEN
-            @@SF_SCHEMA@@._QUADBIN_SIBLING(quadbin, 1, 0)
-        WHEN 'up' THEN
-            @@SF_SCHEMA@@._QUADBIN_SIBLING(quadbin, 0, -1)
-        WHEN 'down' THEN
-            @@SF_SCHEMA@@._QUADBIN_SIBLING(quadbin, 0, 1)
-        ELSE
-            NULL
-    END
+    IFF(quadbin IS NULL OR direction IS NULL,
+        NULL,
+        CASE direction
+            WHEN 'left' THEN
+                @@SF_SCHEMA@@._QUADBIN_SIBLING(quadbin, -1, 0)
+            WHEN 'right' THEN
+                @@SF_SCHEMA@@._QUADBIN_SIBLING(quadbin, 1, 0)
+            WHEN 'up' THEN
+                @@SF_SCHEMA@@._QUADBIN_SIBLING(quadbin, 0, -1)
+            WHEN 'down' THEN
+                @@SF_SCHEMA@@._QUADBIN_SIBLING(quadbin, 0, 1)
+            ELSE
+                NULL
+        END
+    )
 $$;
