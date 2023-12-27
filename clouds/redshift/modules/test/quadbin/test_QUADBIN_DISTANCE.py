3017ba9fe9133c4ae045edfd73cac541f23ab892
vdelacruzb <vdelacruzb@gmail.com>
2023-12-27 17:39:47 +0100

    add QUADBIN_DISTANCE to RS

---
+++ clouds/redshift/modules/test/quadbin/test_QUADBIN_DISTANCE.py
@@ -0,0 +1,8 @@
+from test_utils import run_query
+
+
+def test_quadbin_distance():
+    result = run_query('SELECT @@RS_SCHEMA@@.QUADBIN_DISTANCE(5207251884775047167, 5207128739472736255)')
+
+    assert len(result[0]) == 1
+    assert result[0][0] == 1
