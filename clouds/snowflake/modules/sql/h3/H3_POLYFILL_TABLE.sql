7bd069eb1c7b3fc782f3ec80e4ecf1ced175ff08
Dean Sherwin <deansherwin@gmail.com>
2024-04-04 20:06:49 +0200

    modes working

--- clouds/snowflake/modules/sql/h3/H3_POLYFILL_TABLE.sql
+++ clouds/snowflake/modules/sql/h3/H3_POLYFILL_TABLE.sql
@@ -2,7 +2,6 @@
 -- Copyright (C) 2023-2024 CARTO
 --------------------------------
 
--- TODO: mode
 -- TODO: CLUSTER
 -- TODO: disallow query column names which conflict with FLATTEN columns
 -- TODO: SCHEMA
@@ -21,16 +20,20 @@ EXECUTE AS CALLER
 AS $$
 DECLARE
     column_names_csv STRING;
+    polyfill_query STRING;
 BEGIN
+
     -- Validate
-    EXECUTE IMMEDIATE 'SELECT * FROM (' || :input_query || ') WHERE FALSE';
+    EXECUTE IMMEDIATE 'SELECT * FROM (' || input_query || ') WHERE FALSE';
 
     -- New table with correct columns
-    EXECUTE IMMEDIATE 'CREATE TABLE ' || :output_table || ' AS SELECT * EXCLUDE geom, NULL as H3 FROM (' || :input_query || ') WHERE FALSE';
+    EXECUTE IMMEDIATE 'CREATE TABLE ' || output_table || ' AS SELECT * EXCLUDE geom, NULL as H3 FROM (' || input_query || ') WHERE FALSE';
 
     column_names_csv := (SELECT LISTAGG(COLUMN_NAME, ',') FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME ILIKE :output_table AND TABLE_SCHEMA=CURRENT_SCHEMA());
 
-    EXECUTE IMMEDIATE 'INSERT INTO ' || :output_table || ' (' || :column_names_csv || ') SELECT ' || :column_names_csv || ' FROM (WITH virtual_table AS (' || :input_query || ') SELECT *, value as H3 FROM virtual_table, LATERAL FLATTEN(input => H3_POLYFILL(virtual_table.geom, ' || :resolution || ')))';
+    polyfill_query := 'INSERT INTO ' || output_table || ' (' || column_names_csv || ') SELECT ' || column_names_csv || ' FROM (WITH virtual_table AS (' || input_query || ') SELECT *, value as H3 FROM virtual_table, LATERAL FLATTEN(input => H3_POLYFILL(virtual_table.geom, ' || resolution || ',\'' || mode || '\')))';
+
+    EXECUTE IMMEDIATE polyfill_query;
 
     RETURN 'Finished!';
 END;
