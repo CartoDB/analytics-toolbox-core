0e7f536db323b9aa46c5fd42125a7f36609b34af
Dean Sherwin <deansherwin@gmail.com>
2024-04-04 21:35:18 +0200

    H3_POLYFILL_TABLE can target different databases and schemas

--- clouds/snowflake/modules/sql/h3/H3_POLYFILL_TABLE.sql
+++ clouds/snowflake/modules/sql/h3/H3_POLYFILL_TABLE.sql
@@ -2,10 +2,45 @@
 -- Copyright (C) 2023-2024 CARTO
 --------------------------------
 
--- TODO: CLUSTER
--- TODO: disallow query column names which conflict with FLATTEN columns
--- TODO: SCHEMA
--- TODO: TESTS
+CREATE OR REPLACE FUNCTION @@SF_SCHEMA@@._GET_SCHEMA
+(table_identifier STRING)
+RETURNS STRING
+LANGUAGE SQL
+IMMUTABLE
+AS $$
+	WITH split_identifier AS (
+		SELECT ARRAY_SIZE(SPLIT(table_identifier, '.')) AS parts_count,
+		SPLIT_PART(table_identifier, '.', 1) AS first_part,
+		SPLIT_PART(table_identifier, '.', 2) AS second_part
+		FROM (SELECT TABLE_IDENTIFIER AS table_identifier)
+	)
+	SELECT
+		CASE parts_count
+			WHEN 3 THEN second_part
+			WHEN 2 THEN first_part
+			ELSE CURRENT_SCHEMA()
+		END
+	FROM split_identifier
+$$;
+
+CREATE OR REPLACE FUNCTION @@SF_SCHEMA@@._GET_DATABASE
+(table_identifier STRING)
+RETURNS STRING
+LANGUAGE SQL
+IMMUTABLE
+AS $$
+	WITH split_identifier AS (
+		SELECT ARRAY_SIZE(SPLIT(table_identifier, '.')) AS parts_count,
+		SPLIT_PART(table_identifier, '.', 1) AS first_part
+		FROM (SELECT TABLE_IDENTIFIER AS table_identifier)
+	)
+	SELECT
+		CASE parts_count
+			WHEN 3 THEN first_part
+			ELSE CURRENT_DATABASE()
+		END
+	FROM split_identifier
+$$;
 
 CREATE OR REPLACE PROCEDURE @@SF_SCHEMA@@.H3_POLYFILL_TABLE
 (
@@ -27,9 +62,9 @@ BEGIN
     EXECUTE IMMEDIATE 'SELECT * FROM (' || input_query || ') WHERE FALSE';
 
     -- New table with correct columns
-    EXECUTE IMMEDIATE 'CREATE TABLE ' || output_table || ' AS SELECT * EXCLUDE geom, NULL as H3 FROM (' || input_query || ') WHERE FALSE';
+    EXECUTE IMMEDIATE 'CREATE OR REPLACE TABLE ' || output_table || ' CLUSTER BY (H3) AS SELECT * EXCLUDE geom, NULL as H3 FROM (' || input_query || ') WHERE FALSE';
 
-    column_names_csv := (SELECT LISTAGG(COLUMN_NAME, ',') FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME ILIKE :output_table AND TABLE_SCHEMA=CURRENT_SCHEMA());
+    column_names_csv := (SELECT LISTAGG(COLUMN_NAME, ',') FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME ILIKE :output_table AND TABLE_CATALOG=@@SF_SCHEMA@@._GET_DATABASE(:output_table) AND TABLE_SCHEMA=@@SF_SCHEMA@@._GET_SCHEMA(:output_table));
 
     polyfill_query := 'INSERT INTO ' || output_table || ' (' || column_names_csv || ') SELECT ' || column_names_csv || ' FROM (WITH virtual_table AS (' || input_query || ') SELECT *, value as H3 FROM virtual_table, LATERAL FLATTEN(input => H3_POLYFILL(virtual_table.geom, ' || resolution || ',\'' || mode || '\')))';
 
