5d0b66d3cbbbfe0f2e486c6421b01c4a1831e56e
volaya <volayaf@gmail.com>
2024-04-30 12:09:34 +0200

    fixed test

--- clouds/snowflake/modules/test/h3/H3_POLYFILL_TABLE.test.js
+++ clouds/snowflake/modules/test/h3/H3_POLYFILL_TABLE.test.js
@@ -1,9 +1,9 @@
 const { runQuery, deleteTable } = require('../../../common/test-utils');
 
 test('H3_POLYFILL_TABLE should work', async () => {
-    await deleteTable('polyfill_test_output_table');
+    await deleteTable('@@SF_SCHEMA@@.polyfill_test_output_table');
 
-    let query = `CALL @@SF_DATASET@@.H3_POLYFILL_TABLE(
+    let query = `CALL @@SF_SCHEMA@@.H3_POLYFILL_TABLE(
                     'SELECT TO_GEOGRAPHY(
                         ''POLYGON ((-3.71219873428345 40.413365349070865,
                                     -3.7144088745117 40.40965661286395,
@@ -12,14 +12,14 @@ test('H3_POLYFILL_TABLE should work', async () => {
                                 ))''
                         ) AS geom',
                     9, 'intersects',
-                    '@@SF_DATASET@@.polyfill_test_output_table'
+                    '@@SF_SCHEMA@@.polyfill_test_output_table'
                   );
                 );`;
     await runQuery(query);
 
-    query = 'SELECT * FROM @@SF_DATASET@@.polyfill_test_output_table`;';
+    query = 'SELECT * FROM @@SF_SCHEMA@@.polyfill_test_output_table`;';
     const rows = await runQuery(query);
     expect(rows.length).toEqual(6);
 
-    await deleteTable('polyfill_test_output_table');
+    await deleteTable('@@SF_SCHEMA@@.polyfill_test_output_table');
 });
