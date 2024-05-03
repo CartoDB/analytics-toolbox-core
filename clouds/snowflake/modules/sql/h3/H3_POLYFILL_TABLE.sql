171bbc202e39382aca2e3d2a97914793b1fdd70a
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2024-05-03 16:22:20 +0200

    refactor: H3_POLYFILL_TABLE procedure in Snowflake

--- clouds/snowflake/modules/sql/h3/H3_POLYFILL_TABLE.sql
+++ clouds/snowflake/modules/sql/h3/H3_POLYFILL_TABLE.sql
@@ -13,23 +13,14 @@ RETURNS STRING
 LANGUAGE SQL
 EXECUTE AS CALLER
 AS $$
-DECLARE
-    column_names_csv STRING;
-    polyfill_query STRING;
 BEGIN
+    EXECUTE IMMEDIATE '
+        CREATE OR REPLACE TABLE ' || output_table || ' CLUSTER BY (H3) AS
+        WITH __input AS ( ' || input_query || ' )
+        SELECT CAST(cell.value AS STRING) AS h3, i.* EXCLUDE(geom)
+        FROM __input AS i, TABLE(FLATTEN(@@SF_SCHEMA@@.H3_POLYFILL(geom, ' || resolution || ', ''' || mode || '''))) AS cell;
+    ';
 
-    -- Validate
-    EXECUTE IMMEDIATE 'SELECT * FROM (' || input_query || ') WHERE FALSE';
-
-    -- New table with correct columns
-    EXECUTE IMMEDIATE 'CREATE OR REPLACE TABLE ' || output_table || ' CLUSTER BY (H3) AS SELECT * EXCLUDE geom, NULL as H3 FROM (' || input_query || ') WHERE FALSE';
-
-    column_names_csv := (SELECT LISTAGG(COLUMN_NAME, ',') FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME ILIKE :output_table AND TABLE_CATALOG=@@SF_SCHEMA@@._GET_DATABASE(:output_table) AND TABLE_SCHEMA=@@SF_SCHEMA@@._GET_SCHEMA(:output_table));
-
-    polyfill_query := 'INSERT INTO ' || output_table || ' (' || column_names_csv || ') SELECT ' || column_names_csv || ' FROM (WITH virtual_table AS (' || input_query || ') SELECT *, value as H3 FROM virtual_table, LATERAL FLATTEN(input => @@SF_SCHEMA@@.H3_POLYFILL(virtual_table.geom, ' || resolution || ',\'' || mode || '\')))';
-
-    EXECUTE IMMEDIATE polyfill_query;
-
-    RETURN 'Finished!';
+    RETURN 'H3 Polyfill result added in table ' || output_table;
 END;
 $$;
