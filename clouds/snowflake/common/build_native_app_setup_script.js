c9d4ef9e0464ae4e819bb61f51e1f8519e9ba1c9
Valentin de la Cruz Barquero <6054336+vdelacruzb@users.noreply.github.com>
2024-05-31 10:14:20 +0200

    feat(sf): refactor at snowflake native app to an installer (#512)

--- clouds/snowflake/common/build_native_app_setup_script.js
+++ clouds/snowflake/common/build_native_app_setup_script.js
@@ -1,143 +1,16 @@
 #!/usr/bin/env node
 
-// Build the setup_script for the native app file based on the input filters
-// and ordered to solve the dependencies
+// Build the setup_script for the native app installer setup file
 
-// ./build_native_app_setup_script.js modules --output=build --diff="clouds/snowflake/modules/sql/quadbin/QUADBIN_TOZXY.sql"
-// ./build_native_app_setup_script.js modules --output=build --functions=ST_TILEENVELOPE
-// ./build_native_app_setup_script.js modules --output=build --modules=quadbin
-// ./build_native_app_setup_script.js modules --output=build --production --dropfirst
+// ./build_native_app_setup_script.js modules --output=build --libs_build_dir=../libraries/javascript/build native_app_dir=../native_app --production --dropfirst
 
 const fs = require('fs');
 const path = require('path');
 const argv = require('minimist')(process.argv.slice(2));
 
-const inputDirs = argv._[0] && argv._[0].split(',');
 const outputDir = argv.output || 'build';
 const libsBuildDir = argv.libs_build_dir || '../libraries/javascript/build';
 const nativeAppDir = argv.native_app_dir || '../native_app';
-const diff = argv.diff || [];
-const nodeps = argv.nodeps;
-let modulesFilter = (argv.modules && argv.modules.split(',')) || [];
-let functionsFilter = (argv.functions && argv.functions.split(',')) || [];
-let all = !(diff.length || modulesFilter.length || functionsFilter.length);
-
-if (all) {
-    console.log('- Build all');
-} else if (diff && diff.length) {
-    console.log(`- Build input diff: ${argv.diff}`);
-} else if (modulesFilter && modulesFilter.length) {
-    console.log(`- Build input modules: ${argv.modules}`);
-} else if (functionsFilter && functionsFilter.length) {
-    console.log(`- Build input functions: ${argv.functions}`);
-}
-
-// Convert diff to modules
-if (diff.length) {
-    const patternsAll = [
-        /\.github\/workflows\/snowflake\.yml/,
-        /clouds\/snowflake\/common\/.+/,
-        /clouds\/snowflake\/libraries\/.+/,
-        /clouds\/snowflake\/.*Makefile/,
-        /clouds\/snowflake\/version/
-    ];
-    const patternModulesSql = /clouds\/snowflake\/modules\/sql\/([^\s]*?)\//g;
-    const patternModulesTest = /clouds\/snowflake\/modules\/test\/([^\s]*?)\//g;
-    const diffAll = patternsAll.some(p => diff.match(p));
-    if (diffAll) {
-        console.log('-- all');
-        all = diffAll;
-    } else {
-        const modulesSql = [...diff.matchAll(patternModulesSql)].map(m => m[1]);
-        const modulesTest = [...diff.matchAll(patternModulesTest)].map(m => m[1]);
-        const diffModulesFilter = [...new Set(modulesSql.concat(modulesTest))];
-        if (diffModulesFilter) {
-            console.log(`-- modules: ${diffModulesFilter}`);
-            modulesFilter = diffModulesFilter;
-        }
-    }
-}
-
-// Extract functions
-const functions = [];
-for (let inputDir of inputDirs) {
-    const sqldir = path.join(inputDir, 'sql');
-    const modules = fs.readdirSync(sqldir);
-    modules.forEach(module => {
-        const moduledir = path.join(sqldir, module);
-        if (fs.statSync(moduledir).isDirectory()) {
-            const files = fs.readdirSync(moduledir);
-            files.forEach(file => {
-                if (file.endsWith('.sql')) {
-                    const name = path.parse(file).name;
-                    const content = fs.readFileSync(path.join(moduledir, file)).toString().replace(/--.*\n/g, '');
-                    functions.push({
-                        name,
-                        module,
-                        content,
-                        dependencies: []
-                    });
-                }
-            });
-        }
-    });
-}
-
-// Check filters
-modulesFilter.forEach(m => {
-    if (!functions.map(fn => fn.module).includes(m)) {
-        console.log(`ERROR: Module not found ${m}`);
-        process.exit(1);
-    }
-});
-functionsFilter.forEach(f => {
-    if (!functions.map(fn => fn.name).includes(f)) {
-        console.log(`ERROR: Function not found ${f}`);
-        process.exit(1);
-    }
-});
-
-// Extract function dependencies
-if (!nodeps) {
-    functions.forEach(mainFunction => {
-        functions.forEach(depFunction => {
-            if (mainFunction.name != depFunction.name) {
-                if (mainFunction.content.includes(`SCHEMA@@.${depFunction.name}(`)) {
-                    mainFunction.dependencies.push(depFunction.name);
-                }
-            }
-        });
-    });
-}
-
-// Check circular dependencies
-if (!nodeps) {
-    functions.forEach(mainFunction => {
-        functions.forEach(depFunction => {
-            if (mainFunction.dependencies.includes(depFunction.name) &&
-                depFunction.dependencies.includes(mainFunction.name)) {
-                console.log(`ERROR: Circular dependency between ${mainFunction.name} and ${depFunction.name}`);
-                process.exit(1);
-            }
-        });
-    });
-}
-
-// Filter and order functions
-const output = [];
-function add (f, include) {
-    include = include || all || functionsFilter.includes(f.name) || modulesFilter.includes(f.module);
-    for (const dependency of f.dependencies) {
-        add(functions.find(f => f.name === dependency), include);
-    }
-    if (!output.map(f => f.name).includes(f.name) && include) {
-        output.push({
-            name: f.name,
-            content: f.content
-        });
-    }
-}
-functions.forEach(f => add(f));
 
 // Replace environment variables
 let separator;
@@ -146,7 +19,7 @@ if (argv.production) {
 } else {
     separator = '\n-->\n';  // marker to future SQL split
 }
-let content = output.map(f => fetchPermissionsGrant(f.content)).join(separator);
+let content = fs.readFileSync(path.resolve(nativeAppDir, 'SETUP_SCRIPT.sql')).toString();
 
 function apply_replacements (text) {
     const libraries = [... new Set(text.match(new RegExp('@@SF_LIBRARY_.*@@', 'g')))];
@@ -172,79 +45,25 @@ function apply_replacements (text) {
     return text;
 }
 
-function getFunctionSignatures (functionMatches)
-{
-    const functSignatures = []
-    for (const functionMatch of functionMatches) {
-        //Remove spaces and diacritics
-        let qualifiedFunctName = functionMatch[0].split('(')[0].replace(/\s+/gm,'');
-        qualifiedFunctNameArr = qualifiedFunctName.split('.');
-        const functName = qualifiedFunctNameArr[qualifiedFunctNameArr.length - 1];
-        if (functName.startsWith('_'))
-        {
-            continue;
-        }
-        //Remove diacritics and go greedy to take the outer parentheses
-        let functArgs = functionMatch[0].matchAll(new RegExp('(?<=\\()(.*)(?=\\))','g')).next().value;
-        if (functArgs)
-        {
-            functArgs = functArgs[0];
-        }
-        else
-        {
-            continue;
-        }
-        functArgs = functArgs.split(',')
-        let functArgsTypes = [];
-        for (const functArg of functArgs) {
-            const functArgSplitted = functArg.trim(' ').split(' ');
-            functArgsTypes.push(functArgSplitted[functArgSplitted.length - 1]);
-        }
-        const functSignature = qualifiedFunctName + '(' + functArgsTypes.join(',') + ')';
-        functSignatures.push(functSignature)
-    }
-    return functSignatures
-}
-
-function fetchPermissionsGrant (content)
-{
-    let fileContent = content.split('\n');
-    for (let i = 0 ; i < fileContent.length; i++)
-    {
-        if (fileContent[i].startsWith('--'))
-        {
-            delete fileContent[i];
-        }
-    }
-    fileContent = fileContent.join(' ').replace(/[\p{Diacritic}]/gu, '').replace(/\s+/gm,' ');
-    const functionMatches = fileContent.matchAll(new RegExp(/(?<=(?<!TEMP )FUNCTION)(.*?)(?=RETURNS)/gs));
-    const functSignatures = getFunctionSignatures(functionMatches).map(f => `GRANT USAGE ON FUNCTION ${f} TO APPLICATION ROLE @@APP_ROLE@@;`).join('\n')
-    const procMatches = fileContent.matchAll(new RegExp(/(?<=PROCEDURE)(.*?)(?=AS)/gs));
-    const procSignatures = getFunctionSignatures(procMatches).map(f => `GRANT USAGE ON PROCEDURE ${f} TO APPLICATION ROLE @@APP_ROLE@@;`).join('\n')
-    return content + functSignatures +procSignatures
-}
-
 if (argv.dropfirst) {
-    const header = fs.readFileSync(path.resolve(__dirname, 'DROP_FUNCTIONS.sql')).toString();
+    let header = fs.readFileSync(path.resolve(__dirname, 'DROP_FUNCTIONS.sql')).toString();
+    const pattern = new RegExp('@@SF_SCHEMA@@', 'g');
+    header = header.replace(pattern, '@@SF_APP_SCHEMA@@');
     content = header + separator + content
 }
 
 const header = `CREATE OR REPLACE APPLICATION ROLE @@APP_ROLE@@;
-CREATE OR ALTER VERSIONED SCHEMA @@SF_SCHEMA@@;
-GRANT USAGE ON SCHEMA @@SF_SCHEMA@@ TO APPLICATION ROLE @@APP_ROLE@@;\n`;
+CREATE OR ALTER VERSIONED SCHEMA @@SF_APP_SCHEMA@@;
+GRANT USAGE ON SCHEMA @@SF_APP_SCHEMA@@ TO APPLICATION ROLE @@APP_ROLE@@;\n`;
 
 let additionalTables = '';
 if (argv.production) {
     additionalTables = fs.readFileSync(path.resolve(nativeAppDir, 'ADDITIONAL_TABLES.sql')).toString() + separator;
 }
 
-const footer = fetchPermissionsGrant (fs.readFileSync(path.resolve(__dirname, 'VERSION.sql')).toString());
-content = header + separator + additionalTables + content + separator + footer;
+content = header + separator + additionalTables + content;
 
 content = apply_replacements(content);
-
-// Execute as caller replacement
-content = content.replace(/EXECUTE\s+AS\s+CALLER/g, 'EXECUTE AS OWNER');
 
 // Write setup_script.sql file
 fs.writeFileSync(path.join(outputDir, 'setup_script.sql'), content);
---
+++ clouds/snowflake/native_app/SETUP_SCRIPT.sql
@@ -0,0 +1,55 @@
+CREATE OR REPLACE PROCEDURE @@SF_APP_SCHEMA@@.GET_MODULES_SQL_FROM_STAGE()
+    returns string
+    language python
+    runtime_version = '3.8'
+    packages = ('snowflake-snowpark-python')
+    imports = (
+        '/get_modules_sql_from_stage.py',
+        '/modules.sql'
+    )
+    handler = 'get_modules_sql_from_stage.main'
+    ;
+
+GRANT USAGE ON PROCEDURE @@SF_APP_SCHEMA@@.GET_MODULES_SQL_FROM_STAGE() TO APPLICATION ROLE @@APP_ROLE@@;
+
+CREATE OR REPLACE PROCEDURE @@SF_APP_SCHEMA@@.GENERATE_INSTALLER(DESTINATION_SCHEMA STRING)
+RETURNS VARCHAR
+LANGUAGE JAVASCRIPT
+EXECUTE AS OWNER
+AS $$
+  const QUOTE = '$' + '$'
+  const functions = [`
+    CREATE OR REPLACE PROCEDURE ${DESTINATION_SCHEMA}.INSTALL(SOURCE_APP STRING, DESTINATION_SCHEMA STRING)
+    RETURNS VARCHAR
+    LANGUAGE JAVASCRIPT
+    EXECUTE AS OWNER
+    AS ${QUOTE}
+        const QUOTE = '$' + '$';
+        const rs = snowflake.execute({ sqlText: 'CALL ' + SOURCE_APP + '.@@SF_APP_SCHEMA@@.GET_MODULES_SQL_FROM_STAGE()'});
+        rs.next();
+        let modulesSql = rs.getColumnValue(1);
+        let resultMessage = 'Procedures installed';
+        
+        const replacements = [
+            ['@@SF_SCHEMA@@', DESTINATION_SCHEMA]
+        ]
+
+        for (const [variable, value] of replacements) {
+          modulesSql = modulesSql.replaceAll(variable, value);
+        }
+
+        snowflake.execute({ sqlText: modulesSql });
+        snowflake.execute({ sqlText: 'DROP PROCEDURE IF EXISTS ${DESTINATION_SCHEMA}.INSTALL(STRING, STRING)' });
+        return resultMessage
+
+    ${QUOTE};
+    `]
+  let resultMessage = 'Procedure installed';
+
+  for (let modulesSql of functions) {
+    snowflake.execute({ sqlText: modulesSql });
+  }
+  return resultMessage
+$$;
+
+GRANT USAGE ON PROCEDURE @@SF_APP_SCHEMA@@.GENERATE_INSTALLER(STRING) TO APPLICATION ROLE @@APP_ROLE@@;
