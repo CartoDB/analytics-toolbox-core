e9033a2ed66ed684f82bfc4feeb22051ac73efd4
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2024-07-02 13:08:00 +0200

    restore dependencies

--- clouds/snowflake/common/rollup.config.js
+++ clouds/snowflake/common/rollup.config.js
@@ -3,7 +3,7 @@ import path from 'path';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import json from '@rollup/plugin-json';
-import terser from '@rollup/plugin-terser';
+import { terser } from 'rollup-plugin-terser';
 import bundleSize from 'rollup-plugin-bundle-size';
 
 // Find final input path from dirs array
