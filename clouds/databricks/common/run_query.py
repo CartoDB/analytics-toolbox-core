ae8a0b0dbb471ea285d930a791b7476f21d7b2d4
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2025-09-15 15:07:31 +0200

    update databricks common scripts

--- clouds/databricks/common/run_query.py
+++ clouds/databricks/common/run_query.py
@@ -3,8 +3,6 @@ from databricks import sql
 
 
 def run_query(query):
-    query = query.replace('@@DB_SCHEMA@@', os.getenv('DB_SCHEMA'))
-    query = query.replace('\\', '')
     with sql.connect(
         server_hostname=os.getenv('DB_HOST_NAME'),
         http_path=os.getenv('DB_HTTP_PATH'),
--- clouds/databricks/modules/test/product/test_VERSION_CORE.py
+++ clouds/databricks/modules/test/product/test_VERSION_CORE.py
@@ -4,4 +4,4 @@ from test_utils import run_query
 def test_version_core_success():
     query = 'SELECT @@DB_SCHEMA@@.VERSION_CORE();'
     result = run_query(query)
-    assert result[0][0] == '0.1.0'
+    assert result[0][0] == '0.1.0-dev'
