9e25b60de80375e97379f164bd696b5b7f3b27fb
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2025-12-23 10:44:32 +0100

    fix(db): strip quotes from diff paths in list_functions.js

--- clouds/databricks/common/list_functions.js
+++ clouds/databricks/common/list_functions.js
@@ -11,7 +11,8 @@ const path = require('path');
 const argv = require('minimist')(process.argv.slice(2));
 
 const inputDir = '.';
-const diff = argv.diff || [];
+// Strip quotes from diff paths (get-diff-action@v4 adds quotes around paths)
+const diff = (argv.diff || '').replace(/'/g, '');
 let modulesFilter = (argv.modules && argv.modules.split(',')) || [];
 let functionsFilter = (argv.functions && argv.functions.split(',')) || [];
 let all = !(diff.length || modulesFilter.length || functionsFilter.length);
