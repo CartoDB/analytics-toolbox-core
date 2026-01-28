ec5e5cd2d37cfed2416937979abbfc05dbb35f3b
vdelacruzb <vdelacruzb@gmail.com>
2026-01-28 17:19:39 +0100

    add isGeometryCloseTo

--- clouds/snowflake/modules/test/transformations/ST_GREATCIRCLE.test.js
+++ clouds/snowflake/modules/test/transformations/ST_GREATCIRCLE.test.js
@@ -1,14 +1,41 @@
-const { runQuery } = require('../../../common/test-utils');
+const { runQuery, isGeometryCloseTo } = require('../../../common/test-utils');
 
 test('ST_GREATCIRCLE should work', async () => {
-    const query = `SELECT ST_GREATCIRCLE(ST_POINT(0, 0), ST_POINT(0, 10), 11) as greatcircle1,
-    ST_GREATCIRCLE(ST_POINT(-1.70325, 1.4167), ST_POINT(1.70325, -1.4167), 5) as greatcircle2,
-    ST_GREATCIRCLE(ST_POINT(5, 5), ST_POINT(-5, -5), 9) as greatcircle3`;
+    // ST_ASGEOJSON returns GeoJSON objects directly (no PARSE_JSON needed)
+    const query = `SELECT
+        ST_ASGEOJSON(ST_GREATCIRCLE(ST_POINT(0, 0), ST_POINT(0, 10), 11)) as greatcircle1,
+        ST_ASGEOJSON(ST_GREATCIRCLE(ST_POINT(-1.70325, 1.4167), ST_POINT(1.70325, -1.4167), 5)) as greatcircle2,
+        ST_ASGEOJSON(ST_GREATCIRCLE(ST_POINT(5, 5), ST_POINT(-5, -5), 9)) as greatcircle3`;
     const rows = await runQuery(query);
     expect(rows.length).toEqual(1);
-    expect(JSON.stringify(rows[0].GREATCIRCLE1)).toEqual('{"coordinates":[[0,0],[0,1],[0,1.9999999999999996],[0,3.0000000000000004],[0,4],[0,4.999999999999999],[0,6],[0,7],[0,7.999999999999998],[0,9.000000000000002],[0,10]],"type":"LineString"}');
-    expect(JSON.stringify(rows[0].GREATCIRCLE2)).toEqual('{"coordinates":[[-1.7032500000000002,1.4166999999999998],[-0.8514948107357666,0.7084282465414006],[0,0],[0.8514948107357666,-0.7084282465414006],[1.7032500000000002,-1.4166999999999998]],"type":"LineString"}');
-    expect(JSON.stringify(rows[0].GREATCIRCLE3)).toEqual('{"coordinates":[[4.999999999999999,4.999999999999999],[3.7458253753562287,3.7520830815016724],[2.4952312784633994,2.5023786781263677],[1.2470204043859645,1.2514859293872385],[0,0],[-1.2470204043859645,-1.2514859293872385],[-2.4952312784633994,-2.5023786781263677],[-3.7458253753562287,-3.7520830815016724],[-4.999999999999999,-4.999999999999999]],"type":"LineString"}');
+
+    // Test GREATCIRCLE1 - simple north-south line
+    expect(isGeometryCloseTo(rows[0].GREATCIRCLE1, {
+        type: 'LineString',
+        coordinates: [
+            [0, 0], [0, 1], [0, 2], [0, 3], [0, 4], [0, 5],
+            [0, 6], [0, 7], [0, 8], [0, 9], [0, 10]
+        ]
+    })).toBe(true);
+
+    // Test GREATCIRCLE2 - diagonal line
+    expect(isGeometryCloseTo(rows[0].GREATCIRCLE2, {
+        type: 'LineString',
+        coordinates: [
+            [-1.70325, 1.4167], [-0.851495, 0.708428], [0, 0],
+            [0.851495, -0.708428], [1.70325, -1.4167]
+        ]
+    })).toBe(true);
+
+    // Test GREATCIRCLE3 - longer diagonal line
+    expect(isGeometryCloseTo(rows[0].GREATCIRCLE3, {
+        type: 'LineString',
+        coordinates: [
+            [5, 5], [3.745825, 3.752083], [2.495231, 2.502379],
+            [1.24702, 1.251486], [0, 0], [-1.24702, -1.251486],
+            [-2.495231, -2.502379], [-3.745825, -3.752083], [-5, -5]
+        ]
+    })).toBe(true);
 });
 
 test('ST_GREATCIRCLE should return NULL if any NULL mandatory argument', async () => {
