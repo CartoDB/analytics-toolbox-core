d30b40712ffbd505738557caec83f1fb28ef25c1
Diego Vicente <dvicente@carto.com>
2026-04-08 17:01:10 +0200

    feat(ora|quadbin): add QUADBIN_ISVALID

---
+++ clouds/oracle/modules/test/quadbin/test_QUADBIN_ISVALID.py
@@ -0,0 +1,52 @@
+# Copyright (c) 2026, CARTO
+
+import sys
+import os
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', '..', '..', 'common'))
+from run_query import run_query
+
+
+def test_quadbin_isvalid():
+    result = run_query(
+        """
+        SELECT @@ORA_SCHEMA@@.QUADBIN_ISVALID(5209574053332910079) FROM DUAL
+        UNION ALL
+        SELECT @@ORA_SCHEMA@@.QUADBIN_ISVALID(5192650370358181887) FROM DUAL
+        """,
+        fetch=True,
+    )
+    assert result[0][0] == 1
+    assert result[1][0] == 1
+
+
+def test_quadbin_isvalid_invalid():
+    result = run_query(
+        """
+        SELECT @@ORA_SCHEMA@@.QUADBIN_ISVALID(0) FROM DUAL
+        UNION ALL
+        SELECT @@ORA_SCHEMA@@.QUADBIN_ISVALID(-1) FROM DUAL
+        UNION ALL
+        SELECT @@ORA_SCHEMA@@.QUADBIN_ISVALID(1234) FROM DUAL
+        """,
+        fetch=True,
+    )
+    assert result[0][0] == 0
+    assert result[1][0] == 0
+    assert result[2][0] == 0
+
+
+def test_quadbin_isvalid_invalid_trailing_bits():
+    result = run_query(
+        "SELECT @@ORA_SCHEMA@@.QUADBIN_ISVALID(5209538868960821248) FROM DUAL",
+        fetch=True,
+    )
+    assert result[0][0] == 0
+
+
+def test_quadbin_isvalid_null():
+    result = run_query(
+        "SELECT @@ORA_SCHEMA@@.QUADBIN_ISVALID(NULL) FROM DUAL",
+        fetch=True,
+    )
+    assert result[0][0] == 0
